4788c5c1ee2d10a0532ca242d3573de00a4588ef
Vincent Le Ligeour <yoda-jm at users.sourceforge.net>
2006-10-16 18:41:32 +0000

    Added SDL_thread include
    svn: r18

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_mixer.h>
-#include <SDL/SDL_ttf.h> 
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_thread.h>
 
 #include <sys/ioctl.h>
 #include <linux/soundcard.h>
