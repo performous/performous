1bb9b4d28b80d9a5e0376bdd8f9a72df9328b63f
Vincent Le Ligeour <yoda-jm at users.sourceforge.net>
2007-01-15 01:25:29 +0000

    Added experimental gstreamer support
    svn: r97

--- include/audio.h
+++ include/audio.h
@@ -1,8 +1,18 @@
 #ifndef __AUDIO_H_
 #define __AUDIO_H_
 
-#include <xine.h>
 #include <vector>
+
+#ifdef USE_LIBXINE
+#  include <xine.h>
+#endif
+
+#ifdef USE_GSTREAMER
+#  warning ###################################
+#  warning Gstreamer support is experimental
+#  warning ###################################
+#  include <gst/gst.h>
+#endif
        
 class CAudio {
 	public:
@@ -17,14 +27,20 @@ class CAudio {
 	void playSound( int channel , int id );
 	int getPosition( void );
         private:
-        std::vector <char *> sounds; 
+        std::vector <char *> sounds;
+	int length;
+#ifdef USE_LIBXINE
         xine_t               *xine;
         xine_stream_t        *stream;
         xine_video_port_t    *vo_port;
         xine_audio_port_t    *ao_port;
         xine_event_queue_t   *event_queue;
         bool xine_playing;
-	int length;
+#endif
+#ifdef USE_GSTREAMER
+	GstElement *music;
+	static gboolean bus_call (GstBus *bus,GstMessage *msg,gpointer data);
+#endif
 };
 
 #endif
--- src/audio.cpp
+++ src/audio.cpp
@@ -1,21 +1,36 @@
 #include <audio.h>
-#include <xine.h>
 
-#define XINE_LENGTH_ERROR -1
+#define LENGTH_ERROR -1
+
+#ifdef USE_GSTREAMER
+bool gst_playing;
+#endif
 
 CAudio::CAudio()
 {
+#ifdef USE_LIBXINE
 	xine = xine_new();
         xine_init(xine);
         vo_port = xine_open_video_driver ( xine, NULL, XINE_VISUAL_TYPE_NONE, NULL);    /*Create a fake vo_port*/ 
         ao_port = xine_open_audio_driver(xine , "auto", NULL);
         stream = xine_stream_new(xine, ao_port, vo_port);
         event_queue = xine_event_new_queue(stream);
+#endif
+#ifdef USE_GSTREAMER
+	/* init GStreamer */
+	gst_init (NULL, NULL);
+	/* set up */
+	music = gst_element_factory_make ("playbin", "play");
+	GstBus *bus = gst_pipeline_get_bus (GST_PIPELINE (music));
+	gst_bus_add_watch ( bus ,bus_call, NULL);
+	gst_object_unref (bus);
+#endif
 	length = 0;
 }
 
 CAudio::~CAudio()
 {
+#ifdef USE_LIBXINE
         xine_close(stream);
         xine_event_dispose_queue(event_queue);
         xine_dispose(stream);
@@ -23,6 +38,10 @@ CAudio::~CAudio()
         xine_close_audio_driver(xine, ao_port);
         xine_close_video_driver(xine, vo_port);   
         xine_exit(xine);
+#endif
+#ifdef USE_GSTREAMER
+	gst_object_unref (GST_OBJECT (music));
+#endif
 }
 
 void CAudio::loadMusic( char * )
@@ -32,32 +51,55 @@ void CAudio::loadMusic( char * )
 
 void CAudio::playMusic( char * filename )
 {
+        if (isPlaying()) 
+            stopMusic();
+
+#ifdef USE_LIBXINE
         int pos_stream;
 	int pos_time;
 
-        if (isPlaying()) 
-            stopMusic();
-        
         if (!xine_open(stream, filename) || !xine_play(stream, 0, 0)) {
             printf("could not open %s\n", filename);
         }
 
 	if( !xine_get_pos_length(stream, &pos_stream, &pos_time, &length) )
-		length = XINE_LENGTH_ERROR;
+		length = LENGTH_ERROR;
 
         xine_playing = 1;
+#endif
+#ifdef USE_GSTREAMER
+	g_object_set (G_OBJECT (music), "uri", g_strconcat("file://",filename,NULL), NULL);
+	gst_element_set_state (music, GST_STATE_PLAYING);
+	GstFormat fmt = GST_FORMAT_TIME;
+	gint64 len;
+	if (!gst_element_query_duration (music, &fmt, &len))
+		length = LENGTH_ERROR;
+	else
+		length = (int) (len/1000000);
+	gst_playing = 1;
+#endif
 }
 
 int CAudio::getLength( void )
 {
-	if( length == XINE_LENGTH_ERROR ) {
+	if( length == LENGTH_ERROR ) {
+#ifdef USE_LIBXINE
         	int pos_stream;
 		int pos_time;
 		if( !xine_get_pos_length(stream, &pos_stream, &pos_time, &length) )
-			length = XINE_LENGTH_ERROR;
+			length = LENGTH_ERROR;
+#endif
+#ifdef USE_GSTREAMER
+		GstFormat fmt = GST_FORMAT_TIME;
+		gint64 len;
+		if (!gst_element_query_duration (music, &fmt, &len))
+			length = LENGTH_ERROR;
+		else
+			length = (int) (len/1000000);
+#endif
 	}
 
-	if( length == XINE_LENGTH_ERROR )
+	if( length == LENGTH_ERROR )
 		return 0;
 	else
 		return length;
@@ -65,6 +107,7 @@ int CAudio::getLength( void )
 
 bool CAudio::isPlaying( void )
 {
+#ifdef USE_LIBXINE
         xine_event_t *event; 
         while((event = xine_event_get(event_queue))) {
             switch(event->type) {
@@ -79,11 +122,21 @@ bool CAudio::isPlaying( void )
 	} else {
 		return false;
 	}
+#endif
+#ifdef USE_GSTREAMER
+	return gst_playing;
+#endif
 }
 
 void CAudio::stopMusic( void )
 {
+#ifdef USE_LIBXINE
         xine_stop(stream);
+#endif
+#ifdef USE_GSTREAMER
+	gst_element_set_state (music, GST_STATE_NULL);
+	gst_playing = 0;
+#endif
 }
 
 int CAudio::loadSound( char * filename )
@@ -92,22 +145,17 @@ int CAudio::loadSound( char * filename )
         return sounds.size()-1;
 }
 
-void CAudio::playSound( int , int id )
+void CAudio::playSound( int , int )
 {
-        if (isPlaying()) 
-            stopMusic();
-        
-        if (!xine_open(stream, sounds[id]) || !xine_play(stream, 0, 0)) {
-            printf("could not open %s\n", sounds[id]);
-        }
-        xine_playing = 1;
 }
 
 int CAudio::getPosition( void )
 {
+  int position;
+
+#ifdef USE_LIBXINE
   int pos_stream;
   int length_time;
-  int position;
   int pos_time;
 
   if (xine_get_pos_length(stream, &pos_stream, &pos_time, &length_time)) {
@@ -115,6 +163,38 @@ int CAudio::getPosition( void )
   } else {
     position = 0;
   }
+#endif
+#ifdef USE_GSTREAMER
+  GstFormat fmt = GST_FORMAT_TIME;
+  gint64 pos;
+  if (!gst_element_query_position (music, &fmt, &pos))
+    position = 0;
+  else
+    position = (int) (pos/1000000);
+#endif
 
   return position;
 }
+
+#ifdef USE_GSTREAMER
+gboolean CAudio::bus_call( GstBus *bus, GstMessage *msg, gpointer    data )
+{
+	bus = bus;
+	data = data;
+
+	switch (GST_MESSAGE_TYPE (msg)) {
+		case GST_MESSAGE_EOS:
+			gst_playing = 0;
+			break;
+		case GST_MESSAGE_ERROR: {
+			break;
+	        }
+		case GST_MESSAGE_TAG: {
+			break;
+		}
+		default:
+			break;
+	}
+	return TRUE;
+}
+#endif
