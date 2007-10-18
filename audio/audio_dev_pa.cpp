b2d3a505322572cad87dc1a83532da26b82b5e25
Lasse Kärkkäinen <tronic+ndrm at trn.iki.fi>
2007-10-18 08:56:21 +0000

    Fixed PortAudio
    svn: r450

--- audio/audio_dev_pa.cpp
+++ audio/audio_dev_pa.cpp
@@ -10,12 +10,12 @@ namespace da {
 			pa19_record& self = *static_cast<pa19_record*>(userdata);
 			int16_t const* iptr = static_cast<int16_t const*>(input);
 			try {
-				std::vector<sample_t> buf(frames * self.s.channels);
+				std::vector<sample_t> buf(frames * self.s.channels());
 				std::transform(iptr, iptr + buf.size(), buf.begin(), conv_from_s16);
-				pcm_data data(&buf[0], frames, self.s.channels);
-				self.s.callback(data, self.s);
+				pcm_data data(&buf[0], frames, self.s.channels());
+				self.s.callback()(data, self.s);
 			} catch (std::exception& e) {
-				if (self.s.debug) *self.s.debug << "Exception from recording callback: " << e.what() << std::endl;
+				self.s.debug(std::string("Exception from recording callback: ") + e.what());
 			}
 			return paContinue;
 		}
@@ -31,32 +31,32 @@ namespace da {
 			PaStream* handle;
 			strm( pa19_record* rec) {
 				PaStreamParameters p;
-				if (rec->s.subdev.empty()) {
+				if (rec->s.subdev().empty()) {
 					p.device = Pa_GetDefaultInputDevice();
 				} else {
-					std::istringstream iss(rec->s.subdev);
+					std::istringstream iss(rec->s.subdev());
 					iss >> p.device;
 					if (!iss.eof() || p.device < 0 || p.device > Pa_GetDeviceCount() - 1) throw std::invalid_argument("Invalid PortAudio device number");
 				}
-				p.channelCount = rec->s.channels;
+				p.channelCount = rec->s.channels();
 				p.sampleFormat = paInt16;
-				if (rec->s.frames != settings::high) p.suggestedLatency = Pa_GetDeviceInfo(p.device)->defaultLowInputLatency;
+				if (rec->s.frames() != settings::high) p.suggestedLatency = Pa_GetDeviceInfo(p.device)->defaultLowInputLatency;
 				else p.suggestedLatency = Pa_GetDeviceInfo(p.device)->defaultHighInputLatency;
 				p.hostApiSpecificStreamInfo = NULL;
-				PaError err = Pa_OpenStream(&handle, &p, NULL, rec->s.rate, 50, paClipOff, rec->c_callback, rec);
-				if (err != paNoError) throw std::runtime_error("Cannot open PortAudio audio stream " + rec->s.subdev + ": " + Pa_GetErrorText(err));
+				PaError err = Pa_OpenStream(&handle, &p, NULL, rec->s.rate(), 50, paClipOff, rec->c_callback, rec);
+				if (err != paNoError) throw std::runtime_error("Cannot open PortAudio audio stream " + rec->s.subdev() + ": " + Pa_GetErrorText(err));
 			}
 			~strm() { Pa_CloseStream(handle); }
 		} stream;
 	  public:
 		pa19_record(settings& s_orig): s(s_orig), initialize(), stream(this) {
 			PaError err = Pa_StartStream(stream.handle);
-			if( err != paNoError ) throw std::runtime_error("Cannot start PortAudio audio stream " + s.subdev + ": " + Pa_GetErrorText(err));
+			if( err != paNoError ) throw std::runtime_error("Cannot start PortAudio audio stream " + s.subdev() + ": " + Pa_GetErrorText(err));
 			s_orig = s;
 		}
 	};
 	namespace {
-		record_plugin::reg<pa19_record> r(info("pa", "PortAudio v19 PCM capture. Device number as settings (otherwise PA default)."));
+		record_plugin::reg<pa19_record> r(devinfo("pa", "PortAudio v19 PCM capture. Device number as settings (otherwise PA default)."));
 	}
 }
 
--- audio/audio_dev_pa18.cpp
+++ audio/audio_dev_pa18.cpp
@@ -10,12 +10,12 @@ namespace da {
 			pa18_record& self = *static_cast<pa18_record*>(userdata);
 			int16_t const* iptr = static_cast<int16_t const*>(input);
 			try {
-				std::vector<sample_t> buf(frames * self.s.channels);
+				std::vector<sample_t> buf(frames * self.s.channels());
 				std::transform(iptr, iptr + buf.size(), buf.begin(), conv_from_s16);
-				pcm_data data(&buf[0], frames, self.s.channels);
-				self.s.callback(data, self.s);
+				pcm_data data(&buf[0], frames, self.s.channels());
+				self.s.callback()(data, self.s);
 			} catch (std::exception& e) {
-				if (self.s.debug) *self.s.debug << "Exception from recording callback: " << e.what() << std::endl;
+				self.s.debug(std::string("Exception from recording callback: ") + e.what());
 			}
 			return 0;
 		}
@@ -31,18 +31,18 @@ namespace da {
 			PaStream* handle;
 			strm(pa18_record* rec) {
 				// TODO: Use more settings from s
-				PaError err = Pa_OpenStream(&handle, Pa_GetDefaultInputDeviceID(), rec->s.channels, paInt16, NULL, paNoDevice, 0, paInt16, NULL, rec->s.rate, 50, 0, 0, rec->c_callback, rec);
-				if (err != paNoError) throw std::runtime_error("Cannot open PortAudio audio stream " + rec->s.subdev + ": " + Pa_GetErrorText(err));
+				PaError err = Pa_OpenStream(&handle, Pa_GetDefaultInputDeviceID(), rec->s.channels(), paInt16, NULL, paNoDevice, 0, paInt16, NULL, rec->s.rate, 50, 0, 0, rec->c_callback, rec);
+				if (err != paNoError) throw std::runtime_error("Cannot open PortAudio audio stream " + rec->s.subdev() + ": " + Pa_GetErrorText(err));
 			}
 			~strm() { Pa_CloseStream(handle); }
 		} stream;
 	  public:
 		pa18_record(settings& s): s(s), initialize(), stream(this) {
 			PaError err = Pa_StartStream(stream.handle);
-			if( err != paNoError ) throw std::runtime_error("Cannot start PortAudio audio stream " + s.subdev + ": " + Pa_GetErrorText(err));
+			if( err != paNoError ) throw std::runtime_error("Cannot start PortAudio audio stream " + s.subdev() + ": " + Pa_GetErrorText(err));
 		}
 	};
 	namespace {
-		record_plugin::reg<pa18_record> r(info("pa18", "PortAudio v18 PCM capture. Settings are not used."));
+		record_plugin::reg<pa18_record> r(devinfo("pa18", "PortAudio v18 PCM capture. Settings are not used."));
 	}
 }
