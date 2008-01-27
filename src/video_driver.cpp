732350b48237e49f965f100863382860ee12fdef
Vincent Le Ligeour <yoda-jm at users.sourceforge.net>
2008-01-27 13:22:17 +0000

    SDL_Surface are RGBA ordered
    svn: r557

--- src/video_driver.cpp
+++ src/video_driver.cpp
@@ -157,8 +157,8 @@ void CVideoDriver::drawSurface(SDL_Surface* _surf, int _x, int _y)
 {
 #ifdef USE_OPENGL
 	unsigned int texture;
-	SDL_GL::initTexture (_surf->w,_surf->h, &texture, GL_BGRA);
-	SDL_GL::draw_func(_surf->w,_surf->h,(unsigned char*)_surf->pixels,texture, GL_BGRA, _x, _y);
+	SDL_GL::initTexture (_surf->w,_surf->h, &texture, GL_RGBA);
+	SDL_GL::draw_func(_surf->w,_surf->h,(unsigned char*)_surf->pixels,texture, GL_RGBA, _x, _y);
 	SDL_GL::freeTexture(texture);
 #else
 	CScreenManager * sm = CScreenManager::getSingletonPtr();
