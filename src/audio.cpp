baae872b44c3dbb22d0a1637995a041a40373390
Vincent Le Ligeour <yoda-jm at users.sourceforge.net>
2008-04-26 09:25:29 +0000

    Fixed warning about gstreamer version when compiled with xine audio backend
    svn: r715

--- src/audio.cpp
+++ src/audio.cpp
@@ -254,9 +254,11 @@ void CAudio::togglePause_internal() {
 #endif
 }
 
-#ifndef GSTREAMER_HAS_SEEK_SIMPLE
-#  define gst_element_seek_simple(element,format,flag,pos) (gst_element_seek(element,1.0,format,flag,GST_SEEK_TYPE_SET,pos,GST_SEEK_TYPE_NONE,0))
-#  warning "gstreamer < 0.10.7 support is obsolete, please update your gstreamer version"
+#ifdef USE_GSTREAMER_AUDIO
+#  ifndef GSTREAMER_HAS_SEEK_SIMPLE
+#    define gst_element_seek_simple(element,format,flag,pos) (gst_element_seek(element,1.0,format,flag,GST_SEEK_TYPE_SET,pos,GST_SEEK_TYPE_NONE,0))
+#    warning "gstreamer < 0.10.7 support is obsolete, please update your gstreamer version"
+#  endif
 #endif
 
 void CAudio::seek_internal(double seek_dist) {
