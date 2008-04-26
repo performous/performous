14fd67d2eb30ed1e90aae02d7e9d237c12ae9acb
Vincent Le Ligeour <yoda-jm at users.sourceforge.net>
2008-04-26 09:26:02 +0000

    Fixed new ffmpeg include path
    svn: r716

--- include/ffmpeg.hpp
+++ include/ffmpeg.hpp
@@ -5,11 +5,21 @@
 
 #ifdef USE_FFMPEG_VIDEO
 
+#ifdef USE_NEW_FFMPEG_VIDEO
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+}
+#endif
+
+#ifdef USE_OLD_FFMPEG_VIDEO
 extern "C" {
 #include <ffmpeg/avcodec.h>
 #include <ffmpeg/avformat.h>
 #include <ffmpeg/swscale.h>
 }
+#endif
 
 #include <boost/ptr_container/ptr_deque.hpp>
 #include <boost/ptr_container/ptr_set.hpp>
