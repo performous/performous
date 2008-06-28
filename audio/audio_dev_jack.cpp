f426b34630ab2e815ea3fd2153db350285644545
Lasse Kärkkäinen <tronic+ndrm at trn.iki.fi>
2008-06-28 16:34:35 +0000

    Compiles fine
    svn: r814

--- audio/audio_dev_jack.cpp
+++ audio/audio_dev_jack.cpp
@@ -1,4 +1,5 @@
 #include "audio_dev.hpp"
+#include <boost/lexical_cast.hpp>
 #include <jack/jack.h>
 #include <algorithm>
 
@@ -21,8 +22,8 @@ namespace {
 				if (!p) throw std::runtime_error("Unable to register JACK port");
 				m_ports.push_back(p);
 			}
-			s.set_rate(m_clientack_get_sample_rate(m_client));
-			s.set_frames(m_clientack_get_buffer_size(m_client));
+			s.set_rate(jack_get_sample_rate(m_client));
+			s.set_frames(jack_get_buffer_size(m_client));
 			m_s = s;
 			jack_set_process_callback(m_client, libda_jack_record_callback, this);
 			jack_activate(m_client);
@@ -30,12 +31,14 @@ namespace {
 		~jack_record() { jack_client_close(m_client); }
 		int callback(jack_nframes_t frames) {
 			std::vector<jack_default_audio_sample_t*> samples(m_ports.size());
-			for (size_t ch = 0; ch < m_ports.size(); ++ch) samples[ch] = static_cast<jack_default_audio_sample_t*>(jack_port_get_buffer(m_ports[ch], nframes));
+			for (size_t ch = 0; ch < m_ports.size(); ++ch) samples[ch] = static_cast<jack_default_audio_sample_t*>(jack_port_get_buffer(m_ports[ch], frames));
 			std::vector<sample_t> buf;
 			buf.reserve(frames * m_s.channels());
 			for (jack_nframes_t fr = 0; fr < frames; ++fr) for (size_t ch = 0; ch < m_ports.size(); ++ch) buf.push_back(*samples[ch]++);
+			pcm_data data(&buf[0], frames, m_ports.size());
 			m_s.set_frames(frames);
-			m_s.callback()(@buf[0], m_s);
+			m_s.callback()(data, m_s);
+			return 0;
 		}
 	};
 	extern "C" int libda_jack_record_callback(jack_nframes_t frames, void* arg) { return static_cast<jack_record*>(arg)->callback(frames); }
@@ -44,7 +47,8 @@ namespace {
 	extern "C" int libda_jack_playback_callback(jack_nframes_t frames, void* arg);
 	class jack_playback: public playback::dev {
 		settings m_s;
-		jack_client_t m_client;
+		jack_client_t* m_client;
+		std::vector<jack_port_t*> m_ports;
 	  public:
 		jack_playback(settings& s):
 		  m_client(jack_client_new(s.subdev().empty() ? "libda_jack_playback" : s.subdev().c_str()))
@@ -56,14 +60,22 @@ namespace {
 				if (!p) throw std::runtime_error("Unable to register JACK port");
 				m_ports.push_back(p);
 			}
-			s.set_rate(m_clientack_get_sample_rate(m_client));
-			s.set_frames(m_clientack_get_buffer_size(m_client));
+			s.set_rate(jack_get_sample_rate(m_client));
+			s.set_frames(jack_get_buffer_size(m_client));
 			m_s = s;
 			jack_set_process_callback(m_client, libda_jack_playback_callback, this);
 			jack_activate(m_client);
 		}
 		~jack_playback() { jack_client_close(m_client); }
 		int callback(jack_nframes_t frames) {
+			std::vector<sample_t> buf(frames * m_s.channels());
+			pcm_data data(&buf[0], frames, m_ports.size());
+			m_s.set_frames(frames);
+			m_s.callback()(data, m_s);
+			std::vector<jack_default_audio_sample_t*> samples(m_ports.size());
+			for (size_t ch = 0; ch < m_ports.size(); ++ch) samples[ch] = static_cast<jack_default_audio_sample_t*>(jack_port_get_buffer(m_ports[ch], frames));
+			for (jack_nframes_t fr = 0; fr < frames; ++fr) for (size_t ch = 0; ch < m_ports.size(); ++ch) *samples[ch]++ = buf[fr * m_ports.size() + ch];
+			return 0;
 		}
 	};
 	extern "C" int libda_jack_playback_callback(jack_nframes_t frames, void* arg) { return static_cast<jack_playback*>(arg)->callback(frames); }
