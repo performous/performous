dce3dcc3b8608a381d322efe9e9a766f3ed46041
Vincent Le Ligeour <yoda-jm at users.sourceforge.net>
2008-09-23 21:09:41 +0000

    Added first opengl themed texture (WIP)
    svn: r983

---
+++ game/opengl_text.cpp
@@ -0,0 +1,112 @@
+#include "opengl_text.hh"
+#include <pango/pangocairo.h>
+#include <math.h>
+#include <iostream>
+
+namespace {
+	unsigned int nextPow2(double _val) {
+		unsigned int ret = 1;
+		unsigned int val = ceil(_val);
+		while (ret < val) ret *= 2;
+		return ret;
+	}
+}
+
+OpenGLText::OpenGLText(TThemeTxtOpenGL _text) {
+	// set some pango values
+	PangoAlignment alignment;
+	PangoWeight weight;
+	PangoStyle style;
+	PangoLayout *layout;
+
+	if( _text.fontalign == std::string("start") ) {
+		alignment = PANGO_ALIGN_LEFT;
+	} else if( _text.fontalign == std::string("center") ) {
+		alignment = PANGO_ALIGN_CENTER;
+	} else if( _text.fontalign == std::string("end") ) {
+		alignment = PANGO_ALIGN_RIGHT;
+	} else {
+		alignment = PANGO_ALIGN_LEFT;
+	}
+	if( _text.fontweight == std::string("normal") ) {
+		weight = PANGO_WEIGHT_NORMAL;
+	} else if( _text.fontweight == std::string("bold") ) {
+		weight = PANGO_WEIGHT_BOLD;
+	} else if( _text.fontweight == std::string("bolder") ) {
+		weight = PANGO_WEIGHT_ULTRABOLD;
+	} else {
+		weight = PANGO_WEIGHT_NORMAL;
+	}
+	if( _text.fontstyle == std::string("normal") ) {
+		style = PANGO_STYLE_NORMAL;
+	} else if( _text.fontstyle == std::string("italic") ) {
+		style = PANGO_STYLE_ITALIC;
+	} else if( _text.fontstyle == std::string("oblique") ) {
+		style = PANGO_STYLE_OBLIQUE;
+	} else {
+		style = PANGO_STYLE_NORMAL;
+	}
+	// set font description
+	PangoFontDescription *desc = pango_font_description_new();
+	pango_font_description_set_weight(desc, weight);
+	pango_font_description_set_style(desc, style);
+	pango_font_description_set_family(desc, _text.fontfamily.c_str());
+	pango_font_description_set_absolute_size (desc,_text.fontsize * PANGO_SCALE);
+
+	// compute text extents
+	PangoRectangle rec;
+	PangoContext* ctx=NULL;
+	ctx = pango_cairo_font_map_create_context ((PangoCairoFontMap*)pango_cairo_font_map_get_default());
+	layout = pango_layout_new(ctx);
+	pango_layout_set_alignment(layout, alignment);
+	pango_layout_set_font_description (layout, desc);
+	pango_layout_set_text (layout, _text.text.c_str(), -1);
+	pango_layout_get_pixel_extents (layout,NULL,&rec);
+	x = rec.width;
+	y = rec.height;
+	x_advance = rec.width+rec.x;
+	y_advance = rec.height+rec.y;
+	x_power_of_two = nextPow2(x);
+	y_power_of_two = nextPow2(y);
+	g_object_unref (layout);
+	g_object_unref (ctx);
+
+	// create surface
+	cairo_surface_t *surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, x_power_of_two, y_power_of_two);
+	cairo_t *dc = cairo_create(surface);
+
+	// draw the surface
+	layout = pango_cairo_create_layout(dc);
+	pango_layout_set_alignment(layout, alignment);
+	pango_layout_set_font_description (layout, desc);
+	pango_layout_set_text (layout, _text.text.c_str(), -1);
+	pango_cairo_update_layout (dc, layout);
+	pango_cairo_show_layout (dc, layout);
+	pango_cairo_layout_path(dc,layout);
+	if (_text.fill_col.r != -1 && _text.fill_col.g != -1 && _text.fill_col.b != -1) {
+		cairo_set_source_rgba(dc, _text.fill_col.r, _text.fill_col.g, _text.fill_col.b, _text.fill_col.a);
+		if (_text.stroke_col.r != -1 && _text.stroke_col.g != -1 && _text.stroke_col.b != -1) cairo_fill_preserve(dc);
+		else cairo_fill(dc);
+	}
+	if (_text.stroke_col.r != -1 && _text.stroke_col.g != -1 && _text.stroke_col.b != -1) {
+		cairo_set_line_width(dc, _text.stroke_width);
+		cairo_set_source_rgba(dc, _text.stroke_col.r, _text.stroke_col.g, _text.stroke_col.b, _text.stroke_col.a);
+		cairo_stroke(dc);
+	}
+	g_object_unref(layout);
+
+	m_surf.reset(new Surface(surface));
+
+	// delete surface
+	cairo_destroy(dc);
+	cairo_surface_destroy(surface);
+	// delete the font description
+	pango_font_description_free(desc);
+}
+
+void OpenGLText::draw(void) {
+	m_surf->draw();
+}
+
+OpenGLText::~OpenGLText() {
+}
---
+++ game/opengl_text.hh
@@ -0,0 +1,47 @@
+#ifndef __OPENGL_THEME_H__
+#define __OPENGL_THEME_H__
+
+#include "theme.hh"
+#include "surface.hh"
+#include <boost/scoped_ptr.hpp>
+
+// Special theme for creating opengl themed surfaces
+// this structure does not include:
+//  - library specific field
+//  - global positionning
+// the font{family,style,weight,align} are the one found into SVGs
+struct TThemeTxtOpenGL {
+	TRGBA fill_col;
+	TRGBA stroke_col;
+	double stroke_width;
+	double fontsize;
+	std::string fontfamily;
+	std::string fontstyle;
+	std::string fontweight;
+	std::string fontalign;
+	std::string text;
+	TThemeTxtOpenGL(): stroke_width(), fontsize() {}
+};
+
+// this class will enable to create a texture from a themed text structure
+// it will not cache any data (class using this class should)
+// it provides size of the texture are drawn (x,y)
+// it provides size of the texture created (x_power_of_two, y_power_of_two)
+class OpenGLText {
+	public:
+	OpenGLText(TThemeTxtOpenGL _text);
+	~OpenGLText();
+	void draw(void);
+	private:
+	unsigned int x_power_of_two;
+	unsigned int y_power_of_two;
+	double x;
+	double y;
+	double x_advance;
+	double y_advance;
+	// TODO: only have a texture id uploaded here
+	// because Surface do way too much things (global positionning for example)
+	boost::scoped_ptr<Surface> m_surf;
+};
+
+#endif // __OPENGL_TEXT_H__
