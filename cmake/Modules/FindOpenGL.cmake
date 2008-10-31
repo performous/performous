6d1c24b13eb40c7d310d57b68857307250cb1ddc
Lasse Kärkkäinen <tronic+ndrm at trn.iki.fi>
2008-10-31 21:20:45 +0000

    Better OpenGL finding script
    svn: r1154

---
+++ cmake/Modules/FindOpenGL.cmake
@@ -0,0 +1,35 @@
+# - Try to find OpenGL
+# Once done, this will define
+#
+#  OpenGL_FOUND - system has OpenGL (GL and GLU)
+#  OpenGL_INCLUDE_DIRS - the OpenGL include directories
+#  OpenGL_LIBRARIES - link these to use OpenGL
+#  OpenGL_GL_LIBRARY - only GL
+#  OpenGL_GLU_LIBRARY - only GLU
+#
+# See documentation on how to write CMake scripts at
+# http://www.cmake.org/Wiki/CMake:How_To_Find_Libraries
+
+include(LibFindMacros)
+
+libfind_pkg_check_modules(OpenGL_PKGCONF gl)
+
+find_path(OpenGL_INCLUDE_DIR
+  NAMES GL/gl.h
+  PATHS ${OpenGL_PKGCONF_INCLUDE_DIRS}
+)
+
+find_library(OpenGL_GL_LIBRARY
+  NAMES GL
+  PATHS ${OpenGL_PKGCONF_LIBRARY_DIRS}
+)
+
+find_library(OpenGL_GLU_LIBRARY
+  NAMES GLU
+  PATHS ${OpenGL_PKGCONF_LIBRARY_DIRS}
+)
+
+set(OpenGL_PROCESS_INCLUDES OpenGL_INCLUDE_DIR)
+set(OpenGL_PROCESS_LIBS OpenGL_GL_LIBRARY OpenGL_GLU_LIBRARY)
+libfind_process(OpenGL)
+
