07be0e16772bb548a6ab38c9404827a9596b9d8d
Lasse Kärkkäinen <tronic+ndrm at trn.iki.fi>
2008-11-03 11:17:09 +0000

    Fix a race condition
    svn: r1170

--- game/record.hh
+++ game/record.hh
@@ -9,11 +9,13 @@
 class Capture {
 	static const std::size_t DEFAULT_RATE = 48000;
 	// The order of the following variables is important!
+	volatile bool m_running;
 	boost::ptr_vector<Analyzer> m_analyzers;
 	da::settings m_rs;
 	da::record m_record;
   public:
 	Capture(std::size_t channels = 2, std::string const& device = "", std::size_t rate = DEFAULT_RATE):
+	  m_running(false),
 	  m_rs(da::settings(device)
 	  .set_callback(boost::ref(*this))
 	  .set_channels(channels)
@@ -24,10 +26,13 @@ class Capture {
 		for (std::size_t ch = 0; ch < channels; ++ch) {
 			m_analyzers.push_back(new Analyzer(m_rs.rate()));
 		}
+		m_running = true;
 	}
 	void operator()(da::pcm_data& areas, da::settings const&) {
+		if (!m_running) return;
 		for (std::size_t ch = 0; ch < m_analyzers.size(); ++ch) m_analyzers[ch].input(areas.begin(ch), areas.end(ch));
 	}
+	void setRunning(bool value) { m_running = value; }
 	boost::ptr_vector<Analyzer>& analyzers() { return m_analyzers; }
 };
 
