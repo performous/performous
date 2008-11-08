32ec8d3da60ff2a154db44915a44fcf059d4f40d
Vincent Le Ligeour <yoda-jm at users.sourceforge.net>
2008-11-08 17:49:43 +0000

    Updated GStreamer cmake to new scheme
    svn: r1228

--- cmake/Modules/FindGStreamer.cmake
+++ cmake/Modules/FindGStreamer.cmake
@@ -1,71 +1,32 @@
 # - Try to find GStreamer
-# Once done this will define
+# Once done, this will define
 #
-#  GSTREAMER_FOUND - system has GStreamer
-#  GSTREAMER_INCLUDE_DIR - the GStreamer include directory
-#  GSTREAMER_LIBRARIES - the libraries needed to use GStreamer
-#  GSTREAMER_DEFINITIONS - Compiler switches required for using GStreamer
+#  GStreamer_FOUND - system has GStreamer
+#  GStreamer_INCLUDE_DIRS - the GStreamer include directories
+#  GStreamer_LIBRARIES - link these to use GStreamer
 
-#  (c)2006, Tim Beaulen <tbscope@gmail.com>
+include(LibFindMacros)
 
-# TODO: Other versions --> GSTREAMER_X_Y_FOUND (Example: GSTREAMER_0_8_FOUND and GSTREAMER_0_10_FOUND etc)
+# Dependencies
+libfind_package(GStreamer GObject)
 
-IF (GSTREAMER_INCLUDE_DIR AND GSTREAMER_LIBRARIES AND GSTREAMER_BASE_LIBRARY AND GSTREAMER_INTERFACE_LIBRARY)
-   # in cache already
-   SET(GStreamer_FIND_QUIETLY TRUE)
-ELSE (GSTREAMER_INCLUDE_DIR AND GSTREAMER_LIBRARIES AND GSTREAMER_BASE_LIBRARY AND GSTREAMER_INTERFACE_LIBRARY)
-   SET(GStreamer_FIND_QUIETLY FALSE)
-ENDIF (GSTREAMER_INCLUDE_DIR AND GSTREAMER_LIBRARIES AND GSTREAMER_BASE_LIBRARY AND GSTREAMER_INTERFACE_LIBRARY)
+# Use pkg-config to get hints about paths
+libfind_pkg_check_modules(GStreamer_PKGCONF gstreamer-0.10)
 
-IF (NOT WIN32)
-   # use pkg-config to get the directories and then use these values
-   # in the FIND_PATH() and FIND_LIBRARY() calls
-   INCLUDE(UsePkgConfig)
-   PKGCONFIG(gstreamer-0.10 _GStreamerIncDir _GStreamerLinkDir _GStreamerLinkFlags _GStreamerCflags)
-   MESSAGE(STATUS "DEBUG: GStreamer include directory = ${_GStreamerIncDir}")
-   MESSAGE(STATUS "DEBUG: GStreamer link directory = ${_GStreamerLinkDir}")
-   MESSAGE(STATUS "DEBUG: GStreamer link flags = ${_GStreamerLinkFlags}")
-   MESSAGE(STATUS "DEBUG: GStreamer CFlags = ${_GStreamerCflags}") 
-   SET(GSTREAMER_DEFINITIONS ${_GStreamerCflags})
-ENDIF (NOT WIN32)
+# Include dir
+find_path(GStreamer_INCLUDE_DIR
+  NAMES gst/gst.h
+  PATHS ${GStreamer_PKGCONF_INCLUDE_DIRS}
+)
 
-FIND_PATH(GSTREAMER_INCLUDE_DIR gst/gst.h
-   PATHS
-   ${_GStreamerIncDir}
-   #PATH_SUFFIXES gst
-   )
+# Finally the library itself
+find_library(GStreamer_LIBRARY
+  NAMES gstreamer-0.10
+  PATHS ${GStreamer_PKGCONF_LIBRARY_DIRS}
+)
 
-FIND_LIBRARY(GSTREAMER_LIBRARIES NAMES gstreamer-0.10
-   PATHS
-   ${_GStreamerLinkDir}
-   )
-
-FIND_LIBRARY(GSTREAMER_BASE_LIBRARY NAMES gstbase-0.10
-   PATHS
-   ${_GStreamerLinkDir}
-   )
-
-FIND_LIBRARY(GSTREAMER_INTERFACE_LIBRARY NAMES gstinterfaces-0.10
-   PATHS
-   ${_GStreamerLinkDir}
-   )
-
-IF (GSTREAMER_INCLUDE_DIR)
-   #MESSAGE(STATUS "DEBUG: Found GStreamer include dir: ${GSTREAMER_INCLUDE_DIR}")
-ELSE (GSTREAMER_INCLUDE_DIR)
-   MESSAGE(STATUS "GStreamer: WARNING: include dir not found")
-ENDIF (GSTREAMER_INCLUDE_DIR)
-
-IF (GSTREAMER_LIBRARIES)
-   #MESSAGE(STATUS "DEBUG: Found GStreamer library: ${GSTREAMER_LIBRARIES}")
-ELSE (GSTREAMER_LIBRARIES)
-   MESSAGE(STATUS "GStreamer: WARNING: library not found")
-ENDIF (GSTREAMER_LIBRARIES)
-
-IF (GSTREAMER_INTERFACE_LIBRARY)
-   #MESSAGE(STATUS "DEBUG: Found GStreamer interface library: ${GSTREAMER_INTERFACE_LIBRARY}")
-ELSE (GSTREAMER_INTERFACE_LIBRARY)
-   MESSAGE(STATUS "GStreamer: WARNING: interface library not found")
-ENDIF (GSTREAMER_INTERFACE_LIBRARY)
-
-MARK_AS_ADVANCED(GSTREAMER_INCLUDE_DIR GSTREAMER_LIBRARIES GSTREAMER_BASE_LIBRARY GSTREAMER_INTERFACE_LIBRARY)
+# Set the include dir variables and the libraries and let libfind_process do the rest.
+# NOTE: Singular variables for this library, plural for libraries this this lib depends on.
+set(GStreamer_PROCESS_INCLUDES GStreamer_INCLUDE_DIR GObject_INCLUDE_DIRS)
+set(GStreamer_PROCESS_LIBS GStreamer_LIBRARY GObject_LIBRARIES)
+libfind_process(GStreamer)
