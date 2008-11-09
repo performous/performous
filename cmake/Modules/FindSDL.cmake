7a718515ea3ca7ed12536e5de1f419fa328459d7
Lasse Kärkkäinen <tronic+ndrm at trn.iki.fi>
2008-11-09 13:15:31 +0000

    Our own FindSDL
    svn: r1241

---
+++ cmake/Modules/FindSDL.cmake
@@ -0,0 +1,36 @@
+# - Try to find SDL
+# Once done, this will define
+#
+#  SDL_FOUND - system has SDL
+#  SDL_INCLUDE_DIRS - the SDL include directories
+#  SDL_LIBRARIES - link these to use SDL
+#  SDL_SDL_LIBRARY - only libSDL
+#  SDL_SDLmain_LIBRARY - only libSDLmain
+#
+# See documentation on how to write CMake scripts at
+# http://www.cmake.org/Wiki/CMake:How_To_Find_Libraries
+
+include(LibFindMacros)
+
+libfind_pkg_check_modules(SDL_PKGCONF sdl)
+
+find_path(SDL_INCLUDE_DIR
+  NAMES SDL.h
+  PATHS ${SDL_PKGCONF_INCLUDE_DIRS}
+  PATH_SUFFIXES SDL
+)
+
+find_library(SDL_SDL_LIBRARY
+  NAMES SDL
+  PATHS ${SDL_PKGCONF_LIBRARY_DIRS}
+)
+
+find_library(SDL_SDLmain_LIBRARY
+  NAMES SDLmain
+  PATHS ${SDL_PKGCONF_LIBRARY_DIRS}
+)
+
+set(SDL_PROCESS_INCLUDES SDL_INCLUDE_DIR)
+set(SDL_PROCESS_LIBS SDL_SDL_LIBRARY SDL_SDLmain_LIBRARY)
+libfind_process(SDL)
+
