a77f6d15dce62ac6b3b9203f3be5850d1f8a0854
Lasse Kärkkäinen <tronic+ndrm at trn.iki.fi>
2008-11-09 14:22:09 +0000

    Pango fix Glib and GObject
    svn: r1247

--- cmake/Modules/FindPango.cmake
+++ cmake/Modules/FindPango.cmake
@@ -30,7 +30,7 @@ find_library(Pango_LIBRARY
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this this lib depends on.
-set(Pango_PROCESS_INCLUDES Pango_INCLUDE_DIR Freetype_INCLUDE_DIRS)
-set(Pango_PROCESS_LIBS Pango_LIBRARY Freetype_LIBRARIES)
+set(Pango_PROCESS_INCLUDES Pango_INCLUDE_DIR Freetype_INCLUDE_DIRS Glib_INCLUDE_DIRS)
+set(Pango_PROCESS_LIBS Pango_LIBRARY Freetype_LIBRARIES Glib_LIBRARIES GObject_LIBRARIES)
 libfind_process(Pango)
 
