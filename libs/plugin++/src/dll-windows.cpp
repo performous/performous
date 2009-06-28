05936993f34c26ebcc2184121346359f828fa4b6
Lasse Karkkainen <tronic+ndrm@trn.iki.fi>
2009-06-28 16:35:08 -0500

    Now it actually compiles :)

--- libs/plugin++/src/dll-windows.cpp
+++ libs/plugin++/src/dll-windows.cpp
@@ -5,11 +5,11 @@
 
 namespace plugin {
 	dll::dll(std::string const& filename): lib(LoadLibrary(filename.c_str())) {
-		if (!lib) throw std::runtime_error(dlerror());
+		if (!lib) throw std::runtime_error("Unable to open " + filename);
 	}
 
 	dll::~dll() {
-		FreeLibrary(static_cast<HINSTANCE*>(lib));
+		FreeLibrary(static_cast<HINSTANCE>(lib));
 	}
 }
 
