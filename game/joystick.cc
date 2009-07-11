88deff77391ab2c7520eb63235cb19de6bed6999
Vincent Le Ligeour <yoda-jm@users.sourceforge.net>
2009-07-11 15:33:07 +0200

    Used SDL for joysticks states

--- game/joystick.cc
+++ game/joystick.cc
@@ -8,42 +8,28 @@ class Joystick {
   	Joystick() {};
 	Joystick(unsigned int _id): m_id(_id) {
 		m_joystick = SDL_JoystickOpen(m_id);
-		for( int i = 0 ; i < SDL_JoystickNumButtons(m_joystick) ; i++ ) {
-			m_buttons_states[i] = false;
-		}
-		for( int i = 0 ; i < SDL_JoystickNumAxes(m_joystick) ; i++ ) {
-			m_axes_values[i] = 0;
-		}
-		for( int i = 0 ; i < SDL_JoystickNumHats(m_joystick) ; i++ ) {
-			m_hats_values[i] = 0;
-		}
 	};
 	~Joystick() {/*if(SDL_JoystickOpened(m_id)) SDL_JoystickClose(m_joystick);*/}; // should be called before SDL finished
 	std::string getName() const {return std::string(SDL_JoystickName(m_id));};
 	std::string getDescription() const {
 		std::string desc;
 		desc += "axes: ";
-		desc += boost::lexical_cast<std::string>(m_axes_values.size());
+		desc += boost::lexical_cast<std::string>(SDL_JoystickNumAxes(m_joystick));
 		desc += ", buttons: ";
-		desc += boost::lexical_cast<std::string>(m_buttons_states.size());
+		desc += boost::lexical_cast<std::string>(SDL_JoystickNumButtons(m_joystick));
+		desc += ", balls: ";
+		desc += boost::lexical_cast<std::string>(SDL_JoystickNumBalls(m_joystick));
 		desc += ", hats: ";
-		desc += boost::lexical_cast<std::string>(m_hats_values.size());
+		desc += boost::lexical_cast<std::string>(SDL_JoystickNumHats(m_joystick));
 		return desc;
 	};
-	void buttonPressed( int _button_id) { m_buttons_states[_button_id] = true; };
-	void buttonReleased( int _button_id) { m_buttons_states[_button_id] = false; };
-	bool buttonState(int _button_id) { return m_buttons_states[_button_id];};
-	void hat( int _hat_id, int _value) { m_hats_values[_hat_id] = _value;};
-	void axe( int _axe_id, int _value) { m_axes_values[_axe_id] = _value;};
-
-	int hat( int _hat_id) { return m_hats_values[_hat_id];};
-	int axe( int _axe_id) { return m_axes_values[_axe_id];};
+	bool buttonState(unsigned char _button_id) { return (SDL_JoystickGetButton(m_joystick, _button_id) != 0);};
+	unsigned char hat(unsigned char _hat_id) { return SDL_JoystickGetHat(m_joystick, _hat_id);};
+	short axe(unsigned char _axe_id) { return SDL_JoystickGetAxis(m_joystick, _axe_id);};
+	std::pair<int, int> ball(int _ball_id) {int dx, dy;SDL_JoystickGetBall(m_joystick, _ball_id, &dx,&dy);return std::pair<int,int>(dx,dy);};
   private:
 	SDL_Joystick * m_joystick;
 	unsigned int m_id;
-	std::map<int, bool> m_buttons_states;
-	std::map<int, int> m_axes_values;
-	std::map<int, int> m_hats_values;
 };
 
 std::map<unsigned int,Joystick> joysticks;
@@ -58,16 +44,13 @@ std::map<unsigned int,Joystick> joysticks;
 void check_joystick_event(SDL_Event event, Audio &audio) {
 	switch( event.type ) {
 		case SDL_JOYAXISMOTION:
-			joysticks[(int)event.jaxis.which].axe((int)event.jaxis.axis,(int)event.jaxis.value);
 			break;
 		case SDL_JOYHATMOTION:
-			joysticks[(int)event.jhat.which].axe((int)event.jhat.hat,(int)event.jhat.value);
 			break;
 		case SDL_JOYBALLMOTION:
 			// relatives things, we do not want to manage this for the moment
 			break;
 		case SDL_JOYBUTTONDOWN:
-			joysticks[(int)event.jbutton.which].buttonPressed((int)event.jbutton.button);
 			switch( event.jbutton.button ) {
 				case PS3_DRUM_CONTROLLER_RED: // Snare drum
 					audio.playSample(getDataPath("sounds/drum_snare.ogg"));
@@ -90,7 +73,6 @@ void check_joystick_event(SDL_Event event, Audio &audio) {
 			}
 			break;
 		case SDL_JOYBUTTONUP:
-			joysticks[(int)event.jbutton.which].buttonReleased((int)event.jbutton.button);
 			break;
 	}
 }
