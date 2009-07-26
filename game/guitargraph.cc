1e4a04316dcb0d3f7554d2bfdc58160034a862e3
Lasse Karkkainen <tronic+ndrm@trn.iki.fi>
2009-07-26 14:38:17 +0300

    Rip out Chord stuff, make difficulty level selection better.

--- game/guitargraph.cc
+++ game/guitargraph.cc
@@ -2,74 +2,15 @@
 
 #include "joystick.hh"
 
-Chord::Chord(int frt,double begin){
-  this->fret=frt;
-  this->start=begin;
-  this->state=COMING;
-  this->dt=0.075;
-}
-Chord::Chord(const Chord &c){
-  this->fret=c.fret;
-  this->event=c.event;
-  this->start=c.start;
-  this->end=c.end;
-  this->dt=c.dt;
-  this->state=c.state;
-}
-
-Chord::~Chord(){}
-Chord &Chord::operator=(const Chord &c){
-  if(&c!=this){
-    this->fret=c.fret;
-    this->event=c.event;
-    this->start=c.start;
-    this->end=c.end;
-    this->dt=c.dt;
-    this->state=c.state;
-  }
-  return *this;
-}
-
-unsigned char Chord::getEvent(){
-  return this->event;
-}
-
-int Chord::getFret(){
-  return this->fret;
-}
-
-void Chord::setEnd(double e){
-
-  this->end=e;
-}
-
-bool Chord::isBetween(double time){
-
-  return (time>this->start&&time<this->end);
-}
-
-Chord::ChordState Chord::getState(){
-  
-  return this->state;
-}
-
-void Chord::setState(Chord::ChordState s){
-  
-  this->state=s;
-}
-
-bool Chord::pressNow(double t){
-
-  return (t>(this->start-this->dt)&&t<(this->start+dt));
-}
-
-double Chord::getDt(){
-
-  return this->dt;
-}
-
-
 namespace {
+	struct Diff { std::string name; int basepitch; } diffv[] = {
+		{ "Supaeasy", 0x3C },
+		{ "Easy", 0x48 },
+		{ "Medium", 0x54 },
+		{ "Amazing", 0x60 }
+	};
+	const size_t diffsz = sizeof(diffv) / sizeof(*diffv);
+
 	const float past = -0.3f;
 	const float future = 3.0f;
 	const float timescale = 60.0f;
@@ -82,10 +23,12 @@ namespace {
 }
 
 GuitarGraph::GuitarGraph(Song const& song): m_song(song), m_button("button.svg"), m_pickValue(0.0, 5.0), m_instrument(), m_level(), m_text(getThemePath("sing_timetxt.svg"), config["graphic/text_lod"].f()) {
-	if (m_song.tracks.empty()) throw std::runtime_error("No tracks");
+	std::size_t tracks = m_song.tracks.size();
+	if (tracks == 0) throw std::runtime_error("No tracks");
 	m_necks.push_back(new Texture("guitarneck.svg"));
-	m_necks.push_back(new Texture("bassneck.svg"));
-	m_necks.push_back(new Texture("bassneck.svg")); // Drums
+	if (tracks > 1) m_necks.push_back(new Texture("bassneck.svg"));
+	if (tracks > 2) m_necks.push_back(new Texture("bassneck.svg")); // Drums
+	difficultyAuto();
 }
 
 void GuitarGraph::inputProcess() {
@@ -108,35 +51,26 @@ void GuitarGraph::inputProcess() {
 void GuitarGraph::engine(double time) {
 	if (picked && time < -0.5) {
 		if (fretPressed[0]) difficulty(DIFFICULTY_SUPAEASY);
-		if (fretPressed[1]) difficulty(DIFFICULTY_EASY);
-		if (fretPressed[2]) difficulty(DIFFICULTY_MEDIUM);
-		if (fretPressed[3]) difficulty(DIFFICULTY_AMAZING);
-		if (fretPressed[4]) m_instrument = (m_instrument + 1) % m_song.tracks.size();
+		else if (fretPressed[1]) difficulty(DIFFICULTY_EASY);
+		else if (fretPressed[2]) difficulty(DIFFICULTY_MEDIUM);
+		else if (fretPressed[3]) difficulty(DIFFICULTY_AMAZING);
+		if (fretPressed[4]) { m_instrument = (m_instrument + 1) % m_song.tracks.size(); difficultyAuto(); }
 	}
 	if (picked) { m_pickValue.setValue(1.0); }
 	picked = false;
 }
 
+void GuitarGraph::difficultyAuto() {
+	for (int level = 0; level < DIFFICULTYCOUNT; ++level) if (difficulty(Difficulty(level))) return;
+	throw std::runtime_error("No difficulty levels found");
+}
+
 bool GuitarGraph::difficulty(Difficulty level) {
-	Chords c;
-	uint8_t basepitch;
-	switch (level) {
-		case DIFFICULTY_SUPAEASY: basepitch = 0x3c; break;
-		case DIFFICULTY_EASY: basepitch = 0x48; break;
-		case DIFFICULTY_MEDIUM: basepitch = 0x54; break;
-		case DIFFICULTY_AMAZING: basepitch = 0x60; break;
-		default: throw std::logic_error("Invalid difficulty level");
-	}
+	uint8_t basepitch = diffv[level].basepitch;
 	NoteMap const& nm = m_song.tracks[m_instrument].nm;
-	NoteMap::const_iterator nmIt[5];
 	int fail = 0;
-	for (int fret = 0; fret < 5; ++fret) {
-		nmIt[fret] = nm.find(basepitch + fret);
-		if (nmIt[fret] == nm.end()) ++fail;
-	}
+	for (int fret = 0; fret < 5; ++fret) if (nm.find(basepitch + fret) == nm.end()) ++fail;
 	if (fail == 5) return false;
-	//std::sort(c.begin(), c.end(), chordlt);
-	m_chords = c;
 	m_level = level;
 	return true;
 }
@@ -145,14 +79,7 @@ void GuitarGraph::draw(double time) {
 	m_text.dimensions.screenBottom(-0.05).middle(0.0);
 	if (time < -0.5) {
 		std::string txt = "Play a fret to change:\n";
-		txt += m_song.tracks[m_instrument].name + "/";
-		switch (m_level) {
-			case DIFFICULTY_SUPAEASY: txt += "Supaeasy"; break;
-			case DIFFICULTY_EASY: txt += "Easy"; break;
-			case DIFFICULTY_MEDIUM: txt += "Medium"; break;
-			case DIFFICULTY_AMAZING: txt += "Amazing"; break;
-			default: throw std::logic_error("Invalid difficulty level");
-		}
+		txt += m_song.tracks[m_instrument].name + "/" + diffv[m_level].name;
 		m_text.draw(txt);
 	}
 	engine(time);
@@ -181,15 +108,7 @@ void GuitarGraph::draw(double time) {
 			glTexCoord2f(1.0f, texCoord); glVertex2f(2.5f, time2y(tEnd));
 		}
 	}
-	int basepitch;
-
-	switch (m_level) {
-		case DIFFICULTY_SUPAEASY: basepitch = 0x3c; break;
-		case DIFFICULTY_EASY: basepitch = 0x48; break;
-		case DIFFICULTY_MEDIUM: basepitch = 0x54; break;
-		case DIFFICULTY_AMAZING: basepitch = 0x60; break;
-		default: throw std::logic_error("Invalid difficulty level");
-	}
+	int basepitch = diffv[m_level].basepitch;
 
 	static glutil::Color fretColors[5] = {
 		glutil::Color(0.0f, 1.0f, 0.0f),
