c3216e9310fb5ac3d6cf56d1addd869e88a0a986
Vincent Le Ligeour <yoda-jm@users.sourceforge.net>
2009-08-29 11:02:22 +0200

    Played a little bit more with ffmpeg

--- editor/main.cc
+++ editor/main.cc
@@ -6,14 +6,13 @@ int main(int argc, char **argv) {
 	FFmpeg mpeg(false, true, argv[1], 48000);
 	int64_t position = 0;
 
-	da::sample_t *sample = new da::sample_t(2*4800);
+	da::sample_t *sample = new da::sample_t(1500);
 	da::pcm_data data(sample, 1500, 2, 48000);
 
 	while( mpeg.audioQueue(data, position) ) {
-		std::cout << "Position: " << position << std::endl;
-		position += (data.frames * data.channels);
+		std::cout << "Position: " << position << " " << double(position) / double(48000*2)<< std::endl;
 	}
 
-	std::cout << "Total: " << mpeg.audioQueue.duration() << std::endl;
+	std::cout << "Duration: " << mpeg.audioQueue.duration() << std::endl;
 	return EXIT_SUCCESS;
 }
