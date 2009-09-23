18d180d859afd68c610a11483c52c892039f7b0a
Vincent Le Ligeour <yoda-jm@users.sourceforge.net>
2009-09-23 15:36:37 +0200

    Fixed the fix

--- cmake/Modules/FindSDL.cmake
+++ cmake/Modules/FindSDL.cmake
@@ -29,6 +29,7 @@ find_library(SDL_SDL_LIBRARY
 # Process others than OSX with native SDL normally
 if(NOT "${SDL_SDL_LIBRARY}" MATCHES "framework")
   if(MINGW)
+    set(MINGW32_LIBRARY mingw)
     set(SDL_PROCESS_LIBS ${SDL_PROCESS_LIBS} MINGW32_LIBRARY)
   endif(MINGW)
   find_library(SDL_SDLmain_LIBRARY
