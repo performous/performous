7aa4e54ed2879196d251e662aa68390249fd3fc7
Jaakko Nikkola <jaakko.nikkola@tkk.fi>
2009-11-12 18:19:25 +0200

    minor fix to DanceTrack constructor

--- game/notes.cc
+++ game/notes.cc
@@ -78,7 +78,7 @@ double Note::scoreMultiplier(double error) const {
 
 Duration::Duration(): begin(getNaN()), end(getNaN()) {}
 
-DanceData::DanceData(std::string d, Chords c) : description(d), chords(c) {}
+DanceTrack::DanceTrack(std::string d, Chords c) : description(d), chords(c) {}
 
 
 
