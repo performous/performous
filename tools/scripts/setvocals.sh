c1c0be469583f902a21fb5802c7f55e04514cfc0
Miguel Sánchez de León Peque <peque_msl@hotmail.com>
2010-01-07 04:46:38 +0100

    setvocals.sh script can be used to set the vocals file in Rock Band and Guitar Hero songs

---
+++ tools/scripts/setvocals.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+# this script guess which is the vocals track of a song and name it "vocals.ogg"
+# usage ./setvocals.sh [songdir]
+
+cd "$1"
+
+function setvocals_rockband {
+	if [ -f "song.ogg" -a -f "drums.ogg" -a -f "rhythm.ogg" -a -f "guitar.ogg" ];
+	then mv song.ogg vocals.ogg;
+	fi
+}
+
+function setvocals_guitarhero {
+	if [ -f "song.ogg" -a -f "drumsFoFiX.ogg" -a -f "songFoFiX.ogg" -a -f "rhythm.ogg" -a -f "guitar.ogg" ];
+	then mv songFoFiX.ogg vocals.ogg;
+	fi
+}
+
+setvocals_rockband
+setvocals_guitarhero
