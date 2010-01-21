6fbe91b2cbbb3b2d78fa1dd5873d1a069de29019
Lasse Karkkainen <tronic+ndrm@trn.iki.fi>
2010-01-21 10:04:09 +0200

    Rewritten pa19 driver (it compiles, ship it -- not tested at all)

--- libs/libda/plugins/audio_dev_pa19.cpp
+++ libs/libda/plugins/audio_dev_pa19.cpp
@@ -1,117 +1,61 @@
+#include "portaudio.hh"
 #include <libda/audio_dev.hpp>
-#include <stdint.h>
-#include <portaudio.h>
 #include <ostream>
 #include <sstream>
 
 namespace {
 	using namespace da;
 	class pa19_record: public record::dev {
-		static int c_callback(const void* input, void*, unsigned long frames, const PaStreamCallbackTimeInfo*, PaStreamCallbackFlags, void* userdata)
-		{
-			pa19_record& self = *static_cast<pa19_record*>(userdata);
-			int16_t const* iptr = static_cast<int16_t const*>(input);
-			try {
-				std::vector<sample_t> buf(frames * self.s.channels());
-				std::transform(iptr, iptr + buf.size(), buf.begin(), conv_from_s16);
-				pcm_data data(&buf[0], frames, self.s.channels(), self.s.rate());
-				self.s.callback()(data);
-			} catch (std::exception& e) {
-				self.s.debug(std::string("Exception from recording callback: ") + e.what());
-			}
-			return paContinue;
-		}
 		settings s;
-		struct init {
-			init() {
-				PaError err = Pa_Initialize();
-				if( err != paNoError ) throw std::runtime_error(std::string("Cannot initialize PortAudio: ") + Pa_GetErrorText(err));
-			}
-			~init() { /* FIXME: This causes segfault soon after the call: Pa_Terminate(); */ }
-		} initialize;
-		struct strm {
-			PaStream* handle;
-			strm( pa19_record* rec) {
-				PaStreamParameters p;
-				if (rec->s.subdev().empty()) {
-					p.device = Pa_GetDefaultInputDevice();
-					if (p.device < 0) throw std::runtime_error("No PortAudio devices available");
-				} else {
-					std::istringstream iss(rec->s.subdev());
-					iss >> p.device;
-					if (!iss.eof() || p.device < 0 || p.device > Pa_GetDeviceCount() - 1) throw std::invalid_argument("Invalid PortAudio device number");
-				}
-				p.channelCount = rec->s.channels();
-				p.sampleFormat = paInt16;
-				if (rec->s.frames() != settings::high) p.suggestedLatency = Pa_GetDeviceInfo(p.device)->defaultLowInputLatency;
-				else p.suggestedLatency = Pa_GetDeviceInfo(p.device)->defaultHighInputLatency;
-				p.hostApiSpecificStreamInfo = NULL;
-				PaError err = Pa_OpenStream(&handle, &p, NULL, rec->s.rate(), 50, paClipOff, rec->c_callback, rec);
-				if (err != paNoError) throw std::runtime_error("Cannot open PortAudio audio stream " + rec->s.subdev() + ": " + Pa_GetErrorText(err));
-			}
-			~strm() { Pa_CloseStream(handle); }
-		} stream;
+		portaudio::Init init;
+		portaudio::Stream stream;
 	  public:
-		pa19_record(settings& s_orig): s(s_orig), initialize(), stream(this) {
-			PaError err = Pa_StartStream(stream.handle);
+		pa19_record(settings& s_orig):
+		  s(s_orig),
+		  stream(*this, portaudio::Params().channelCount(s.channels()).device(s.subdev()), portaudio::Params(), s.rate())
+		{
+			PaError err = Pa_StartStream(stream);
 			if( err != paNoError ) throw std::runtime_error("Cannot start PortAudio audio stream " + s.subdev() + ": " + Pa_GetErrorText(err));
 			s_orig = s;
+		}
+		int operator()(void const* input, void*, unsigned long frames, const PaStreamCallbackTimeInfo*, PaStreamCallbackFlags)
+		{
+			try {
+				pcm_data data((da::sample_t*)(input), frames, s.channels(), s.rate());
+				s.callback()(data);
+			} catch (std::exception& e) {
+				s.debug(std::string("Exception from recording callback: ") + e.what());
+			}
+			return paContinue;
 		}
 	};
 	plugin::simple<record_plugin, pa19_record> r(devinfo("pa19", "PortAudio v19 PCM capture. Device number as settings (otherwise PA default)."));
 
 	class pa19_playback: public playback::dev {
-		static int c_callback(const void*, void* output, unsigned long frames, const PaStreamCallbackTimeInfo*,PaStreamCallbackFlags, void* userdata)
-		{
-			pa19_playback& self = *static_cast<pa19_playback*>(userdata);
-			int16_t* iptr = static_cast<int16_t*>(output);
-			for(unsigned int i = 0 ; i < frames * self.s.channels() ; i++)
-				iptr[i] = 0;
-			try {
-				std::vector<sample_t> buf(frames * self.s.channels());
-				pcm_data data(&buf[0], frames, self.s.channels(), self.s.rate());
-				self.s.callback()(data);
-				for( unsigned int i = 0 ; i < frames*self.s.channels() ; i++ )
-					iptr[i] = da::conv_to_s16(data[i]);
-			} catch (std::exception& e) {
-				self.s.debug(std::string("Exception from playback callback: ") + e.what());
-			}
-			return 0;
-		}
 		settings s;
-		struct init {
-			init() {
-				PaError err = Pa_Initialize();
-				if( err != paNoError ) throw std::runtime_error(std::string("Cannot initialize PortAudio: ") + Pa_GetErrorText(err));
-			}
-			~init() { /* FIXME: This causes segfault soon after the call: Pa_Terminate(); */ }
-		} initialize;
-		struct strm {
-			PaStream* handle;
-			strm(pa19_playback* pb) {
-				PaStreamParameters p;
-				if (pb->s.subdev().empty()) {
-					p.device = Pa_GetDefaultOutputDevice();
-					if (p.device < 0) throw std::runtime_error("No PortAudio devices available");
-				} else {
-					std::istringstream iss(pb->s.subdev());
-					iss >> p.device;
-					if (!iss.eof() || p.device < 0 || p.device > Pa_GetDeviceCount() - 1) throw std::invalid_argument("Invalid PortAudio device number");
-				}
-				p.channelCount = pb->s.channels();
-				p.sampleFormat = paInt16;
-				if (pb->s.frames() != settings::high) p.suggestedLatency = Pa_GetDeviceInfo(p.device)->defaultLowOutputLatency;
-				else p.suggestedLatency = Pa_GetDeviceInfo(p.device)->defaultHighInputLatency;
-				p.hostApiSpecificStreamInfo = NULL;
-				PaError err = Pa_OpenStream(&handle, NULL, &p, pb->s.rate(), 100, paClipOff, pb->c_callback, pb);
-				if (err != paNoError) throw std::runtime_error("Cannot open PortAudio audio stream " + pb->s.subdev() + ": " + Pa_GetErrorText(err));
-			}
-			~strm() { Pa_CloseStream(handle); }
-		} stream;
+		portaudio::Init init;
+		portaudio::Stream stream;
 	  public:
-		pa19_playback(settings& _s): s(_s), initialize(), stream(this) {
-			PaError err = Pa_StartStream(stream.handle);
+		pa19_playback(settings& s_orig):
+		  s(s_orig),
+		  stream(*this, portaudio::Params().channelCount(s.channels()).device(s.subdev()), portaudio::Params(), s.rate())
+		{
+			PaError err = Pa_StartStream(stream);
 			if( err != paNoError ) throw std::runtime_error("Cannot start PortAudio audio stream " + s.subdev() + ": " + Pa_GetErrorText(err));
+			s_orig = s;
+		}
+		int operator()(void const*, void* output, unsigned long frames, const PaStreamCallbackTimeInfo*, PaStreamCallbackFlags)
+		{
+			float* iptr = static_cast<da::sample_t*>(output);
+			unsigned int samples = frames * s.channels();
+			for (unsigned int i = 0; i < samples; i++) iptr[i] = 0;
+			try {
+				pcm_data data(iptr, frames, s.channels(), s.rate());
+				s.callback()(data);
+			} catch (std::exception& e) {
+				s.debug(std::string("Exception from playback callback: ") + e.what());
+			}
+			return paContinue;
 		}
 	};
 	plugin::simple<playback_plugin, pa19_playback> p(devinfo("pa19", "PortAudio v19 PCM playback. Settings are not used."));
---
+++ libs/libda/plugins/portaudio.hh
@@ -0,0 +1,82 @@
+#include <portaudio.h>
+#include <cstdlib>
+#include <stdexcept>
+#include <stdint.h>
+
+#define PORTAUDIO_CHECKED(func, args) portaudio::internal::check(func args, #func)
+
+namespace portaudio {
+	class Error: public std::runtime_error {
+	public:
+		Error(PaError code_, char const* func_):
+		  runtime_error(std::string(func_) + " failed: " + Pa_GetErrorText(code_)),
+		  m_code(code_),
+		  m_func(func_)
+		{}
+		PaError code() const { return m_code; }
+		std::string func() const { return m_func; }
+	private:
+		PaError m_code;
+		char const* m_func;
+	};
+	
+	namespace internal {
+		void check(PaError code, char const* func) { if (code != paNoError) throw Error(code, func); }
+	}
+
+	struct Init {
+		Init() { PORTAUDIO_CHECKED(Pa_Initialize, ()); }
+		~Init() { Pa_Terminate(); }
+	};
+
+	struct Params {
+		PaStreamParameters params;
+		Params(PaStreamParameters const& init = PaStreamParameters()): params(init) {
+			sampleFormat(paFloat32);
+		}
+		Params& channelCount(int val) { params.channelCount = val; return *this; }
+		Params& device(PaDeviceIndex val) { params.device = val; return *this; }
+		Params& device(std::string const& name) {
+			device(name.empty() ? 0 : std::atoi(name.c_str())); // TODO: Name matching and error handling
+			return *this;
+		}
+		Params& sampleFormat(PaSampleFormat val) { params.sampleFormat = val; return *this; }
+		Params& suggestedLatency(PaTime val) { params.suggestedLatency = val; return *this; }
+		// Params& hostAPISpecificStreamInfo(void* val) { params.hostAPISpecificStreamInfo = val; return *this; }
+		operator PaStreamParameters const*() const { return &params; }
+	};
+		
+	template <typename Functor> int functorCallback(void const* input, void* output, unsigned long frameCount, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void* userData) {
+		return (*static_cast<Functor*>(userData))(input, output, frameCount, timeInfo, statusFlags);
+	}
+
+	class Stream {
+		PaStream* m_handle;
+	public:
+		/*Stream(
+		  PaStreamParameters const* input,
+		  PaStreamParameters const* output,
+		  double sampleRate,
+		  unsigned long framesPerBuffer = paFramesPerBufferUnspecified,
+		  PaStreamFlags flags = paNoFlag,
+		  PaStreamCallback* callback = NULL,
+		  void* userData = NULL)
+		{
+			PORTAUDIO_CHECKED(Pa_OpenStream, (&m_handle, input, output, sampleRate, framesPerBuffer, flags, callback, userData));
+		}*/
+		template <typename Functor> Stream(
+		  Functor& functor,
+		  PaStreamParameters const* input,
+		  PaStreamParameters const* output,
+		  double sampleRate,
+		  unsigned long framesPerBuffer = paFramesPerBufferUnspecified,
+		  PaStreamFlags flags = paNoFlag)
+		{
+			PORTAUDIO_CHECKED(Pa_OpenStream, (&m_handle, input, output, sampleRate, framesPerBuffer, flags, functorCallback<Functor>, &functor));
+		}
+		~Stream() { Pa_CloseStream(m_handle); }
+		operator PaStream*() { return m_handle; }
+	};
+
+}
+
