0c2c6f693dd3c0141ed05066f6079e7a4961305e
Vincent Le Ligeour <yoda-jm@users.sourceforge.net>
2010-03-30 23:53:07 +0200

    Fixed some UltraStar files misdetected as Stepmania files (reported by duke42)

--- game/songparser-sm.cc
+++ game/songparser-sm.cc
@@ -40,8 +40,10 @@ namespace {
 /// 'Magick' to check if this file looks like correct format
 bool SongParser::smCheck(std::vector<char> const& data) {
 	if (data[0] != '#' || data[1] < 'A' || data[1] > 'Z') return false;
-	for (std::vector<char>::const_iterator it = data.begin(); it != data.end(); ++it)
-		if (*it == ';') return true;
+	for (std::vector<char>::const_iterator it = data.begin(); it != data.end(); ++it){
+		if (*it == '\n') return false;
+		else if (*it == ';') return true;
+	}
 	return false;
 }
 
