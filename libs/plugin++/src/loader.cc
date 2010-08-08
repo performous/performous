c248ca972d5565c50ce2626671a6d377b59ce328
Tapio Vierros <tapiovierros@gmail.com>
2010-08-08 23:18:12 +0300

    Whitespace.

--- libs/plugin++/src/loader.cc
+++ libs/plugin++/src/loader.cc
@@ -23,33 +23,33 @@ namespace fs = boost::filesystem;
 
 /// Break var (a delimited path string) into paths (appends folder if supplied).
 void loader::parse(std::set<fs::path>& paths, char const* var, fs::path const& folder) {
-    if (!var) return;
-    std::istringstream iss(var);
-    std::string elem;
-    while (std::getline(iss, elem, PATHSEP)) {
-        fs::path p = elem;
-        if (!folder.empty()) p /= folder;
-        if (fs::is_directory(p)) paths.insert(p);
-    }
+	if (!var) return;
+	std::istringstream iss(var);
+	std::string elem;
+	while (std::getline(iss, elem, PATHSEP)) {
+		fs::path p = elem;
+		if (!folder.empty()) p /= folder;
+		if (fs::is_directory(p)) paths.insert(p);
+	}
 }
 
 /// Try to load all libraries in the given folder.
 void loader::load(fs::path const& path) {
-    for (fs::directory_iterator it(path), end; it != end; ++it) {
-        try {
-            dlls.push_back(new dll(it->string()));
-        } catch (std::runtime_error const& e) {
-            std::cerr << e.what() << std::endl;
-        }
-    }
+	for (fs::directory_iterator it(path), end; it != end; ++it) {
+		try {
+			dlls.push_back(new dll(it->string()));
+		} catch (std::runtime_error const& e) {
+			std::cerr << e.what() << std::endl;
+		}
+	}
 }
 
 loader::loader(fs::path const& folder) {
-    std::set<fs::path> paths;
-    // Try PLUGIN_PATH environment setting
-    parse(paths, std::getenv("PLUGIN_PATH"));
-    // Try other options only if folder was given
-    if (!folder.empty()) {
+	std::set<fs::path> paths;
+	// Try PLUGIN_PATH environment setting
+	parse(paths, std::getenv("PLUGIN_PATH"));
+	// Try other options only if folder was given
+	if (!folder.empty()) {
 		// Try relative path from executable
 		fs::path p = execname();
 		p = p.parent_path().parent_path();
@@ -58,17 +58,17 @@ loader::loader(fs::path const& folder) {
 			if (fs::is_directory(p)) paths.insert(p);
 		}
 #ifndef _WIN32
-	    // Try UNIX library paths
-        parse(paths, std::getenv("LD_LIBRARY_PATH"), folder);
-        parse(paths, UNIX_LIBPATH, folder);
+		// Try UNIX library paths
+		parse(paths, std::getenv("LD_LIBRARY_PATH"), folder);
+		parse(paths, UNIX_LIBPATH, folder);
 #endif
-    }
-    // Load the contents of each folder
-    for (std::set<fs::path>::const_iterator it = paths.begin(); it != paths.end(); ++it) load(*it);
+	}
+	// Load the contents of each folder
+	for (std::set<fs::path>::const_iterator it = paths.begin(); it != paths.end(); ++it) load(*it);
 #ifndef _WIN32
 	// HACK: ifdeffing this message out since it would be consfusing on Windows,
 	// where PortAudio is built into libda (and thus no plugins used).
-    if (dlls.empty()) std::cerr << "No plugins found. Try setting PLUGIN_PATH to the folder with the plugins." << std::endl;
+	if (dlls.empty()) std::cerr << "No plugins found. Try setting PLUGIN_PATH to the folder with the plugins." << std::endl;
 #endif
 }
 
