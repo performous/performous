5fc70148025807ed00ac145711cfe5952ded7a91
Tapio Vierros <tapiovierros@gmail.com>
2010-08-09 18:57:53 +0300

    Return 1 when exiting main() with fatal error.

--- game/main.cc
+++ game/main.cc
@@ -428,6 +428,7 @@ int main(int argc, char** argv) try {
 	return 0; // Do not remove. SDL_Main (which this function is called on some platforms) needs return statement.
 } catch (std::exception& e) {
 	std::cerr << "FATAL ERROR: " << e.what() << std::endl;
+	return 1;
 }
 
 void outputOptionalFeatureStatus() {
