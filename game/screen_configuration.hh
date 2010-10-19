b9b699fa39d0a9ab0f999a58c4b4d2850ab42b5a
Tapio Vierros <tapiovierros@gmail.com>
2010-10-19 17:31:33 +0300

    Add back dummy screen_configuration.* as a work-around for ppa troubles.

---
+++ game/screen_configuration.hh
@@ -0,0 +1,4 @@
+// Dummy file
+// The PPA deb packaging diff doesn't seem to know how to delete files,
+// so this is here as a work-around to avoid compilation errors.
+// If someone knows how to fix this, the ppa script is tools/ppa/ppa.sh
