cfef2bba36d8eb4626cc5adb3e185d26ecfc0d9f
Vincent Le Ligeour <yoda-jm@users.sourceforge.net>
2010-10-24 18:54:32 +0200

    Fixed score coloring

--- game/screen_sing.cc
+++ game/screen_sing.cc
@@ -654,7 +654,7 @@ void ScoreWindow::draw() {
 
 	for (Database::cur_scores_t::const_iterator p = m_database.scores.begin(); p != m_database.scores.end(); ++p, ++i) {
 		int score = p->score;
-		glutil::Color(p->color);
+		glutil::Color c(p->color);
 		double x = -0.12 + spacing * (0.5 + i - 0.5 * m_database.scores.size());
 		m_scoreBar.dimensions.middle(x).bottom(0.20);
 		m_scoreBar.draw(score / 10000.0);
