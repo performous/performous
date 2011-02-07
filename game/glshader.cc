performous/performous

commit a136b2ee85144bed84ee983860dce0ba3b398022

Better diagnostics for shader compile errors.

game/glshader.cc
@@ -62,7 +62,11 @@ Shader& Shader::compileFile(std::string const& filename, std::string const& defi
 		if (pos == std::string::npos) throw std::runtime_error("Shader " + filename + " missing //DEFINES (needed for injected code)");
 		srccode = srccode.substr(0, pos) + defines + srccode.substr(pos + 9);
 	}
-	return compileCode(srccode, type);
+	try {
+		compileCode(srccode, type);
+	} catch (std::runtime_error& e) {
+		throw std::runtime_error(filename + ": " + e.what());
+	}
 }
 
 
@@ -75,10 +79,11 @@ Shader& Shader::compileCode(std::string const& srccode, GLenum type) {
 	ec.check("glShaderSource");
 
 	glCompileShader(new_shader);
+	ec.check("glCompileShader");
 	glGetShaderiv(new_shader, GL_COMPILE_STATUS, &gl_response);
 	if (gl_response != GL_TRUE) {
 		dumpInfoLog(new_shader);
-		throw std::runtime_error("Something went wrong compiling the shader.");
+		throw std::runtime_error("Shader compile error");
 	}
 
 	shader_ids.push_back(new_shader);

commit 7ecee918b1e0318d10859d5f27c9a0a49c0bdecf

Fixed segfault introduced in a136b2ee85144bed84ee983860dce0ba3b398022

game/glshader.cc
@@ -63,7 +63,7 @@ Shader& Shader::compileFile(std::string const& filename, std::string const& defi
 		srccode = srccode.substr(0, pos) + defines + srccode.substr(pos + 9);
 	}
 	try {
-		compileCode(srccode, type);
+		return compileCode(srccode, type);
 	} catch (std::runtime_error& e) {
 		throw std::runtime_error(filename + ": " + e.what());
 	}