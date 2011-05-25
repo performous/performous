performous/performous

commit c0e78b1f19ed2b25c3c37d4cf610939ef33abcab

More precise timecode handling and more tolerance before seeking (workaround for infinite seekloops).

game/ffmpeg.hh
@@ -162,7 +162,7 @@ class AudioBuffer {
 	void setDuration(double seconds) { m_duration = seconds; }
 	bool wantSeek() {
 		size_t oldest = m_pos - m_data.size();
-		return oldest > 0 && m_posReq < int64_t(oldest);
+		return m_posReq + int64_t(m_sps) * 2.0 /* seconds tolerance */ < int64_t(oldest);
 	}
   private:
 	bool wantMore() { return int64_t(m_pos) - int64_t(m_data.capacity() / 2) < m_posReq; }

game/ffmpeg.cc
@@ -280,11 +280,12 @@ void FFmpeg::decodeNextFrame() {
 				std::vector<int16_t> resampled(AVCODEC_MAX_AUDIO_FRAME_SIZE);
 				int frames = audio_resample(pResampleCtx, &resampled[0], audioFrames, outsize);
 				resampled.resize(frames * AUDIO_CHANNELS);
-				// Calculate new positions
+				// Use timecode from packet if available
 				if (packet.time() == packet.time()) m_position = packet.time();
-				else m_position += double(resampled.size())/double(audioQueue.getSamplesPerSecond());
 				// Push to output queue (may block)
 				audioQueue.push(resampled, m_position);
+				// Increment current time
+				m_position += double(resampled.size())/double(audioQueue.getSamplesPerSecond());
 			}
 			// Audio frames are always finished
 			frameFinished = 1;

commit 9cf2d2fe6cd7991fb3516f16cfb74f23679320f8

An attempt at fixing the regressions I introduced in commit c0e78b1f19ed2b25c3c37d4cf610939ef33abcab. Not tested.

game/ffmpeg.hh
@@ -162,7 +162,7 @@ class AudioBuffer {
 	void setDuration(double seconds) { m_duration = seconds; }
 	bool wantSeek() {
 		size_t oldest = m_pos - m_data.size();
-		return m_posReq + int64_t(m_sps) * 2.0 /* seconds tolerance */ < int64_t(oldest);
+		return m_posReq > 0 && m_posReq + int64_t(m_sps) * 2.0 /* seconds tolerance */ < int64_t(oldest);
 	}
   private:
 	bool wantMore() { return int64_t(m_pos) - int64_t(m_data.capacity() / 2) < m_posReq; }