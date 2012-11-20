performous/performous

commit 23ef38a5da034397aa853e6b22544f01bda80666

Nice background for songs without one in song browser.
Tile cover images.

game/screen_songs.cc
@@ -197,8 +197,27 @@ void ScreenSongs::drawMultimedia() {
 		Transform ft(farTransform());  // 3D effect
 		double length = m_audio.getLength();
 		double time = clamp(m_audio.getPosition() - config["audio/video_delay"].f(), 0.0, length);
+		Song& song = m_songs.current();
 		m_songbg_default->draw();   // Default bg
 		if (m_songbg.get()) m_songbg->draw();
+		else if (!song.cover.empty()) {
+			// Create a background image by tiling covers
+			try {
+				Surface& cover = m_covers[song.path + song.cover];
+				Dimensions backup = cover.dimensions;
+				const float s = 0.3;
+				cover.dimensions.fixedWidth(s).screenTop(0.0);
+				cover.dimensions.top(0.0).right(-s); cover.draw();
+				cover.dimensions.top(0.0).right(0.0); cover.draw();
+				cover.dimensions.top(0.0).left(0.0); cover.draw();
+				cover.dimensions.top(0.0).left(s); cover.draw();
+				cover.dimensions.top(s).right(-s); cover.draw();
+				cover.dimensions.top(s).right(0.0); cover.draw();
+				cover.dimensions.top(s).left(0.0); cover.draw();
+				cover.dimensions.top(s).left(s); cover.draw();
+				cover.dimensions = backup;
+			} catch (std::exception const&) {}
+		}
 		if (m_video.get()) m_video->render(time);
 	}
 	if (!m_jukebox) {

commit 6a647abd478b8acd9dd37a1e0738b3696c738f27

Fix crash introduced by 23ef38a5.

game/screen_songs.cc
@@ -197,28 +197,30 @@ void ScreenSongs::drawMultimedia() {
 		Transform ft(farTransform());  // 3D effect
 		double length = m_audio.getLength();
 		double time = clamp(m_audio.getPosition() - config["audio/video_delay"].f(), 0.0, length);
-		Song& song = m_songs.current();
 		m_songbg_default->draw();   // Default bg
-		if (m_songbg.get()) m_songbg->draw();
-		else if (!song.cover.empty()) {
-			// Create a background image by tiling covers
-			try {
-				Surface& cover = m_covers[song.path + song.cover];
-				Dimensions backup = cover.dimensions;
-				const float s = 0.3;
-				cover.dimensions.fixedWidth(s).screenTop(0.0);
-				cover.dimensions.top(0.0).right(-s); cover.draw();
-				cover.dimensions.top(0.0).right(0.0); cover.draw();
-				cover.dimensions.top(0.0).left(0.0); cover.draw();
-				cover.dimensions.top(0.0).left(s); cover.draw();
-				cover.dimensions.top(s).right(-s); cover.draw();
-				cover.dimensions.top(s).right(0.0); cover.draw();
-				cover.dimensions.top(s).left(0.0); cover.draw();
-				cover.dimensions.top(s).left(s); cover.draw();
-				cover.dimensions = backup;
-			} catch (std::exception const&) {}
+		if (!m_songs.empty()) {
+			Song& song = m_songs.current();
+			if (m_songbg.get()) m_songbg->draw();
+			else if (!song.cover.empty()) {
+				// Create a background image by tiling covers
+				try {
+					Surface& cover = m_covers[song.path + song.cover];
+					Dimensions backup = cover.dimensions;
+					const float s = 0.3;
+					cover.dimensions.fixedWidth(s).screenTop(0.0);
+					cover.dimensions.top(0.0).right(-s); cover.draw();
+					cover.dimensions.top(0.0).right(0.0); cover.draw();
+					cover.dimensions.top(0.0).left(0.0); cover.draw();
+					cover.dimensions.top(0.0).left(s); cover.draw();
+					cover.dimensions.top(s).right(-s); cover.draw();
+					cover.dimensions.top(s).right(0.0); cover.draw();
+					cover.dimensions.top(s).left(0.0); cover.draw();
+					cover.dimensions.top(s).left(s); cover.draw();
+					cover.dimensions = backup;
+				} catch (std::exception const&) {}
+			}
+			if (m_video.get()) m_video->render(time);
 		}
-		if (m_video.get()) m_video->render(time);
 	}
 	if (!m_jukebox) {
 		m_songbg_ground->draw();