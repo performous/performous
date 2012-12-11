a4014ee75f2ea78c93c82bb4441bb4677aa2011d
Lasse Karkkainen <tronic+ndrm@trn.iki.fi>
2012-12-11 21:33:46 +0200

    Remove commented out MIDI code.

--- game/controllers-midi.cc
+++ game/controllers-midi.cc
@@ -28,11 +28,6 @@ namespace input {
 				unsigned char note = ev.message >> 8;
 				unsigned char vel  = ev.message >> 16;
 				unsigned chan = (ev.message & 0x0F) + 1;  // It is conventional to use one-based indexing
-				/* Old drum-specific filtering is no longer used:
-				if (chan < 10 || chan > 11) continue; // only accept channels 10-11 (percussion in GM and secondary percussion in GM level 2)
-				if (evnt != 0x90) continue; // 0x90 = any channel note-on
-				if (vel  == 0x00) continue; // velocity 0 is often used instead of note-off
-				*/
 				if (evnt == 0x80 /* NOTE OFF */) { evnt = 0x90; vel = 0; }  // Translate NOTE OFF into NOTE ON with zero-velocity
 				if (evnt != 0x90 /* NOTE ON */) continue;  // Ignore anything that isn't NOTE ON/OFF
 				std::clog << "controller-midi/info: MIDI NOTE ON/OFF event: ch=" << unsigned(chan) << " note=" << unsigned(note) << " vel=" << unsigned(vel) << std::endl;
