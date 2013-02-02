performous/performous

commit da635890d22b18db56309c9d73f982dcadc10ee1

Add a separate pass for preparing a screen, for slow operations that we don't want to do in draw(), and use that for loading the next video frame.

game/video.cc
@@ -1,12 +1,11 @@
 #include "video.hh"
 
 #include "util.hh"
-
 #include <cmath>
 
 Video::Video(std::string const& _videoFile, double videoGap): m_mpeg(true, false, _videoFile), m_videoGap(videoGap), m_surfaceTime(), m_lastTime(), m_alpha(-0.5, 1.5) {}
 
-void Video::render(double time) {
+void Video::prepare(double time) {
 	time += m_videoGap;
 	VideoFrame& fr = m_videoFrame;
 	// Time to switch frame?
@@ -18,6 +17,17 @@ void Video::render(double time) {
 		m_surface.load(bitmap);
 		m_surfaceTime = fr.timestamp;
 	}
+	// Preload the next future frame
+	if (fr.data.empty()) while (m_mpeg.videoQueue.tryPop(fr) && fr.timestamp < time) {};
+	// Do a seek before next render, if required
+	if (time < m_lastTime - 1.0 || (!fr.data.empty() && time > fr.timestamp + 7.0)) {
+		m_mpeg.seek(std::max(0.0, time - 5.0));  // -5 to workaround ffmpeg inaccurate seeking
+		fr.data.clear();
+	}
+	m_lastTime = time;
+}
+
+void Video::render(double time) {
 	double tdist = std::abs(m_surfaceTime - time);
 	m_alpha.setTarget(tdist < 0.4 ? 1.2f : -0.5f);
 	float alpha = clamp(m_alpha.get());
@@ -33,13 +43,5 @@ void Video::render(double time) {
 			m_surface.draw();
 		}
 	}
-	// Preload the next future frame
-	if (fr.data.empty()) while (m_mpeg.videoQueue.tryPop(fr) && fr.timestamp < time) {};
-	// Do a seek before next render, if required
-	if (time < m_lastTime - 1.0 || (!fr.data.empty() && time > fr.timestamp + 7.0)) {
-		m_mpeg.seek(std::max(0.0, time - 5.0));  // -5 to workaround ffmpeg inaccurate seeking
-		fr.data.clear();
-	}
-	m_lastTime = time;
 }
 

game/main.cc
@@ -185,6 +185,7 @@ void mainLoop(std::string const& songlist) {
 				glFinish();
 				prof("swap");
 				updateSurfaces();
+				sm.prepareScreen();
 				glFinish();
 				prof("surfaces");
 				if (config["graphic/fps"].b()) {

game/screen.hh
@@ -19,6 +19,8 @@ class Screen {
 	virtual ~Screen() {}
 	/// eventhandler
 	virtual void manageEvent(SDL_Event event) = 0;
+	/// prepare screen for drawing
+	virtual void prepare() {}
 	/// draws screen
 	virtual void draw() = 0;
 	/// enters screen
@@ -49,6 +51,8 @@ class ScreenManager: public Singleton <ScreenManager> {
 	void activateScreen(std::string const& name);
 	/// Does actual switching of screens (if necessary)
 	void updateScreen();
+	/// Prepare (slow loading operations) of the current screen for rendering
+	void prepareScreen();
 	/// Draws the current screen and possible transition effects
 	void drawScreen();
 	/// Reload OpenGL resources (after fullscreen toggle etc)

game/screen_sing.cc
@@ -399,6 +399,11 @@ namespace {
 
 }
 
+void ScreenSing::prepare() {
+	double time = m_audio.getPosition();
+	if (m_video) m_video->prepare(time);
+}
+
 void ScreenSing::draw() {
 	// Get the time in the song
 	double length = m_audio.getLength();
@@ -432,7 +437,7 @@ void ScreenSing::draw() {
 		// Webcam
 		if (m_cam && config["graphic/webcam"].b()) m_cam->render();
 		// Video
-		if (m_video /* && (!m_cam || !m_cam->is_good()) */) {
+		if (m_video) {
 			m_video->render(time); double tmp = m_video->dimensions().ar(); if (tmp > 0.0) ar = tmp;
 		}
 		// Top/bottom borders

game/screen_sing.hh
@@ -55,6 +55,7 @@ class ScreenSing: public Screen {
 	void exit();
 	void reloadGL();
 	void manageEvent(SDL_Event event);
+	void prepare();
 	void draw();
 
 	void setSong (boost::shared_ptr<Song> song_)

game/screenmanager.cc
@@ -43,6 +43,10 @@ Screen* ScreenManager::getScreen(std::string const& name) {
 	}
 }
 
+void ScreenManager::prepareScreen() {
+	getCurrentScreen()->prepare();
+}
+
 void ScreenManager::drawScreen() {
 	getCurrentScreen()->draw();
 	drawLogo();

game/video.hh
@@ -10,8 +10,8 @@ class Video {
   public:
 	/// opens given video file
 	Video(std::string const& videoFile, double videoGap = 0.0);
-	/// renders video
-	void render(double time);
+	void prepare(double time);  ///< Load the current video frame into a texture
+	void render(double time);  ///< Render the prepared video frame
 	/// returns Dimensions of video clip
 	Dimensions& dimensions() { return m_surface.dimensions; }
 	/// returns Dimensions of video clip

commit 8211175f2b548552cd286b6ff7ea4685db76a44b

Fix bug in videoGap handling, introduced in da635890. Videos with several seconds of gap were not being displayed at all and others were displayed at wrong time.

game/video.cc
@@ -28,6 +28,7 @@ void Video::prepare(double time) {
 }
 
 void Video::render(double time) {
+	time += m_videoGap;
 	double tdist = std::abs(m_surfaceTime - time);
 	m_alpha.setTarget(tdist < 0.4 ? 1.2f : -0.5f);
 	double alpha = clamp(m_alpha.get());