559f71ef322f8063694d8f5539580782439155d7
Tapio Vierros <tapiovierros@gmail.com>
2013-04-29 19:46:02 +0300

    Still some whitespace.

--- game/screen_playlist.hh
+++ game/screen_playlist.hh
@@ -24,37 +24,37 @@ class ThemeInstrumentMenu;
 class ScreenPlaylist : public Screen
 {
 public:
-  typedef std::vector< boost::shared_ptr<Song> > SongList;
-  ScreenPlaylist(std::string const& name,Audio& audio, Songs& songs, Backgrounds& bgs);
-  void manageEvent(input::NavEvent const& event);
-  void manageEvent(SDL_Event);
-  void prepare();
-  void draw();
-  void enter();
-  void exit();
-  void reloadGL();
+	typedef std::vector< boost::shared_ptr<Song> > SongList;
+	ScreenPlaylist(std::string const& name, Audio& audio, Songs& songs, Backgrounds& bgs);
+	void manageEvent(input::NavEvent const& event);
+	void manageEvent(SDL_Event);
+	void prepare();
+	void draw();
+	void enter();
+	void exit();
+	void reloadGL();
 private:
-  bool keyPressed = false;
-  Menu overlay_menu;
-  Menu songlist_menu;
-  bool m_first;
-  AnimValue m_selAnim;
-  AnimValue m_submenuAnim;
-  Audio& m_audio;
-  Songs& m_songs;
-  void createSongListMenu();
-  void createEscMenu();
-  void createSongMenu(int songNumber);
-  void drawMenu();
-  void createMenuFromPlaylist();
-  Backgrounds& m_backgrounds;
-  Cachemap<std::string, Surface> m_covers;
-  boost::scoped_ptr<ThemeInstrumentMenu> m_menuTheme;
-  boost::scoped_ptr<ThemePlaylistScreen> theme;
-  boost::scoped_ptr<Surface> m_background;
-  SvgTxtTheme& getTextObject(std::string const& txt);
-  AnimValue m_nextTimer;
-  void draw_menu_options();
+	bool keyPressed = false;
+	Menu overlay_menu;
+	Menu songlist_menu;
+	bool m_first;
+	AnimValue m_selAnim;
+	AnimValue m_submenuAnim;
+	Audio& m_audio;
+	Songs& m_songs;
+	void createSongListMenu();
+	void createEscMenu();
+	void createSongMenu(int songNumber);
+	void drawMenu();
+	void createMenuFromPlaylist();
+	Backgrounds& m_backgrounds;
+	Cachemap<std::string, Surface> m_covers;
+	boost::scoped_ptr<ThemeInstrumentMenu> m_menuTheme;
+	boost::scoped_ptr<ThemePlaylistScreen> theme;
+	boost::scoped_ptr<Surface> m_background;
+	SvgTxtTheme& getTextObject(std::string const& txt);
+	AnimValue m_nextTimer;
+	void draw_menu_options();
 };
 
 
