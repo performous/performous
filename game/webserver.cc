87c7afca08d08f16fd811dcd64c8c422fec66770
Niek <nieknooijens@gmail.com>
2013-06-05 13:22:48 +0200

    created stub class for webserver and added library reference to cpp-
    download here:http://cpp-netlib.org
    you should dump the boost-folder inside /usr/include

---
+++ game/webserver.cc
@@ -0,0 +1,5 @@
+#include "webserver.hh"
+
+WebServer::WebServer()
+{
+}
---
+++ game/webserver.hh
@@ -0,0 +1,11 @@
+#pragma once
+#include <boost/network/protocol/http/server.hpp>
+
+class WebServer
+{
+public:
+	WebServer();
+private:
+
+};
+
