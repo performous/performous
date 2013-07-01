152789fe115c74f8f00ef0bb76110c0d8f406285
Niek <nieknooijens@gmail.com>
2013-07-01 17:32:11 +0200

    unblocked the mess, now runs in seperate thread

--- game/webserver.cc
+++ game/webserver.cc
@@ -1,12 +1,32 @@
 #include "webserver.hh"
+#include <boost/network/protocol/http/server.hpp>
+
 namespace http = boost::network::http;
 
-WebServer::WebServer() {
+struct handler;
+typedef http::server<handler> http_server;
+struct handler {
+	void operator() (http_server::request const &request,
+	http_server::response &response) {
+		response = http_server::response::stock_reply(
+		http_server::response::ok, "performous web-frontend!");
+	}
+
+	void log(http_server::string_type const &info) {
+		std::cerr << "ERROR: " << info << '\n';
+	}
+};
+
+
+void StartServer() {
 	handler handler_;
 	http_server::options options(handler_);
 	http_server server_(
 	options.address("127.0.0.1").port("8000"));
-	std::cout << "code hit!!" << std::endl;
 	server_.run();
-	std::cout << "server running!" << std::endl;
+}
+
+WebServer::WebServer()
+	:serverthread(&StartServer) {
+	serverthread.start_thread();
 }
