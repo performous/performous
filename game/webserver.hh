933258d8f43d87b71cc0af9bf211b8e4da172306
Niek <nieknooijens@gmail.com>
2013-07-01 17:32:11 +0200

    unblocked the mess, now runs in seperate thread

--- game/webserver.hh
+++ game/webserver.hh
@@ -1,28 +1,15 @@
 #pragma once
 #include <boost/network/protocol/http/server.hpp>
+#include <boost/thread/thread.hpp>
 #include <string>
 
-namespace http = boost::network::http;
-
-struct handler;
-typedef http::server<handler> http_server;
-struct handler {
-	void operator() (http_server::request const &request,
-	http_server::response &response) {
-		response = http_server::response::stock_reply(
-		http_server::response::ok, "Hello, world!");
-	}
-
-	void log(http_server::string_type const &info) {
-		std::cerr << "ERROR: " << info << '\n';
-	}
-};
-
+using boost::thread;
 
 class WebServer
 {
 public:
 	WebServer();
 private:
+	boost::thread serverthread;
 };
 
