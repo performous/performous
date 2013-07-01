6903774a156d2eb20f68db50c5c3d669b35862e4
Niek <nieknooijens@gmail.com>
2013-07-01 16:43:12 +0200

    yeah a running webserver!!!! is blocking though, need to push it on a seperate thread :-( so the program will freeze at the server initiasation, but you can open a browser and go to localhost:8000 and it should display a blank page containing "hello world". I used latest cpp-netlib from cpp-netlib.org (0.10) if you download the .tar.gz it should contain a folder called "boost" dum it in /usr/include and you're good to go.

--- game/webserver.cc
+++ game/webserver.cc
@@ -1,5 +1,12 @@
 #include "webserver.hh"
+namespace http = boost::network::http;
 
-WebServer::WebServer()
-{
+WebServer::WebServer() {
+	handler handler_;
+	http_server::options options(handler_);
+	http_server server_(
+	options.address("127.0.0.1").port("8000"));
+	std::cout << "code hit!!" << std::endl;
+	server_.run();
+	std::cout << "server running!" << std::endl;
 }
