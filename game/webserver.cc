376fad4d22b2667051e459ab8f15091e668b185d
niek <nieknooijens@gmail.com>
2013-07-09 10:43:30 +0200

    server is now part of a class again, tackled segfault for good, but program crashes at exit :-(

--- game/webserver.cc
+++ game/webserver.cc
@@ -3,22 +3,8 @@
 
 namespace http = boost::network::http;
 
-struct handler;
-typedef http::server<handler> http_server;
-struct handler {
-	void operator() (http_server::request const &request,
-	http_server::response &response) {
-		response = http_server::response::stock_reply(
-		http_server::response::ok, "performous web-frontend!");
-	}
 
-	void log(http_server::string_type const &info) {
-		std::cerr << "ERROR: " << info << '\n';
-	}
-};
-
-
-void StartServer() {
+void WebServer::StartServer() {
 	handler handler_;
 	http_server::options options(handler_);
 	http_server server_(
@@ -26,11 +12,10 @@ void StartServer() {
 	server_.run();
 }
 
-WebServer::WebServer()
-	:serverthread(&StartServer) {
-	serverthread.start_thread();
+WebServer::WebServer() {
+	serverthread.reset(new boost::thread(boost::bind(&WebServer::StartServer,boost::ref(*this))));
 }
 
 WebServer::~WebServer(){
-	serverthread.join();
+	serverthread->join();
 }
--- game/webserver.hh
+++ game/webserver.hh
@@ -1,16 +1,34 @@
 #pragma once
 #include <boost/network/protocol/http/server.hpp>
 #include <boost/thread/thread.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/thread/mutex.hpp>
 #include <string>
 
 using boost::thread;
 
+namespace http = boost::network::http;
+
 class WebServer
 {
 public:
+struct handler;
+typedef http::server<handler> http_server;
+struct handler {
+	void operator() (http_server::request const &request,
+	http_server::response &response) {
+		response = http_server::response::stock_reply(
+		http_server::response::ok, "performous web-frontend!");
+	}
+
+	void log(http_server::string_type const &info) {
+		std::cerr << "ERROR: " << info << '\n';
+	}
+};
 	WebServer();
 	~WebServer();
 private:
-	boost::thread serverthread;
+	boost::scoped_ptr<boost::thread> serverthread;
+	void StartServer();
 };
 
