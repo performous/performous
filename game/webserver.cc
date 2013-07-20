08aaf0c1c3e3687673cd142e507a5b08080ead05
niek <nieknooijens@gmail.com>
2013-07-20 18:22:51 +0200

    now loads index.html, need help with handling other files -cleaned up the header by moving the handler struct to the .cc file, this heavily reduces compile times!!!

--- game/webserver.cc
+++ game/webserver.cc
@@ -3,11 +3,37 @@
 
 namespace http = boost::network::http;
 
+struct WebServer::handler {
+	void operator() (http_server::request const &request,
+	http_server::response &response) {
+	//destination describes the file to be loaded, by default it's "/"
+		if(request.destination == "/") {
+		fs::ifstream f(findFile("index.html"), std::ios::binary);
+		f.seekg(0, std::ios::end);
+		size_t size = f.tellg();
+		f.seekg(0);
+		char responseBuffer[size + 2];
+		f.read(responseBuffer, size);
+		response = http_server::response::stock_reply(
+		http_server::response::ok, responseBuffer);
+		} else {
+		/*here needs code to serve other files, unfortunately using the same method as above will fail
+		this is because the webbrowser requests an .ico file, which cannot be send as text.
+		*/
+		}
+
+
+	}
+
+	void log(http_server::string_type const &info) {
+		std::cerr << "ERROR: " << info << '\n';
+	}
+};
 
 void WebServer::StartServer() {
 	handler handler_;
 	http_server::options options(handler_);
-	server_ = new http_server(options.address("192.168.1.123").port("8000"));
+	server_ = new http_server(options.address("0.0.0.0").port("8000"));
 	server_->run();
 }
 
@@ -20,3 +46,4 @@ WebServer::~WebServer() {
 	serverthread->join();
 	delete server_;
 }
+
