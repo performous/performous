35bdfd2e348471f5769390d5e8eb8213237ebe05
Lasse Karkkainen <tronic+ndrm@trn.iki.fi>
2013-07-30 06:19:26 +0300

    Do proper detection and use of Cppnetlib deps.

--- cmake/Modules/FindCppnetlib.cmake
+++ cmake/Modules/FindCppnetlib.cmake
@@ -15,9 +15,9 @@ include(LibFindMacros)
 #find_package( Cppnetlib REQUIRED ${Cppnetlib_COMPONENTS})
 
 #Use find_package to detect other libraries that the library depends on
-find_package(Crypto REQUIRED)
-find_package(Ssl REQUIRED)
-#libfind_package(Cppnetlib Crypto)
+libfind_package(Cppnetlib Boost)
+libfind_package(Cppnetlib Crypto)
+libfind_package(Cppnetlib Ssl)
 
 # Use pkg-config to get hints about paths
 #libfind_pkg_check_modules(Cppnetlib_PKGCONF cpp-netlib)
@@ -29,11 +29,21 @@ find_path(Cppnetlib_INCLUDE_DIR
   PATHS /usr/include/boost/ /usr/local/include/boost/
 )
 
-# Finally the library itself
-find_library(Cppnetlib_LIBRARY
-  NAMES  libcppnetlib-uri.a libcppnetlib-client-connections.a libcppnetlib-server-parsers.a
+# Finally the library components
+
+find_library(Cppnetlib_uri_LIBRARY
+  NAMES  libcppnetlib-uri.a
   #PATHS ${Cppnetlib_PKGCONF_LIBRARY_DIRS}
-  PATHS /usr/lib/ /usr/loca/lib/
+)
+
+find_library(Cppnetlib_client_connections_LIBRARY
+  NAMES  libcppnetlib-client-connections.a
+  #PATHS ${Cppnetlib_PKGCONF_LIBRARY_DIRS}
+)
+
+find_library(Cppnetlib_server_parsers_LIBRARY
+  NAMES  libcppnetlib-server-parsers.a
+  #PATHS ${Cppnetlib_PKGCONF_LIBRARY_DIRS}
 )
 
 #if(Cppnetlib_INCLUDE_DIR)
@@ -42,6 +52,6 @@ find_library(Cppnetlib_LIBRARY
 #  string(REGEX REPLACE ".*#define SND_LIB_VERSION_STR[ \t]*\"([^\n]*)\".*" "\\1" ALSA_VERSION "${_ALSA_VERSION_H_CONTENTS}")
 #endif(Cppnetlib_INCLUDE_DIR)
 
-set(Cppnetlib_PROCESS_INCLUDES Cppnetlib_INCLUDE_DIR) #BOOST_INCLUDE_DIRS
-set(Cppnetlib_PROCESS_LIBS Cppnetlib_LIBRARY) #BOOST_LIBRARIES
+set(Cppnetlib_PROCESS_INCLUDES Cppnetlib_INCLUDE_DIR BOOST_INCLUDE_DIRS)
+set(Cppnetlib_PROCESS_LIBS Cppnetlib_url_LIBRARY Cppnetlib_client_connections_LIBRARY Cppnetlib_server_parsers_LIBRARY BOOST_LIBRARIES Crypto_LIBRARIES Ssl_LIBRARIES)
 libfind_process(Cppnetlib)
