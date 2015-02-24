18a4f6177a83a5f0b96c5864c29b0e935ad4deb8
niek nooijens <nieknooijens@gmail.com>
2015-02-24 15:01:42 +0100

    add libepoxy to the mxe build targets.

--- win32/mxe/settings.mk
+++ win32/mxe/settings.mk
@@ -1,5 +1,5 @@
 JOBS := 1
 MXE_TARGETS :=  i686-w64-mingw32.shared
-LOCAL_PKG_LIST := gettext sdl sdl2 boost portaudio ffmpeg portmidi pango gdk-pixbuf librsvg libsigc++ libxml++ glew libcdio opencv
+LOCAL_PKG_LIST := gettext sdl sdl2 boost portaudio ffmpeg portmidi pango gdk-pixbuf librsvg libsigc++ libxml++ glew libcdio opencv libepoxy
 .DEFAULT local-pkg-list:
 local-pkg-list: $(LOCAL_PKG_LIST)
