7e0eec58689a72c7fd9c22df0a62d5675424db58
Tobias Gruetzmacher <tobias-git@23.gs>
2015-04-10 00:18:04 +0200

    Fix listFiles.
    Currently, it ignores its parameter and returns all files from the data
    and theme directories. This changes the behaviour to only list files in
    the specified subdirectory.

--- game/fs.cc
+++ game/fs.cc
@@ -226,11 +226,14 @@ fs::path findFile(fs::path const& filename) {
 }
 
 Paths listFiles(fs::path const& dir) {
-	if (dir.is_absolute()) throw std::logic_error("findFile expects a folder name without path.");
+	if (dir.is_absolute()) throw std::logic_error("listFiles expects a folder name without path.");
 	std::set<fs::path> found;  // Filenames already found
 	Paths files;  // Full paths of files found
 	for (fs::path path: getThemePaths()) {
-		for (fs::recursive_directory_iterator dirIt(path), dirEnd; dirIt != dirEnd; ++dirIt) {
+		fs::path subdir = path / dir;
+		if (!fs::is_directory(subdir))
+			continue;
+		for (fs::recursive_directory_iterator dirIt(subdir), dirEnd; dirIt != dirEnd; ++dirIt) {
 			fs::path name = dirIt->path().filename();  // FIXME: Extract full path from current folder, not just the filename
 			// If successfully inserted to "found", it wasn't found before, so add to paths.
 			if (found.insert(name).second) files.push_back(*dirIt);
