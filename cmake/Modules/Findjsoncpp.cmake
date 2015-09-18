d8637df66973be0660ed218f7fcab7958c082f9d
niek <nieknooijens@gmail.com>
2015-09-18 11:15:18 +0200

    jsoncpp-progress

--- cmake/Modules/Findjsoncpp.cmake
+++ cmake/Modules/Findjsoncpp.cmake
@@ -3,37 +3,54 @@
 # - Find JSONCpp
 # Find the JSONCpp includes and library
 # This module defines
-#  JSONCPP_FOUND, System has libjsoncpp.
-#  JSONCPP_INCLUDE_DIRS - The libjsoncpp include directories.
-#  JSONCPP_LIBRARIES - The libraries needed to use libjsoncpp.
-#  JSONCPP_DEFINITIONS - Compiler switches required for using libjsoncpp.
+#  JSONCPP_INCLUDE_DIRS, where to find json.h, etc.
+#  JSONCPP_LIBRARIES, the libraries needed to use jsoncpp.
+#  JSONCPP_FOUND, If false, do not try to use jsoncpp.
+#  also defined, but not for general use are
+#  JSONCPP_LIBRARY, where to find the jsoncpp library.
 
-FIND_PACKAGE(PkgConfig)
-PKG_CHECK_MODULES(PC_JSONCPP jsoncpp)
-SET(JSONCPP_DEFINITIONS ${PC_JSONCPP_CFLAGS_OTHER})
-
-FIND_PATH(JSONCPP_INCLUDE_DIR json/reader.h
-  HINTS ${PC_JSONCPP_INCLUDE_DIR} ${PC_JSONCPP_INCLUDE_DIRS}
-  PATH_SUFFIXES jsoncpp)
+FIND_PATH(JSONCPP_INCLUDE_DIRS json/json.h json/autolink.h json/features.h json/value.h json/config.h json/forwards.h json/reader.h json/writer.h
+/usr/local/include
+/usr/include
+/usr/include/jsoncpp
+/usr/include/jsoncpp/json
+)
 
 # Get the GCC compiler version
 EXEC_PROGRAM(${CMAKE_CXX_COMPILER}
-            ARGS ${CMAKE_CXX_COMPILER_ARG1} -dumpversion
-            OUTPUT_VARIABLE _gcc_COMPILER_VERSION
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-            )
+			ARGS ${CMAKE_CXX_COMPILER_ARG1} -dumpversion
+			OUTPUT_VARIABLE _gcc_COMPILER_VERSION
+			OUTPUT_STRIP_TRAILING_WHITESPACE
+			)
 
-# Try to find a library that was compiled with the same compiler version as we currently use.
+SET(JSONCPP_NAMES ${JSONCPP_NAMES} libjson_linux-gcc-${_gcc_COMPILER_VERSION}_libmt.so)
 FIND_LIBRARY(JSONCPP_LIBRARY
-  NAMES libjson_linux-gcc-${_gcc_COMPILER_VERSION}_libmt.so libjsoncpp.so
-  HINTS ${PC_JSONCPP_LIBDIR} ${PC_JSONCPP_LIBRARY_DIRS}
-  PATHS /usr/lib /usr/local/lib)
+  NAMES ${JSONCPP_NAMES}
+  PATHS /usr/lib /usr/local/lib
+  )
 
-SET(JSONCPP_LIBRARIES ${JSONCPP_LIBRARY})
-SET(JSONCPP_INCLUDE_DIRS ${JSONCPP_INCLUDE_DIR})
+IF (JSONCPP_LIBRARY AND JSONCPP_INCLUDE_DIRS)
+	SET(JSONCPP_LIBRARIES ${JSONCPP_LIBRARY})
+	SET(JSONCPP_FOUND "YES")
+ELSE (JSONCPP_LIBRARY AND JSONCPP_INCLUDE_DIRS)
+  SET(JSONCPP_FOUND "NO")
+ENDIF (JSONCPP_LIBRARY AND JSONCPP_INCLUDE_DIRS)
 
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(JSONCPP DEFAULT_MSG
-  JSONCPP_LIBRARY JSONCPP_INCLUDE_DIR)
 
-MARK_AS_ADVANCED(JSONCPP_LIBRARY JSONCPP_INCLUDE_DIR)
+IF (JSONCPP_FOUND)
+   IF (NOT JSONCPP_FIND_QUIETLY)
+	  MESSAGE(STATUS "Found JSONCpp: ${JSONCPP_LIBRARIES}")
+	  MESSAGE(STATUS "JsonCPP include dirs: " ${JSONCPP_INCLUDE_DIRS})
+   ENDIF (NOT JSONCPP_FIND_QUIETLY)
+ELSE (JSONCPP_FOUND)
+   IF (JSONCPP_FIND_REQUIRED)
+	  MESSAGE(FATAL_ERROR "Could not find JSONCpp library")
+   ENDIF (JSONCPP_FIND_REQUIRED)
+ENDIF (JSONCPP_FOUND)
+
+
+
+MARK_AS_ADVANCED(
+  JSONCPP_LIBRARY
+  JSONCPP_INCLUDE_DIRS
+  )
