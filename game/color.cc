b4a37b9981d5ad1cb90ce5c3131b8eb36ee448a7
skyli91 <skyli91@gmail.com>
2015-11-20 11:50:38 +0100

    Update color.cc

--- game/color.cc
+++ game/color.cc
@@ -60,14 +60,14 @@ glmath::vec4 Color::linear() const {
 }
 
 Color MicrophoneColor::get(std::string name) {
-	if (name == "blue") return Color(0.0, 42.0/255.0, 1.0);
+	if (name == "blue") return Color(0.0, 8.0/255.0, 1.0);
 	else if (name == "red") return Color(1, 0.0, 0.0);
 	else if (name == "green") return Color(0.0, 1.0, 0.0);
 	else if (name == "yellow") return Color(1.0, 1.0, 0.0);
-	else if (name == "fuchsia") return Color(1.0, 86.0/255.0, 214.0/255.0);
-	else if (name == "orange") return Color(1.0, 132.0/255, 0.0);
-	else if (name == "purple") return Color(143.0/255.0, 43.0/255.0, 1.0);
-	else if (name == "aqua") return Color(0.0, 192.0/255.0, 1.0);
+	else if (name == "fuchsia") return Color(1.0, 109.0/255.0, 178.0/255.0);
+	else if (name == "orange") return Color(1.0, 85.0/255, 0.0);
+	else if (name == "purple") return Color(132.0/255.0, 0, 1.0);
+	else if (name == "aqua") return Color(0.0, 1.0, 1.0);
 	else return Color(0.5, 0.5, 0.5);
 }
 
