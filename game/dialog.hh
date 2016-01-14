47e4738a3c5017e0e4dbda2a408dd738985d173f
Nooijens,niek N.P.E <n.nooijens@student.fontys.nl>
2016-01-14 15:09:21 +0100

    Moved code from dialog header to dedicated C file.

--- game/dialog.hh
+++ game/dialog.hh
@@ -9,20 +9,9 @@
 class Dialog {
   public:
 	/// constructor
-	Dialog(std::string const& text):
-	  m_text(text),
-	  m_dialog(findFile("warning.svg")),
-	  m_svgText(findFile("dialog_txt.svg"), config["graphic/text_lod"].f())
-	{
-		m_dialog.dimensions.fixedWidth(0.8);
-	}
+	Dialog(std::string const& text);
 	/// draws dialogue
-	void draw() {
-		using namespace glmath;
-		Transform(translate(vec3(0.0f, 0.0f, 0.1f)));  // Raise a bit in 3D
-		m_dialog.draw();
-		m_svgText.draw(m_text);
-	}
+	void draw();
 
   private:
 	std::string m_text;
