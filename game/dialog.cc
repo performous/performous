bca32a3fc34331f1afdcca8897f1023e927c484b
Nooijens,niek N.P.E <n.nooijens@student.fontys.nl>
2016-01-14 16:16:37 +0100

    first attempt at creating a better dialog

--- game/dialog.cc
+++ game/dialog.cc
@@ -5,12 +5,13 @@ Dialog::Dialog(std::string const& text) :
 	m_dialog(findFile("warning.svg")),
 	m_svgText(findFile("dialog_txt.svg"), config["graphic/text_lod"].f())
 	{
-		m_dialog.dimensions.fixedWidth(0.8);
+		m_dialog.dimensions.screenTop(-0.1f);
 	}
 
 void Dialog::draw() {
-	using namespace glmath;
-	Transform(translate(vec3(0.0f, 0.0f, 0.1f)));  // Raise a bit in 3D
+	double v = 1.0 - 1.0 * 0; //TODO animate dialog
+	m_dialog.dimensions.fixedHeight(0.15).right(0.5).screenTop(-0.10 + 0.11 * v);
 	m_dialog.draw();
+	m_svgText.dimensions.right(0.35).screenTop(0.08 - 0.10 + 0.11 * v);
 	m_svgText.draw(m_text);
 }
