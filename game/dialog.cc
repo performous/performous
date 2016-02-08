69330e7cef12d643f480727872c12a285d502383
Nooijens,niek N.P.E <n.nooijens@student.fontys.nl>
2016-02-08 14:44:42 +0100

    fixed ugly black border and animated the dialog closes #132

--- game/dialog.cc
+++ game/dialog.cc
@@ -6,12 +6,35 @@ Dialog::Dialog(std::string const& text) :
 	m_svgText(findFile("dialog_txt.svg"), config["graphic/text_lod"].f())
 	{
 		m_dialog.dimensions.screenTop(-0.1f);
+		m_animationVal.setValue(1);
+		m_state = SLIDEIN;
 	}
 
 void Dialog::draw() {
-	double v = 1.0 - 1.0 * 0; //TODO animate dialog
-	m_dialog.dimensions.fixedHeight(0.15).right(0.5).screenTop(-0.10 + 0.11 * v);
+	double verticaloffset = 1.0;
+	switch(m_state) {
+	case IDLE:
+		verticaloffset = 0.0;
+		if(m_animationVal.get() == 0) {
+			m_state = SLIDEOUT;
+			m_animationVal.setValue(1);
+		}
+		break;
+		case SLIDEIN :
+		verticaloffset = 1.0 - 1.0 * (1.0-m_animationVal.get()); //TODO animate dialog
+		if(m_animationVal.get() == 0) {
+			m_state = IDLE;
+			m_animationVal.setValue(6);
+		}
+		break;
+		case SLIDEOUT:
+			verticaloffset = 1.0 - 1.0 * m_animationVal.get();
+		break;
+	}
+
+
+	m_dialog.dimensions.fixedHeight(0.15).right(0.5).screenTop(-0.10 + 0.11 - verticaloffset);
 	m_dialog.draw();
-	m_svgText.dimensions.right(0.35).screenTop(0.08 - 0.10 + 0.11 * v);
+	m_svgText.dimensions.right(0.35).screenTop(0.08 - 0.10 + 0.11 - verticaloffset);
 	m_svgText.draw(m_text);
 }
