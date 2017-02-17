afc8cf2915409bc02a3ed602b2e4ba2e4d12573e
Gregorio Litenstein <g.litenstein@gmail.com>
2017-02-17 01:42:12 -0300

    macOS Build script fixes.
    Fixed “if” syntax.
    Better check for binaries required to make the app bundle.
    Clean-up.
    Some more minor fixes.

--- osx-utils/performous-app-build.sh
+++ osx-utils/performous-app-build.sh
@@ -20,31 +20,47 @@ echo # just a final linefeed, optics...
 return $retval
 }
 
-if [ which dylibbundler &> /dev/null ]
+function exists {
+if hash "$1" 2>/dev/null
 then
-    echo "dylibbundler found!"
+return 0
 else
-    echo "dylibbundler not found! you need to install it before creating the bundle."
-    exit
+return 1
+fi
+}
+
+# if [ hash dylibbundler &> /dev/null ]
+# then
+#     echo "dylibbundler found!"
+# else
+#     echo "dylibbundler not found! you need to install it before creating the bundle."
+#     exit
+# fi
+
+# if [ 0 == ´command -v dylibbundler 2>/dev/null || { echo "dylibbundler not found! you need to install it before creating the bundle."; exit 1; }
+
+if exists dylibbundler
+then
+echo "dylibbundler found!"
+else
+echo "dylibbundler not found! you need to install it before creating the bundle."
+exit 1
 fi
 
-if [ which appdmg &> /dev/null ]
+if exists appdmg
 then
-    echo "appdmg found!"
-    FANCY_DMG=1
-else
-    echo "appdmg not found!"
-if [ which npm &> /dev/null ]
-then
-    echo "npm found!"
-if [ asksure "appdmg is not installed, would you like to install it? (y/n)" ]
-then
- sudo npm install -g https://github.com/LinusU/node-appdmg.git
 FANCY_DMG=1
+echo "appdmg found! Will build styled bundle."
 else
-echo "Will build DMG without the fancy style then..."
-FANCY_DMG=0
-fi
+if exists npm
+then
+    if asksure "appdmg is not installed, would you like to install it? (y/n)"
+    then
+    sudo npm install -g https://github.com/LinusU/node-appdmg.git && FANCY_DMG=1
+    else
+    echo "Will build DMG without the fancy style then..."
+    FANCY_DMG=0
+    fi
 else
 echo "npm not found!"
 echo "Will build DMG without the fancy style then..."
@@ -82,7 +98,7 @@ rm -rf ./build
 mkdir build
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX=$TEMPDIR -DENABLE_TOOLS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=1 -DFreetype_INCLUDE_DIR=/opt/local/include/freetype2 -DCMAKE_OSX_DEPLOYMENT_TARGET=${DEPLOYMENT_TARGET} -DFontconfig_INCLUDE_DIR=/opt/local/include/fontconfig -DPng_INCLUDE_DIR=/opt/local/include/libpng -DAVCodec_INCLUDE_DIR=/opt/local/include/libavcodec -DAVFormat_INCLUDE_DIR=/opt/local/include/libavformat -DSWScale_INCLUDE_DIR=/opt/local/include/libswscale -DFreetype_INCLUDE_DIR=/opt/local/include/freetype2/ -DLibXML2_LIBRARY=/opt/local/lib/libxml2.dylib -DLibXML2_INCLUDE_DIR=/opt/local/include/libxml2 -DLibXML++Config_INCLUDE_DIR=/opt/local/lib/libxml++-2.6/include -DGettext_LIBRARY=/opt/local/lib/libgettextlib.dylib  -DGettext_INCLUDE_DIR=/opt/local/include/ -DGlibmmConfig_INCLUDE_DIR=/opt/local/lib/glibmm-2.4/include -DGlibConfig_INCLUDE_DIR=/opt/local/lib/glib-2.0/include -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_FLAGS="-arch x86_64" -DSHARE_INSTALL=Resources -DLOCALE_DIR=Resources/Locales -DCMAKE_CXX_FLAGS="-Wno-unused-function -Wno-unused-local-typedefs -Wno-ignored-qualifiers -stdlib=libc++ -lc++ -lc++abi -arch x86_64 -L/opt/local/lib -lintl -Wl,-framework -Wl,CoreFoundation,-headerpad_max_install_names" -DCMAKE_EXE_LINKER_FLAGS="-ldl -stdlib=libc++ -lc++ -lc++abi -arch x86_64" -DCMAKE_MODULE_LINKER_FLAGS="-ldl -stdlib=libc++ -lc++ -lc++abi -arch x86_64" -DCMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_SHARED_LINKER_FLAGS="-ldl -stdlib=libc++ -lc++ -lc++abi -arch x86_64" -DCMAKE_STATIC_LINKER_FLAGS="-ldl -stdlib=libc++ -lc++ -lc++abi -arch x86_64" ../..
+cmake -DCMAKE_INSTALL_PREFIX=$TEMPDIR -DENABLE_TOOLS=ON -DCMAKE_BUILD_TYPE=Release -DENABLE_WEBSERVER=ON -DCMAKE_VERBOSE_MAKEFILE=1 -DFreetype_INCLUDE_DIR=/opt/local/include/freetype2 -DCMAKE_OSX_DEPLOYMENT_TARGET=${DEPLOYMENT_TARGET} -DFontconfig_INCLUDE_DIR=/opt/local/include/fontconfig -DPng_INCLUDE_DIR=/opt/local/include/libpng -DAVCodec_INCLUDE_DIR=/opt/local/include/libavcodec -DAVFormat_INCLUDE_DIR=/opt/local/include/libavformat -DSWScale_INCLUDE_DIR=/opt/local/include/libswscale -DFreetype_INCLUDE_DIR=/opt/local/include/freetype2/ -DLibXML2_LIBRARY=/opt/local/lib/libxml2.dylib -DLibXML2_INCLUDE_DIR=/opt/local/include/libxml2 -DLibXML++Config_INCLUDE_DIR=/opt/local/lib/libxml++-2.6/include -DGettext_LIBRARY=/opt/local/lib/libgettextlib.dylib  -DGettext_INCLUDE_DIR=/opt/local/include/ -DGlibmmConfig_INCLUDE_DIR=/opt/local/lib/glibmm-2.4/include -DGlibConfig_INCLUDE_DIR=/opt/local/lib/glib-2.0/include -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_FLAGS="-arch x86_64" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DSHARE_INSTALL=Resources -DLOCALE_DIR=Resources/Locales -DCMAKE_CXX_FLAGS="-Wno-unused-function -Wno-unused-local-typedefs -Wno-ignored-qualifiers -stdlib=libc++ -arch x86_64 -L/opt/local/lib -lintl" -DCMAKE_EXE_LINKER_FLAGS="-ldl -stdlib=libc++ -lc++ -lc++abi -arch x86_64" -DCMAKE_OSX_ARCHITECTURES="x86_64" ../..
 make -j${MAKE_JOBS} install # You can change the -j value in order to spawn more build threads.
 
 # then create the rest of the app bundle
@@ -149,6 +165,6 @@ rm -f RWPerformous.dmg
 cd ..
 elif [ $FANCY_DMG == 1 ]
 then
-rm "${CURRDIR}/Performous.dmg"
+rm -f "${CURRDIR}/Performous.dmg"
 appdmg "${CURRDIR}/resources/dmg_spec.json" "${CURRDIR}/Performous.dmg"
 fi
