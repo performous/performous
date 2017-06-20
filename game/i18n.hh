f88972049548ba4ace4cde6029e79d37c929e849
Arjan Spieard <arjan.spieard@gmail.com>
2017-06-20 20:22:44 +0200

    Fixed a bug where i had to call string() instead of c_str()

--- game/i18n.hh
+++ game/i18n.hh
@@ -13,7 +13,7 @@ class Gettext {
 		Gettext(const char *package) {
 			generator gen;
 		    // Specify location of dictionaries
-		    gen.add_messages_path(getLocaleDir().c_str());
+		    gen.add_messages_path(getLocaleDir().string());
 		    gen.add_messages_domain(package);
 		    // Generate locales and imbue them to iostream
 		    locale::global(gen(""));
