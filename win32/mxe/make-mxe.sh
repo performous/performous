5960d73a4661f528537c28871074b12347c45f7d
Arjan Spieard <arjan.spieard@gmail.com>
2017-06-27 22:58:36 +0200

    Added performous/performous-mxe as submodule to performous.
    Updated submodule's readme
    
    Moved windows make-installer.py back to main-repo
    
    Removed settings.mk
    
    Removed depoxyshared from cmakelist

--- win32/mxe/make-mxe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "installing git in case it hasn't been done yet"
-sudo apt-get install git-core
-git clone -b master https://github.com/mxe/mxe.git
-cp settings.mk ./mxe/settings.mk
-cd mxe
-make
-cd ..
-echo "mxe has been build please run the make-performous script"
