8e4936fb22ab3b094a42eab72d32651640740c99
Arjan Spieard <arjan.spieard@gmail.com>
2017-06-28 20:42:32 +0200

    Fixed a bug where refreshing database resulted in a not styled table.

--- data/themes/default/www/js/site.js
+++ data/themes/default/www/js/site.js
@@ -19,9 +19,7 @@ $('#playlist-songs').on("click", "a", function() {
 $('#refresh-database').click(function() {
     $.get("api/getDataBase.json", function(data) {
         var database = JSON.parse(data);
-
-        clearList("database-songs");
-
+        clearTable("#database-songs > tbody");
         $.each(database, function (iterator, songObject){
             var y = "<div>pizza</div>";
             $("#database-songs").append("<tr><td>" + songObject["Artist"] + "</td><td>" + songObject["Title"] + "</td><td class='hidden-xs'>" + songObject["Language"] + "</td><td class='hidden-xs hidden-sm'>" + songObject["Edition"] + "</td><td class='hidden-xs hidden-sm hidden-md'>" + songObject["Creator"] + "</td><td class='text-right text-nowrap fixed-pixel-glyphicon'><span class='glyphicon glyphicon-plus'></span></td></tr>");
@@ -81,6 +79,13 @@ function clearList(selector){
     var myNode = document.getElementById(selector);
     while (myNode.firstChild) {
         myNode.removeChild(myNode.firstChild);
+    }
+}
+
+function clearTable(selector) {
+    var tbody = $(selector);
+    while (tbody.children().length > 1) {
+        tbody.children().last().remove();
     }
 }
 
