2e084d6a7d865dceb09986a44618acea0fe9b02c
Gregorio Litenstein <g.litenstein@gmail.com>
2017-08-29 18:12:30 -0300

    Detect and move config files from old to new path.

--- game/fs.cc
+++ game/fs.cc
@@ -60,6 +60,30 @@ namespace {
 	typedef boost::lock_guard<boost::mutex> Lock;
 }
 
+void copyDirectoryRecursively(const fs::path& sourceDir, const fs::path& destinationDir)
+{
+    if (!fs::exists(sourceDir) || !fs::is_directory(sourceDir))
+    {
+        throw std::runtime_error("Source directory " + sourceDir.string() + " does not exist or is not a directory");
+    }
+    if (fs::exists(destinationDir))
+    {
+        throw std::runtime_error("Destination directory " + destinationDir.string() + " already exists");
+    }
+    if (!fs::create_directory(destinationDir))
+    {
+        throw std::runtime_error("Cannot create destination directory " + destinationDir.string());
+    }
+
+    for (const auto& dirEnt : fs::recursive_directory_iterator{sourceDir})
+    {
+        const auto& path = dirEnt.path();
+        auto relativePathStr = path.string();
+        boost::algorithm::replace_first(relativePathStr, sourceDir.string(), "");
+        fs::copy(path, destinationDir / relativePathStr);
+    }
+}
+
 void pathBootstrap() { Lock l(mutex); cache.pathBootstrap(); }
 void pathInit() { Lock l(mutex); cache.pathInit(); }
 fs::path getLogFilename() { Lock l(mutex); return cache.cache / "infolog.txt"; }
