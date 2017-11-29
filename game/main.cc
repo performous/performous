6c85f277e07910440df481d2791f724a5c29c1b1
Gregorio Litenstein <g.litenstein@gmail.com>
2017-11-29 19:21:24 -0300

    Change var name; display alert on manual reload.

--- game/main.cc
+++ game/main.cc
@@ -189,9 +189,9 @@ void mainLoop(std::string const& songlist) {
 		while (!gm.isFinished()) {
 			Profiler prof("mainloop");
 			bool benchmarking = config["graphic/fps"].b();
-			if (songs.doneLoading == true && songs.shownFinishDialog == false) {
+			if (songs.doneLoading == true && songs.displayedAlert == false) {
 				gm.dialog(_("Done Loading!\n Loaded ") + std::to_string(songs.loadedSongs()) + " Songs.");
-				songs.shownFinishDialog = true;
+				songs.displayedAlert = true;
 			}
 			if( g_take_screenshot ) {
 				try {
