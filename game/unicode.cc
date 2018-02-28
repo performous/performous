77ace8cd3ad75685e67548598b75debfa86e09d2
Gregorio Litenstein <g.litenstein@gmail.com>
2018-02-28 19:59:15 -0300

    Use ICU instead of Glib for handling unicode.
    Also changed convertToUTF8() signature to avoid ambiguity.

--- game/unicode.cc
+++ game/unicode.cc
@@ -1,49 +1,72 @@
 #include "unicode.hh"
 #include "configuration.hh"
 
+#include <boost/regex.hpp>
 #include <boost/scoped_ptr.hpp>
-#include <glibmm/convert.h>
-#include <glibmm/ustring.h>
+#include <unicode/unistr.h>
+#include <unicode/ustream.h>
 #include <sstream>
 #include <stdexcept>
 
-void convertToUTF8(std::stringstream &_stream, std::string _filename) {
+UErrorCode UnicodeUtil::m_icuError = U_ZERO_ERROR;
+icu::RuleBasedCollator UnicodeUtil::m_dummyCollator = icu::RuleBasedCollator(icu::UnicodeString(""), icu::Collator::PRIMARY, m_icuError);
+LocalUCharsetDetectorPointer UnicodeUtil::m_chardet = LocalUCharsetDetectorPointer(ucsdet_open(&UnicodeUtil::m_icuError));
+
+MatchResult UnicodeUtil::getCharset (std::string const& str) {
+	MatchResult retval;
+	auto string = str.c_str();
 	try {
+		ucsdet_setText(UnicodeUtil::m_chardet.getAlias(), string, -1, &m_icuError);
+		const UCharsetMatch* match = ucsdet_detect(UnicodeUtil::m_chardet.getAlias(), &m_icuError);
+		return std::pair<const char*,int>(ucsdet_getName(match, &m_icuError), ucsdet_getConfidence(match, &m_icuError));	
+	} catch (...) {
+		std::clog << "unicode/error: Couldn't pass text to CharsetDetector: " << u_errorName(m_icuError);
+	}
+}
+
+void convertToUTF8(std::stringstream &_stream, std::string _filename) {
 		std::string data = _stream.str();
-		Glib::convert(data, "UTF-8", "UTF-8"); // Test if input is UTF-8
-		if (data.substr(0, 3) == "\xEF\xBB\xBF") {
-			if (config["game/bom_warnings"].b()) std::clog << "unicode/warning: " << _filename << " UTF-8 BOM ignored. Please avoid editors that use BOMs (e.g. Notepad)." << std::endl;
-			_stream.str(data.substr(3)); // Remove BOM if there is one
+		MatchResult match = UnicodeUtil::getCharset(data);
+		icu::UnicodeString ustring;
+		if (match.second >= 50) { // fairly good match?
+			std::string charset = match.first;
+			if (charset == "UTF-8") {
+				if (data.substr(0, 3) == "\xEF\xBB\xBF") {
+					if (config["game/bom_warnings"].b()) {
+						std::clog << "unicode/warning: " << _filename << " UTF-8 BOM ignored. Please avoid editors that add a BOM to UTF-8 (e.g. Notepad)." << std::endl;
+						}
+					_stream.str(data.substr(3)); // Remove BOM if there is one
+				}
+			}
+			else {
+				if (!_filename.empty()) { std::clog << "unicode/warning: " << _filename << " is not UTF-8... (" << charset << ") detected. Use a text-editor or other utility to convert your files." << std::endl; }
+				std::string _str;
+				const char* tmp = data.c_str();
+				icu::UnicodeString ustring = icu::UnicodeString(tmp, charset.c_str());
+				_stream.str(ustring.toUTF8String(_str));
+			}
 		}
-	} catch(...) {
-		std::string codeset = config["game/fallback_encoding"].getEnumName();
-		if (!_filename.empty())
-			std::clog << "unicode/warning: " << _filename << " is not UTF-8.\n  Assuming " << codeset
-				<< ". Use recode " << codeset << "..UTF-8 */*.txt to convert your files." << std::endl;
-		try {
-			_stream.str(Glib::convert(_stream.str(), "UTF-8", codeset)); // Convert from fallback encoding
-		} catch (...) {
-			// Filter out anything but ASCII
+		else { 			// If we're not confident in any particular charset, filter out anything but ASCIIw
 			std::string tmp;
 			for (char ch; _stream.get(ch);) tmp += (ch >= 0x20 && ch < 0x7F) ? ch : '?';
 		}
-	}
+	std::clog << "unicode/info: Finished UTF8 conversion: " << _stream.str() << std::endl;
 }
 
 std::string convertToUTF8(std::string const& str) {
 	std::stringstream ss(str);
-	convertToUTF8(ss);
+	convertToUTF8(ss, std::string());
 	return ss.str();
 }
 
 std::string unicodeCollate(std::string const& str) {
-	Glib::ustring ustr = str, ustr2;
-	if (ustr.substr(0, 4) == "The ") ustr = ustr.substr(4) + "the";
-	if (ustr.substr(0, 2) == "A ") ustr = ustr.substr(2) + "a";
-	// Remove all non-alnum characters
-	for (Glib::ustring::iterator it = ustr.begin(), end = ustr.end(); it != end; ++it) {
-		if (Glib::Unicode::isalnum(*it)) ustr2 += Glib::Unicode::tolower(*it);
-	}
-	return ustr2;
-	// Should use ustr2.casefold_collate_key() instead of tolower, but it seems to be crashing...
+	ConfigItem::StringList termsToCollate = config["game/sorting_ignore"].sl();
+	std::string pattern = std::string("^((");
+	for (auto term: termsToCollate) {
+		if (term != termsToCollate.front()) { pattern.append(std::string("|")); }
+		pattern.append(term);
+		if (term == termsToCollate.back()) { pattern.append(std::string(")\\s(.+))$")); }
+	}	
+	std::string collated = boost::regex_replace(convertToUTF8(str), boost::regex(pattern), "\\3 \\2");
+	return collated;
 }
--- game/unicode.hh
+++ game/unicode.hh
@@ -2,7 +2,27 @@
 
 #include <iostream>
 #include <string>
+#include <unicode/tblcoll.h>
+#include <unicode/ucsdet.h>
 
-void convertToUTF8(std::stringstream &_stream, std::string _filename = std::string());
+
+
+void convertToUTF8(std::stringstream &_stream, std::string _filename);
 std::string convertToUTF8(std::string const& str);
 std::string unicodeCollate(std::string const& str);
+
+typedef std::pair<const char*, int> MatchResult;
+
+struct UnicodeUtil {
+
+UnicodeUtil() {};
+~UnicodeUtil() {};
+friend class Songs;
+
+static MatchResult getCharset(std::string const& str);
+
+private:
+static UErrorCode m_icuError;
+static icu::RuleBasedCollator m_dummyCollator;
+static LocalUCharsetDetectorPointer m_chardet;
+};
