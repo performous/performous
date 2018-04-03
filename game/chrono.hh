1ae26b5077298d41a162c338105ba632da8ad0f5
Lasse Kärkkäinen <ljkarkk2@cc.hut.fi>
2018-04-03 13:20:42 +0300

    Use duration_cast instead of manual calculation.

--- game/chrono.hh
+++ game/chrono.hh
@@ -8,5 +8,5 @@ using Time = Clock::time_point;
 using Seconds = std::chrono::duration<double>;
 
 static inline Clock::duration clockDur(Seconds d) {
-	return static_cast<long long>(d.count() * 1e9) * 1ns;
+	return std::chrono::duration_cast<Clock::duration>(d);
 }
