7b0571dda70700d0aeece140e30f3ad3974470c1
Gregorio Litenstein <g.litenstein@gmail.com>
2018-07-04 20:41:18 -0400

    Add CED submodule.

--- game/unicode.cc
+++ game/unicode.cc
@@ -6,36 +6,60 @@
 #include <stdexcept>
 #include <unicode/unistr.h>
 #include <unicode/ustream.h>
+#include "../ced/compact_enc_det/compact_enc_det.h"
 
-UErrorCode UnicodeUtil::m_icuError = U_ZERO_ERROR;
-icu::RuleBasedCollator UnicodeUtil::m_dummyCollator (icu::UnicodeString (""), icu::Collator::PRIMARY, m_icuError);
+icu::RuleBasedCollator UnicodeUtil::m_dummyCollator (icu::UnicodeString (""), icu::Collator::PRIMARY, m_staticIcuError);
+UErrorCode UnicodeUtil::m_staticIcuError = U_ZERO_ERROR;
 
-MatchResult UnicodeUtil::getCharset (std::string const& str) {
+MatchResult UnicodeUtil::getCharset (UCharsetDetector* m_chardet, std::string const& str) {
 	MatchResult retval;
-	icu::LocalUCharsetDetectorPointer m_chardet (ucsdet_open (&UnicodeUtil::m_icuError));
-	ucsdet_enableInputFilter(m_chardet.getAlias(), true);
+	const char* text = str.c_str();
+	int bytes_consumed;
+	bool is_reliable;
+	Encoding encoding = CompactEncDet::DetectEncoding(
+        text, strlen(text),
+        nullptr, nullptr, nullptr,
+        UNKNOWN_ENCODING,
+        UNKNOWN_LANGUAGE,
+        CompactEncDet::WEB_CORPUS,
+        true,
+        &bytes_consumed,
+        &is_reliable);	
+// 	std::clog << "unicode/debug: Let's try CED for text: " << text << ", detected: " << EncodingName(encoding) << ", reliable?: " << std::to_string(is_reliable) << std::endl;
+	UErrorCode icuError = U_ZERO_ERROR;
+	icu::LocalUCharsetDetectorPointer _m_chardet(ucsdet_open(&icuError));
+	// 	icu::LocalUCharsetDetectorPointer m_chardet (ucsdet_open (&UnicodeUtil::m_staticIcuError));
+	if (U_FAILURE(icuError)) { 
+		std::string err = std::string ("unicode/error: Couldn't create LocalUCharsetDetectorPointer: ");
+		err += u_errorName(UnicodeUtil::m_staticIcuError);
+		throw std::runtime_error (err);
+	}
+	ucsdet_enableInputFilter(m_chardet, true);
+	if (U_FAILURE(icuError)) { 
+		std::string err = std::string ("unicode/error: Couldn't enable input filter: ");
+		err += u_errorName(UnicodeUtil::m_staticIcuError);
+		throw std::runtime_error (err);
+	}
 	auto string = str.c_str();
-	ucsdet_setText(m_chardet.getAlias(), string, -1, &m_icuError);
-	if (U_FAILURE(UnicodeUtil::m_icuError)) {
+	ucsdet_setText(m_chardet, string, strlen(string), &UnicodeUtil::m_staticIcuError);
+	if (U_FAILURE(UnicodeUtil::m_staticIcuError)) {
 		std::string err = std::string ("unicode/error: Couldn't pass text to CharsetDetector: ");
-		err += u_errorName(m_icuError);
+		err += u_errorName(UnicodeUtil::m_staticIcuError);
+		ucsdet_close(m_chardet);
 		throw std::runtime_error (err);
 	}
 	else {
-		const UCharsetMatch* match = ucsdet_detect (m_chardet.getAlias(), &m_icuError);
-		return std::pair<std::string,int>(ucsdet_getName(match, &m_icuError), ucsdet_getConfidence(match, &m_icuError));
-	}
+		const UCharsetMatch* match = ucsdet_detect (m_chardet, &UnicodeUtil::m_staticIcuError);
+		return std::pair<std::string,int>(ucsdet_getName(match, &UnicodeUtil::m_staticIcuError), ucsdet_getConfidence(match, &UnicodeUtil::m_staticIcuError));
+	}	
 }
 
-void convertToUTF8 (std::stringstream &_stream, std::string _filename) {
+void UnicodeUtil::convertToUTF8 (std::stringstream &_stream, std::string _filename) {
 	std::string data = _stream.str();
 	MatchResult match;
-	if (!_filename.empty()) {
-		match = UnicodeUtil::getCharset(data);
-	}
-	else {
-		match = std::pair<std::string,int>("UTF-8",100); // If there's no filename, assume it's internal text and thus utf-8.
-	}
+		UCharsetDetector* m_chardet = ucsdet_open(&m_icuError);
+		match = UnicodeUtil::getCharset(m_chardet, data);	
+// 		match = std::pair<std::string,int>("UTF-8",100); // If there's no filename, assume it's internal text and thus utf-8.
 	icu::UnicodeString ustring;
 	// Test for UTF-8 BOM (a three-byte sequence at the beginning of a file)
 	if (data.substr(0, 3) == "\xEF\xBB\xBF") {
@@ -59,9 +83,10 @@ void convertToUTF8 (std::stringstream &_stream, std::string _filename) {
 			_stream.str(ustring.toUTF8String(_str));
 		}
 	}
+	ucsdet_close(m_chardet);
 }
 
-std::string convertToUTF8 (std::string const& str) {
+std::string UnicodeUtil::convertToUTF8 (std::string const& str) {
 	std::stringstream ss (str);
 	convertToUTF8 (ss, std::string());
 	return ss.str();
@@ -79,6 +104,6 @@ std::string unicodeCollate (std::string const& str) {
 			pattern += std::string(")\\s(.+))$");
 		}
 	}
-	std::string collated = std::regex_replace(convertToUTF8(str), std::regex(pattern, std::regex_constants::icase), "\\3 \\2");
+	std::string collated = std::regex_replace(UnicodeUtil().convertToUTF8(str), std::regex(pattern, std::regex_constants::icase), "\\3 \\2");
 	return collated;
 }
--- game/unicode.hh
+++ game/unicode.hh
@@ -3,22 +3,34 @@
 #include <iostream>
 #include <string>
 #include <unicode/tblcoll.h>
+#include <unicode/uclean.h>
 #include <unicode/ucsdet.h>
+#include <unicode/utypes.h>
 
-
-
-void convertToUTF8(std::stringstream &_stream, std::string _filename);
-std::string convertToUTF8(std::string const& str);
+// void convertToUTF8(std::stringstream &_stream, std::string _filename);
+// std::string convertToUTF8(std::string const& str);
 std::string unicodeCollate(std::string const& str);
 
 typedef std::pair<std::string, int> MatchResult;
 
 struct UnicodeUtil {
-	UnicodeUtil() {};
+	UnicodeUtil(): m_icuError(U_ZERO_ERROR) { 
+		u_init(&m_icuError);
+		if (U_FAILURE(m_icuError)) {
+			std::string err = std::string ("unicode/error: Couldn't initialize ICU: ");
+			err += u_errorName(m_icuError);
+			throw std::runtime_error (err);
+		}
+// 		}
+
+	};
 	~UnicodeUtil() {};
 	friend class Songs;
-	static MatchResult getCharset(std::string const& str);
+	static MatchResult getCharset(UCharsetDetector* m_chardet, std::string const& str);
+	void convertToUTF8 (std::stringstream &_stream, std::string _filename);
+	std::string convertToUTF8 (std::string const& str);
   private:
-	static UErrorCode m_icuError;
+	UErrorCode m_icuError;
+	static UErrorCode m_staticIcuError;
 	static icu::RuleBasedCollator m_dummyCollator;
 };
