655aba78c6ea915d06a8e6ca5b528f20a1059959
Gregorio Litenstein <g.litenstein@gmail.com>
2018-10-25 11:58:31 -0300

    Allow setting vec4 uniforms.

--- game/glmath.hh
+++ game/glmath.hh
@@ -5,6 +5,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_access.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/matrix_operation.hpp>
 #include <algorithm>
 #include <cmath>
