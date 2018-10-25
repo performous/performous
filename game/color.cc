e3405db3a7b100bc86830e4f5a2a54e647d6a55f
Gregorio Litenstein <g.litenstein@gmail.com>
2018-10-25 11:56:18 -0300

    Make default and named colors explicitly opaque.

--- game/color.cc
+++ game/color.cc
@@ -16,23 +16,23 @@ namespace {
 		typedef std::map<std::string, Color> Map;
 		Map m;
 		ColorNames() {
-			m["maroon"] = Color("#800000");
-			m["red"] = Color("#FF0000");
-			m["green"] = Color("#008000");
-			m["lime"] = Color("#00FF00");
-			m["navy"] = Color("#000080");
-			m["blue"] = Color("#0000FF");
-			m["purple"] = Color("#800080");
-			m["fuchsia"] = Color("#FF00FF");
-			m["olive"] = Color("#808000");
-			m["yellow"] = Color("#FFFF00");
-			m["teal"] = Color("#008080");
-			m["aqua"] = Color("#00FFFF");
-			m["white"] = Color("#FFFFFF");
+			m["maroon"] = Color("#800000FF");
+			m["red"] = Color("#FF0000FF");
+			m["green"] = Color("#008000FF");
+			m["lime"] = Color("#00FF00FF");
+			m["navy"] = Color("#000080FF");
+			m["blue"] = Color("#0000FFFF");
+			m["purple"] = Color("#800080FF");
+			m["fuchsia"] = Color("#FF00FFFF");
+			m["olive"] = Color("#808000FF");
+			m["yellow"] = Color("#FFFF00FF");
+			m["teal"] = Color("#008080FF");
+			m["aqua"] = Color("#00FFFFFF");
+			m["white"] = Color("#FFFFFFFF");
 			m["none"] = Color("#00000000");
-			m["black"] = Color("#000000");
-			m["gray"] = Color("#808080");
-			m["silver"] = Color("#C0C0C0");
+			m["black"] = Color("#000000FF");
+			m["gray"] = Color("#808080FF");
+			m["silver"] = Color("#C0C0C0FF");
 			}
 	} colors;
 
@@ -52,7 +52,7 @@ Color::Color(std::string const& str) {
 	ColorNames::Map::const_iterator it = colors.m.find(str);
 	if (it != colors.m.end()) { *this = it->second; return; }
 	std::clog << "color/warning: Unknown color: " << str << " (using magenta to hilight)" << std::endl;
-	*this = Color(1.0, 0.0, 1.0);
+	*this = Color(1.0, 0.0, 1.0, 1.0);
 }
 
 glmath::vec4 Color::linear() const {
@@ -60,16 +60,16 @@ glmath::vec4 Color::linear() const {
 }
 
 Color MicrophoneColor::get(std::string name) {
-	if (name == "black") return Color(3.0/255.0, 3.0/255.0, 3.0/255.0);
-	else if (name == "gray") return Color(24.0/255.0, 24.0/255.0, 24.0/255.0);
-	else if (name == "white") return Color(1.0, 1.0, 1.0);
-	else if (name == "aqua") return Color(0.0, 1.0, 1.0);
-	else if (name == "purple") return Color(63.0/255.0, 0, 1.0);
-	else if (name == "orange") return Color(1.0, 52.0/255.0, 0.0);
-	else if (name == "fuchsia") return Color(1.0, 0.06, 127/255.0);
-	else if (name == "yellow") return Color(1.0, 1.0, 0.0);
-	else if (name == "green") return Color(0.0, 1.0, 0.0);	
-	else if (name == "red") return Color(1, 0.0, 0.0);
-	else if (name == "blue") return Color(0.0, 43.75/255.0, 1.0);
-	else return Color(0.5, 0.5, 0.5);
+	if (name == "black") return Color(3.0/255.0, 3.0/255.0, 3.0/255.0, 1.0);
+	else if (name == "gray") return Color(24.0/255.0, 24.0/255.0, 24.0/255.0, 1.0);
+	else if (name == "white") return Color(1.0, 1.0, 1.0, 1.0);
+	else if (name == "aqua") return Color(0.0, 1.0, 1.0, 1.0);
+	else if (name == "purple") return Color(63.0/255.0, 0.0, 1.0, 1.0);
+	else if (name == "orange") return Color(1.0, 52.0/255.0, 0.0, 1.0);
+	else if (name == "fuchsia") return Color(1.0, 0.06, 127/255.0, 1.0);
+	else if (name == "yellow") return Color(1.0, 1.0, 0.0, 1.0);
+	else if (name == "green") return Color(0.0, 1.0, 0.0, 1.0);	
+	else if (name == "red") return Color(1, 0.0, 0.0, 1.0);
+	else if (name == "blue") return Color(0.0, 43.75/255.0, 1.0, 1.0);
+	else return Color(0.5, 0.5, 0.5, 1.0);
 }
