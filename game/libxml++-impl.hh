af69fe15cdbc895dd580d23dc7ba3c2760328134
L. Kärkkäinen <tronic@users.noreply.github.com>
2020-01-26 17:34:23 +0200

    Restore support for LibXML++-2.6.

--- game/libxml++-impl.hh
+++ game/libxml++-impl.hh
@@ -5,6 +5,21 @@
 #include <libxml++/libxml++.h>
 
 namespace xmlpp {
+#if LIBXMLPP_VERSION == 2
+	typedef NodeSet const_NodeSet; // implementation to satisfy libxml++ 2.6 API
+
+	static inline Element* add_child_element(Element* element, const Glib::ustring& name) {
+		return element->add_child(name);
+	}
+
+	static inline const TextNode* get_first_child_text(const Element& element) {
+		return element.get_child_text();
+	}
+
+	static inline void set_first_child_text(Element* element, const Glib::ustring& content) {
+		return element->set_child_text(content);
+	}
+#else  // Version 3
 	typedef Node::const_NodeSet const_NodeSet; // correct libxml++ 3.0 implementation
 
 	static inline Element* add_child_element(Element* element, const Glib::ustring& name) {
@@ -18,4 +33,5 @@ namespace xmlpp {
 	static inline void set_first_child_text(Element* element, const Glib::ustring& content) {
 		return element->set_first_child_text(content);
 	}
+#endif
 }
