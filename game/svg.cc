a74182592771c6bae43118b98b74d8020a6d52f8
Gregorio Litenstein <g.litenstein@gmail.com>
2020-03-24 17:16:31 -0300

    Fix some unnecessary dereferencing.

--- game/svg.cc
+++ game/svg.cc
@@ -4,26 +4,25 @@
 #include "configuration.hh"
 #include "image.hh"
 
-#include <iostream>
-#include <gio/gio.h>
 #include <librsvg/rsvg.h>
+#include <iostream>
+
+// Avoid deprecation messages with new versions since Ubuntu 12.10.
+#if LIBRSVG_MAJOR_VERSION * 10000 + LIBRSVG_MINOR_VERSION * 100 + LIBRSVG_MICRO_VERSION < 23602
+#include <librsvg/rsvg-cairo.h>
+#endif
 
 void loadSVG(Bitmap& bitmap, fs::path const& filename) {
 	double factor = config["graphic/svg_lod"].f();
 	// Try to load a cached PNG instead
 	if (cache::loadSVG(bitmap, filename, factor)) return;
 	std::clog << "image/debug: Loading SVG: " + filename.string() << std::endl;
-
 	// Open the SVG file in librsvg
 #if !GLIB_CHECK_VERSION(2, 36, 0)   // Avoid deprecation warnings
 	g_type_init();
 #endif
-
 	GError* pError = nullptr;
-    GFile *svgFile;
-	svgFile = g_file_new_for_path (filename.string().c_str());	
-	RsvgHandleFlags svgFlags = (RsvgHandleFlags) (RSVG_HANDLE_FLAG_UNLIMITED | RSVG_HANDLE_FLAG_KEEP_IMAGE_DATA);
-	std::shared_ptr<RsvgHandle> svgHandle(rsvg_handle_new_from_gfile_sync(svgFile, svgFlags, NULL, &pError), g_object_unref);
+	std::shared_ptr<RsvgHandle> svgHandle(rsvg_handle_new_from_file(filename.string().c_str(), &pError), g_object_unref);
 	if (pError) {
 		g_error_free(pError);
 		throw std::runtime_error("Unable to load " + filename.string());
@@ -35,34 +34,19 @@ void loadSVG(Bitmap& bitmap, fs::path const& filename) {
 	bitmap.resize(svgDimension.width*factor, svgDimension.height*factor);
 	bitmap.fmt = pix::INT_ARGB;
 	bitmap.linearPremul = true;
-    
-    RsvgPositionData pos;
-    rsvg_handle_get_position_sub (svgHandle.get(), &pos, nullptr);
 	// Raster with Cairo
 	std::shared_ptr<cairo_surface_t> surface(
-	  cairo_image_surface_create(CAIRO_FORMAT_ARGB32, bitmap.width, bitmap.height),
+	  cairo_image_surface_create_for_data(&bitmap.buf[0], CAIRO_FORMAT_ARGB32, bitmap.width, bitmap.height, bitmap.width * 4),
 	  cairo_surface_destroy);
 	std::shared_ptr<cairo_t> dc(cairo_create(surface.get()), cairo_destroy);
 	cairo_scale(dc.get(), factor, factor);
-    cairo_translate (dc.get(), -pos.x, -pos.y);
-    rsvg_handle_render_cairo_sub(svgHandle.get(), dc.get(), nullptr);
-	// Copy rendered image to our buffer.
-	if (bitmap.ptr) throw std::logic_error("Bitmap shouldn't point to a foreign object.");
-	bitmap.copyFromCairo(surface.get());
-
+	rsvg_handle_render_cairo(svgHandle.get(), dc.get());
 	// Change byte order from BGRA to RGBA
-
-	for (uint8_t *ptr = &*bitmap.buf.begin(), *end = ptr + bitmap.buf.size(); ptr < end; ptr += 4) {
-		uint8_t* b = ptr;
-		uint32_t pixel;
-		std::memcpy(&pixel, b, sizeof(uint32_t));
-		
-		b[0] = (pixel & 0xff0000) >> 16;
-		b[1] = (pixel & 0x00ff00) >> 8;
-		b[2] = (pixel & 0x0000ff) >> 0;		
-		b[3] = (pixel & 0xff000000) >> 24;
+	for (uint32_t *ptr = reinterpret_cast<uint32_t*>(bitmap.buf.data()), *end = ptr + bitmap.buf.size() / 4; ptr < end; ++ptr) {
+		uint8_t* pixel = reinterpret_cast<uint8_t*>(ptr);
+		uint8_t r = pixel[2], g = pixel[1], b = pixel[0], a = pixel[3];
+		pixel[0] = r; pixel[1] = g; pixel[2] = b; pixel[3] = a;
 	}
-	
 	bitmap.fmt = pix::CHAR_RGBA;
 	// Write to cache so that it can be loaded faster the next time
 	fs::path cache_filename = cache::constructSVGCacheFileName(filename, factor);
