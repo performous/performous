8dfd1ba094591fb6a4fd571d94a404793a5fc397
Gregorio Litenstein <g.litenstein@gmail.com>
2020-04-07 15:56:09 -0400

    Include ffmpeg headers without dirty hacks.
    Are these really still needed after 6 years?

--- game/config.cmake.hh
+++ game/config.cmake.hh
@@ -11,17 +11,6 @@
 
 #define SHARED_DATA_DIR "@SHARE_INSTALL@"
 
-// FFMPEG/libav libraries use changing include file names... Get them from CMake.
-#define AVCODEC_INCLUDE <@AVCodec_INCLUDE@>
-#define AVFORMAT_INCLUDE <@AVFormat_INCLUDE@>
-#define SWSCALE_INCLUDE <@SWScale_INCLUDE@>
-#define SWRESAMPLE_INCLUDE <@SWResample_INCLUDE@>
-//libav 0.9 fix
-#define AVUTIL_INCLUDE <@AVUtil_INCLUDE@>
-#define AVUTIL_OPT_INCLUDE <"@AVUtil_INCLUDE_DIRS@/libavutil/opt.h"> //HACK to get AVOption class!
-#define AVUTIL_MATH_INCLUDE <"@AVUtil_INCLUDE_DIRS@/libavutil/mathematics.h">
-#define AVUTIL_ERROR_INCLUDE <"@AVUtil_INCLUDE_DIRS@/libavutil/error.h">
-
 // libxml++ version
 #define LIBXMLPP_VERSION_2_6 @LibXML++_VERSION_2_6@
 #define LIBXMLPP_VERSION_3_0 @LibXML++_VERSION_3_0@
