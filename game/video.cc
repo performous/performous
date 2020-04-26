44f5b3775d9ddea0ab59466b0e9825a3d1420f71
Sébastien Gonzalve <sebastien.gonzalve@aliceadsl.fr>
2020-04-26 09:58:12 +0200

    Make some inheritance in ffmpeg + so cleanups

--- game/video.cc
+++ game/video.cc
@@ -6,21 +6,21 @@
 
 bool Video::tryPop(Bitmap& f, double timestamp) {
 	std::unique_lock<std::mutex> l(m_mutex);
-        
-        // if timestamp is out of the queue's range, ask a seek
-        // FIXME 4 should be linked to queue depth: we know the frame rate, thus how
-        // many frames needed for an arbitrary duration
-        m_seek_asked = timestamp > m_readPosition + 4 || timestamp < m_readPosition;
 
-        m_readPosition = timestamp;
+	// if timestamp is out of the queue's range, ask a seek
+	// FIXME 4 should be linked to queue depth: we know the frame rate, thus how
+	// many frames needed for an arbitrary duration
+	m_seek_asked = timestamp > m_readPosition + 4 || timestamp < m_readPosition;
 
-        // if queue is not empty, we are sure will remove at least one element from it or as fr seek.
-        if (!m_queue.empty()) m_cond.notify_all();
+	m_readPosition = timestamp;
 
-        if (m_seek_asked) return false;
-        
-        // discard outdated frames retaining only the most recent frame that is _before_ timestamp
-        while (!m_queue.empty() && std::next(m_queue.begin()) != m_queue.end() && std::next(m_queue.begin())->timestamp < timestamp) m_queue.pop_front();
+	// if queue is not empty, we are sure will remove at least one element from it or as fr seek.
+	if (!m_queue.empty()) m_cond.notify_all();
+
+	if (m_seek_asked) return false;
+
+	// discard outdated frames retaining only the most recent frame that is _before_ timestamp
+	while (!m_queue.empty() && std::next(m_queue.begin()) != m_queue.end() && std::next(m_queue.begin())->timestamp < timestamp) m_queue.pop_front();
 
 	if (m_queue.empty() || m_queue.front().timestamp > timestamp) return false; // Nothing to deliver
 
@@ -32,66 +32,65 @@ bool Video::tryPop(Bitmap& f, double timestamp) {
 void Video::push(Bitmap&& f) {
 	std::unique_lock<std::mutex> l(m_mutex);
 	m_cond.wait(l, [this]{ return m_quit || m_seek_asked || m_queue.size() < m_max; });
-        if (m_quit || m_seek_asked) return; // Drop frame when seek/quit asked
+	if (m_quit || m_seek_asked) return; // Drop frame when seek/quit asked
 	m_queue.emplace_back(std::move(f));
 }
 
 Video::~Video() { 
-    {
-        std::lock_guard<std::mutex> l(m_mutex);
-        m_quit = true;
-    }
-    m_cond.notify_all();
-    m_grabber.get();
+	{
+		std::lock_guard<std::mutex> l(m_mutex);
+		m_quit = true;
+	}
+	m_cond.notify_all();
+	m_grabber.get();
 }
 
 Video::Video(fs::path const& _videoFile, double videoGap): m_videoGap(videoGap), m_textureTime(), m_alpha(-0.5, 1.5) {
-   
-	auto ffmpeg = std::make_unique<FFmpeg>(_videoFile, 0, nullptr, [this] (auto f) { push(std::move(f)); });
-   m_grabber = std::async(std::launch::async, [this, file = _videoFile, ffmpeg = std::move(ffmpeg)] {
-	int errors = 0;
-	std::unique_lock<std::mutex> l(m_mutex);
-	while (!m_quit) {
+	// make ffmpeg here to get any exception in the current thread
+	auto ffmpeg = std::make_unique<VideoFFmpeg>(_videoFile, [this] (auto f) { push(std::move(f)); });
+	m_grabber = std::async(std::launch::async, [this, file = _videoFile, ffmpeg = std::move(ffmpeg)] {
+			int errors = 0;
+			std::unique_lock<std::mutex> l(m_mutex);
+			while (!m_quit) {
+				if (m_seek_asked) {
+					m_seek_asked = false;
 
-                if (m_seek_asked) {
-                    m_seek_asked = false;
+					auto seek_pos = m_readPosition;
+					// discard all outdated frame. To avoid races between clean and push, clean and push are done in this thread.
+					m_queue.clear();
+					l.unlock();
+					ffmpeg->seek(seek_pos);  // -5 to workaround ffmpeg inaccurate seeking
+				}
+				else l.unlock();
 
-                    auto seek_pos = m_readPosition;
-                    // discard all outdated frame. Do it here to avoid races between clean and push, because push are done in this thread.
-                    m_queue.clear();
-                    l.unlock();
-                    ffmpeg->seek(std::max(0.0, seek_pos - 5.0));  // -5 to workaround ffmpeg inaccurate seeking
-                }
-                else l.unlock();
-
-		try {
-			ffmpeg->handleOneFrame();
-		} catch (FFmpeg::eof_error&) {
-			push(Bitmap()); // EOF marker
-			std::clog << "ffmpeg/debug: done loading " << file << std::endl;
-		} catch (std::exception& e) {
-			std::clog << "ffmpeg/error: " << file << ": " << e.what() << std::endl;
-			if (++errors > 2) { std::clog << "ffmpeg/error: FFMPEG terminating due to multiple errors" << std::endl; break; }
-		}
-		errors = 0;
-                l.lock();
-	}
-   });
+				try {
+					ffmpeg->handleOneFrame();
+				} catch (FFmpeg::eof_error&) {
+					push(Bitmap()); // EOF marker
+					std::clog << "ffmpeg/debug: done loading " << file << std::endl;
+				} catch (std::exception& e) {
+					std::clog << "ffmpeg/error: " << file << ": " << e.what() << std::endl;
+					if (++errors > 2) { std::clog << "ffmpeg/error: FFMPEG terminating due to multiple errors" << std::endl; break; }
+				}
+				errors = 0;
+				l.lock();
+			}
+	});
 }
 
 void Video::prepare(double time) {
-        if (std::isnan(time)) return;
+	if (std::isnan(time)) return;
 
-        // shift video timestamp if gap is declared in song config
-        time += m_videoGap;
+	// shift video timestamp if gap is declared in song config
+	time += m_videoGap;
 
-        // Time to switch frame?
-        tryPop(m_videoFrame, time);
+	// Time to switch frame?
+	tryPop(m_videoFrame, time);
 
-        if (!m_videoFrame.buf.empty()) {
-            m_texture.load(m_videoFrame);
-            m_textureTime = m_videoFrame.timestamp;
-        }
+	if (!m_videoFrame.buf.empty()) {
+		m_texture.load(m_videoFrame);
+		m_textureTime = m_videoFrame.timestamp;
+	}
 }
 
 void Video::render(double time) {
