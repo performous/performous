731299ee71419204ad0cbecaadc3b76ee75021be
Sébastien Gonzalve <sebastien.gonzalve@aliceadsl.fr>
2020-05-01 21:10:49 +0200

    Reindent properly

--- game/ffmpeg.hh
+++ game/ffmpeg.hh
@@ -33,31 +33,31 @@ extern "C" {
 /// ffmpeg class
 class FFmpeg {
   public:
-        // Exceptions thrown by class
+	// Exceptions thrown by class
 	class Eof: public std::exception {};
-        class Error;
-        friend Error;
+	class Error;
+	friend Error;
 
 	/// Decode file, depending on media type audio.
-        FFmpeg(fs::path const& filename, int mediaType);
-        
-        void handleOneFrame();
+	FFmpeg(fs::path const& filename, int mediaType);
 
-        /** Seek to the chosen time. **/
+	void handleOneFrame();
+
+	/** Seek to the chosen time. **/
 	virtual void seek(double time);
 
 	/// duration
 	double duration() const;
 
-        virtual ~FFmpeg() = default;
+	virtual ~FFmpeg() = default;
         
   protected:
 	static void frameDeleter(AVFrame *f) { if (f) av_frame_free(&f); }
 	using uFrame = std::unique_ptr<AVFrame, std::integral_constant<decltype(&frameDeleter), &frameDeleter>>;
 
-        virtual void processFrame(uFrame frame) = 0;
+	virtual void processFrame(uFrame frame) = 0;
 
-        struct Packet;
+	struct Packet;
 	void decodePacket(Packet &);
         
 	static void avformat_close_input(AVFormatContext *fctx);
@@ -73,28 +73,28 @@ class FFmpeg {
 };
 
 class AudioFFmpeg : public FFmpeg {
-public:
-        using AudioCb = std::function<void(const std::int16_t *data, size_t count, int64_t sample_position)>;
-        AudioFFmpeg(fs::path const& file, unsigned int rate, AudioCb audioCb);
+  public:
+	using AudioCb = std::function<void(const std::int16_t *data, size_t count, int64_t sample_position)>;
+	AudioFFmpeg(fs::path const& file, unsigned int rate, AudioCb audioCb);
 
 	void seek(double time) override;
-protected:
-        void processFrame(uFrame frame) override;
-    private:
-        int64_t m_position_in_48k_frames = -1;
+  protected:
+	void processFrame(uFrame frame) override;
+  private:
+	int64_t m_position_in_48k_frames = -1;
 	unsigned int m_rate = 0;
-        AudioCb handleAudioData;
+	AudioCb handleAudioData;
 	std::unique_ptr<SwrContext, void(*)(SwrContext*)> m_resampleContext{nullptr, [] (auto p) { swr_close(p); swr_free(&p); }};
 };
 
 class VideoFFmpeg : public FFmpeg {
-public:
-        using VideoCb = std::function<void(Bitmap)>;
-        VideoFFmpeg(fs::path const& file, VideoCb videoCb);
+  public:
+	using VideoCb = std::function<void(Bitmap)>;
+	VideoFFmpeg(fs::path const& file, VideoCb videoCb);
 
-protected:
-        void processFrame(uFrame frame) override;
-private:
+  protected:
+	void processFrame(uFrame frame) override;
+  private:
 	std::unique_ptr<SwsContext, void(*)(SwsContext*)> m_swsContext{nullptr, sws_freeContext};
         VideoCb handleVideoData;
 
@@ -102,17 +102,17 @@ private:
 
 class AudioBuffer {
   public:
-        using uFvec = std::unique_ptr<fvec_t, std::integral_constant<decltype(&del_fvec), &del_fvec>>;
+	using uFvec = std::unique_ptr<fvec_t, std::integral_constant<decltype(&del_fvec), &del_fvec>>;
 
 	AudioBuffer(fs::path const& file, unsigned int rate, size_t size = 4320256);
-        ~AudioBuffer();
+	~AudioBuffer();
 	
 	uFvec makePreviewBuffer();
 	void operator()(const std::int16_t *data, size_t count, int64_t sample_position);
 	bool prepare(std::int64_t pos);
 	bool read(float* begin, size_t count, std::int64_t pos, float volume = 1.0f);
-        bool terminating();
-        double duration();
+	bool terminating();
+	double duration();
 
   private:
 	// must be called holding the mutex
@@ -134,11 +134,8 @@ class AudioBuffer {
 	std::int64_t m_eof_pos = -1; // -1 until we get the read end from ffmpeg
 
 	const unsigned m_sps;
-
-        const double m_duration{ 0 };
-
-        bool m_seek_asked { false };
-
+	const double m_duration{ 0 };
+	bool m_seek_asked { false };
 	bool m_quit{ false };
 	std::future<void> reader_thread;
 };
