565579ab336000b51ecd675c0645c7bbd8229ac7
Sébastien Gonzalve <sebastien.gonzalve@aliceadsl.fr>
2020-05-01 21:10:49 +0200

    Reindent properly

--- game/video.hh
+++ game/video.hh
@@ -11,7 +11,7 @@ class Video {
   public:
 	/// opens given video file
 	Video(fs::path const& videoFile, double videoGap = 0.0);
-        ~Video();
+	~Video();
 	void prepare(double time);  ///< Load the current video frame into a texture
 	void render(double time);  ///< Render the prepared video frame
 	/// returns Dimensions of video clip
@@ -22,26 +22,26 @@ class Video {
 	Bitmap m_videoFrame;
 	Texture m_texture;
 	double m_textureTime;
-        double m_readPosition = 0;
+	double m_readPosition = 0;
 	AnimValue m_alpha;
-        bool m_quit{false};
-        std::future<void> m_grabber;
+	bool m_quit{false};
+	std::future<void> m_grabber;
 
-        /// trys to pop a video frame from queue
-        bool tryPop(Bitmap& f, double timestamp);
-        /// Add frame to queue
-        void push(Bitmap&& f);
-        /// Clear and unlock the queue
-        void reset();
-        /// return timestamp of next frame to read
-        double headPosition() const { return m_queue.front().timestamp; }
-        /// return timestamp of next frame to read
-        double backPosition() const { return m_queue.back().timestamp; }
+	/// trys to pop a video frame from queue
+	bool tryPop(Bitmap& f, double timestamp);
+	/// Add frame to queue
+	void push(Bitmap&& f);
+	/// Clear and unlock the queue
+	void reset();
+	/// return timestamp of next frame to read
+	double headPosition() const { return m_queue.front().timestamp; }
+	/// return timestamp of next frame to read
+	double backPosition() const { return m_queue.back().timestamp; }
 
-        std::deque<Bitmap> m_queue;
-        mutable std::mutex m_mutex;
-        std::condition_variable m_cond;
-        static const unsigned m_max = 20;
-        bool m_seek_asked{false};
+	std::deque<Bitmap> m_queue;
+	mutable std::mutex m_mutex;
+	std::condition_variable m_cond;
+	static const unsigned m_max = 20;
+	bool m_seek_asked{false};
 };
 
