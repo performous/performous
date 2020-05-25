c14eeaf2d6aa62f30e656d8dea5ffdb736abad96
Gregorio Litenstein <g.litenstein@gmail.com>
2020-05-25 01:22:34 -0400

    Enable song-caching even on non-webserver builds.
    The only real reason to disable it before was the CppRest dependency but we’re using a submodule now.

--- game/song.cc
+++ game/song.cc
@@ -12,7 +12,6 @@ extern "C" {
 #include AVFORMAT_INCLUDE
 #include AVCODEC_INCLUDE
 }
-#ifdef USE_WEBSERVER
 
 Song::Song(nlohmann::json const& song): dummyVocal(TrackName::LEAD_VOCAL), randomIdx(rand()) {
 	std::string temp;
@@ -92,7 +91,6 @@ Song::Song(nlohmann::json const& song): dummyVocal(TrackName::LEAD_VOCAL), rando
 	}
 	collateUpdate();
 }
-#endif
 
 Song::Song(fs::path const& path, fs::path const& filename):
   dummyVocal(TrackName::LEAD_VOCAL), path(path), filename(filename), randomIdx(rand())
