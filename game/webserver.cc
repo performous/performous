2620042df5d9eb27dc5faa95b13064bbabc4ba9c
Gregorio Litenstein <g.litenstein@gmail.com>
2020-06-07 14:21:59 -0400

    Implement option to restart WebServer.

--- game/webserver.cc
+++ game/webserver.cc
@@ -7,6 +7,10 @@
 #include <boost/asio/ip/address_v4_range.hpp>
 
 void WebServer::startServer(int tried, bool fallbackPortInUse) {
+	if(config["webserver/access"].i() == 0) {
+		std::clog << "webserver/notice: Not starting webserver because it's been explicitly turned off." << std::endl;
+		return;
+	}
 	if(tried > 2) {
 			std::string message("webserver/error: Couldn't start webserver after 3 tries");
 		if(fallbackPortInUse == false) {
@@ -71,16 +75,10 @@ void WebServer::startServer(int tried, bool fallbackPortInUse) {
 WebServer::WebServer(Songs& songs)
 : m_songs(songs)
 {
-	if(config["webserver/access"].i() == 0) {
-		std::clog << "webserver/notice: Not starting webserver." << std::endl;
-	}
-	else {
-		m_serverThread = std::make_unique<std::thread>([this] { startServer(0, false); });
-	}
-}
+	restartServer();
 }
 
-WebServer::~WebServer() {
+void WebServer::stopServer() {
 	try {
 		if (m_server) {
 			m_server->m_restinio_server->close_sync();
@@ -89,6 +87,16 @@ WebServer::~WebServer() {
 		if (m_serverThread) { m_serverThread->detach(); } // Using join results in a potential crash and/or locks-up forever waiting for the thread.
 	} catch (const std::exception &e) {
 		std::clog << "webserver/error: Failed to close RESTinio server due to: " << e.what() << "." << std::endl;
-		}
+	}
+}
+
+void WebServer::restartServer() {
+	stopServer();
+	m_serverThread = std::make_unique<std::thread>([this] { startServer(0, false); });
+}
+
+
+WebServer::~WebServer() {
+	stopServer();
 }
 #endif
