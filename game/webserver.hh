e8078491c036a22d2deccf7d2ad7120f7ea944c9
Gregorio Litenstein <g.litenstein@gmail.com>
2020-06-08 13:42:39 -0400

    Add documentation to webserver classes

--- game/webserver.hh
+++ game/webserver.hh
@@ -12,14 +12,14 @@ class WebServer
 public:
 	WebServer(Songs& songs);
 	~WebServer();
-	void restartServer();
+	void restartServer(); ///< Public interface to restart WebServer if configuration changes.
 	
 private:
-	void startServer(int tried, bool fallbackPortInUse);
-	std::unique_ptr<std::thread> m_serverThread = nullptr;
-	std::unique_ptr<RequestHandler> m_server = nullptr;
-	Songs& m_songs;
+	void startServer(int tried, bool fallbackPortInUse); ///< Start the WebServer.
 	void stopServer(); ///< Stop the WebServer; called before restarting and in the destructor.
+	std::unique_ptr<std::thread> m_serverThread = nullptr; ///< Thread responsible for initializing the RequestHandler.
+	std::unique_ptr<RequestHandler> m_server = nullptr; ///< The actual server.
+	Songs& m_songs; ///< Reference to the Songs database.
 };
 #else
 
