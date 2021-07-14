bdcd88a5692b53a8af80a05f2d81529f4831c94b
Arjan Speiard | Asgard Sings! <arjan.spieard@gmail.com>
2021-07-14 20:24:42 +0200

    Uppercase SET to lowercase

--- cmake/Modules/FindGLM.cmake
+++ cmake/Modules/FindGLM.cmake
@@ -48,20 +48,20 @@
 #=============================================================================
 
 # default search dirs
-SET(_glm_HEADER_SEARCH_DIRS
+set(_glm_HEADER_SEARCH_DIRS
     "/usr/include"
     "/usr/local/include")
 
 # check environment variable
-SET(_glm_ENV_ROOT_DIR "$ENV{GLM_ROOT_DIR}")
+set(_glm_ENV_ROOT_DIR "$ENV{GLM_ROOT_DIR}")
 
 IF(NOT GLM_ROOT_DIR AND _glm_ENV_ROOT_DIR)
-    SET(GLM_ROOT_DIR "${_glm_ENV_ROOT_DIR}")
+    set(GLM_ROOT_DIR "${_glm_ENV_ROOT_DIR}")
 ENDIF(NOT GLM_ROOT_DIR AND _glm_ENV_ROOT_DIR)
 
 # put user specified location at beginning of search
 IF(GLM_ROOT_DIR)
-    SET(_glm_HEADER_SEARCH_DIRS "${GLM_ROOT_DIR}"
+    set(_glm_HEADER_SEARCH_DIRS "${GLM_ROOT_DIR}"
                                 "${GLM_ROOT_DIR}/include"
                                  ${_glm_HEADER_SEARCH_DIRS})
 ENDIF(GLM_ROOT_DIR)
@@ -75,7 +75,7 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(GLM DEFAULT_MSG
     GLM_INCLUDE_DIR)
 
 IF(GLM_FOUND)
-    SET(GLM_INCLUDE_DIRS "${GLM_INCLUDE_DIR}")
+    set(GLM_INCLUDE_DIRS "${GLM_INCLUDE_DIR}")
 
     MESSAGE(STATUS "GLM_INCLUDE_DIR = ${GLM_INCLUDE_DIR}")
 ENDIF(GLM_FOUND)
