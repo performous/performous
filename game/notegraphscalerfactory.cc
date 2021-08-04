5c442e549e3da8cedfb19a9432617e5ec19e4a04
Tobias Wollgam <tobias@wollgam.de>
2021-08-04 23:07:09 +0200

    Change fix to fixed
    Change all names containing fix to fixed:
    
    - file names
    - class name
    - enum in schema.xml

--- game/notegraphscalerfactory.cc
+++ game/notegraphscalerfactory.cc
@@ -1,15 +1,15 @@
 #include "notegraphscalerfactory.hh"
 
 #include "dynamicnotegraphscaler.hh"
-#include "fixnotegraphscaler.hh"
+#include "fixednotegraphscaler.hh"
 
 #include "configuration.hh"
 
 NoteGraphScalerPtr NoteGraphScalerFactory::create() const {
-	const auto scalingMode = config["game/notegraphscalingmode"].i();
+    const auto scalingMode = config["game/notegraphscalingmode"].i();
 
-	if(scalingMode == 1)
-		return std::make_shared<FixNoteGraphScaler>();
-		
-	return std::make_shared<DynamicNoteGraphScaler>();
+    if(scalingMode == 1)
+        return std::make_shared<FixedNoteGraphScaler>();
+        
+    return std::make_shared<DynamicNoteGraphScaler>();
 }
