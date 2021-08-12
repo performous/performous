61cbbd45c4f07cb1ec85bde7e3c8ba0fdd1f7f50
Tobias Wollgam <tobias@wollgam.de>
2021-08-12 19:54:17 +0200

    Add more Auto options
    Add options for Auto with 1, 1.5, 2 and 2.5 octaves range.

--- game/notegraphscalerfactory.cc
+++ game/notegraphscalerfactory.cc
@@ -9,17 +9,21 @@
 NoteGraphScalerPtr NoteGraphScalerFactory::create(VocalTrack const& vocal) const {
     const auto scalingMode = config["game/notegraphscalingmode"].i();
 
-    if(scalingMode == 2) {
-        const auto octaves = vocal.noteMax - vocal.noteMin;
-        
-        if(octaves <= 18)
-            return std::make_shared<FixedNoteGraphScaler>();
-
+    if(scalingMode == 0)
         return std::make_shared<DynamicNoteGraphScaler>();
-    }
-    
     if(scalingMode == 1)
         return std::make_shared<FixedNoteGraphScaler>();
         
-    return std::make_shared<DynamicNoteGraphScaler>();
+    const auto octaves = vocal.noteMax - vocal.noteMin;
+
+    if(scalingMode == 2 && octaves <= 12)
+        return std::make_shared<FixedNoteGraphScaler>();
+    if(scalingMode == 3 && octaves <= 18)
+        return std::make_shared<FixedNoteGraphScaler>();
+    if(scalingMode == 4 && octaves <= 24)
+        return std::make_shared<FixedNoteGraphScaler>();
+    if(scalingMode == 5 && octaves <= 30)
+        return std::make_shared<FixedNoteGraphScaler>();
+    
+     return std::make_shared<DynamicNoteGraphScaler>();
 }
