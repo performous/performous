8f9b0d2c05b14c4ee70a38be2bde1689d061b778
L. Kärkkäinen <tronic@users.noreply.github.com>
2021-08-31 21:11:12 +0100

    Adjust mic required dB levels higher to reduce noise.

--- game/pitch.cc
+++ game/pitch.cc
@@ -127,7 +127,7 @@ void Analyzer::calcTones() {
 	const double stepRate = m_rate / m_step;  // Steps per second
 	const double phaseStep = double(m_step) / FFT_N;
 	const double normCoeff = 1.0 / FFT_N;
-	const double minMagnitude = pow(10, -100.0 / 20.0) / normCoeff; // -100 dB
+	const double minMagnitude = pow(10, -80.0 / 20.0) / normCoeff; // -80 dB
 	// Limit frequency range of processing
 	const size_t kMin = std::max(size_t(1), size_t(FFT_MINFREQ / freqPerBin));
 	const size_t kMax = std::min(FFT_N / 2, size_t(FFT_MAXFREQ / freqPerBin));
@@ -155,7 +155,7 @@ void Analyzer::calcTones() {
 	// Find the tones (collections of harmonics) from the array of peaks
 	tones_t tones;
 	for (size_t k = kMax - 1; k >= kMin; --k) {
-		if (peaks[k].db < -70.0) continue;
+		if (peaks[k].db < -60.0) continue;
 		// Find the best divider for getting the fundamental from peaks[k]
 		std::size_t bestDiv = 1;
 		int bestScore = 0;
@@ -165,7 +165,7 @@ void Analyzer::calcTones() {
 			for (std::size_t n = 1; n < div && n < 8; ++n) {
 				Peak& p = match(peaks, k * n / div);
 				--score;
-				if (p.db < -90.0 || std::abs(p.freq / n / freq - 1.0) > .03) continue;
+				if (p.db < -80.0 || std::abs(p.freq / n / freq - 1.0) > .03) continue;
 				if (n == 1) score += 4; // Extra for fundamental
 				score += 2;
 			}
@@ -193,7 +193,7 @@ void Analyzer::calcTones() {
 		}
 		t.freq /= count;
 		// If the tone seems strong enough, add it (-3 dB compensation for each harmonic)
-		if (t.db > -50.0 - 3.0 * count) {
+		if (t.db > -40.0 - 3.0 * count) {
 			t.stabledb = t.db;
 			tones.push_back(t);
 		}
@@ -215,7 +215,7 @@ void Analyzer::mergeWithOld(tones_t& tones) const {
 			it->age = old.age + 1;
 			it->stabledb = 0.8 * old.stabledb + 0.2 * it->db;
 			it->freq = 0.5 * old.freq + 0.5 * it->freq;
-		} else if (old.db > -80.0) {
+		} else if (old.db > -70.0) {
 			// Insert a decayed version of the old tone into new tones
 			Tone& t = *tones.insert(it, old);
 			t.db -= 5.0;
