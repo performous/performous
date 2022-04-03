55fe80d3eda6e4ff491399525f7fb99352740da4
Arjan Speiard | Asgard Sings! <arjan.spieard@gmail.com>
2022-04-03 19:48:05 +0200

    Differences in linking on windows and unix

--- cmake/Modules/FindCppRest.cmake
+++ cmake/Modules/FindCppRest.cmake
@@ -10,7 +10,12 @@
 
 include(LibFindMacros)
 
-libfind_package(CppRest OpenSSL)
+if(WIN32)
+	libfind_package(CppRest OpenSSL)
+else()
+	libfind_package(CppRest Crypto)
+	libfind_package(CppRest Ssl)
+endif()
 
 libfind_pkg_detect(CppRest cpprest FIND_PATH http_client.h PATH_SUFFIXES cpprest FIND_LIBRARY cpprest cpprest_2_10d)
 set(CppRest_VERSION ${CppRest_PKGCONF_VERSION})
