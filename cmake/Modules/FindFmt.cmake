0e3216323a5c4f0afb88e5497d58fbaa72a8201f
Arjan Speiard | Asgard Sings! <arjan.spieard@gmail.com>
2022-04-04 22:19:34 +0200

    Add cmake fetchContent method if fmt is not found

--- cmake/Modules/FindFmt.cmake
+++ cmake/Modules/FindFmt.cmake
@@ -13,3 +13,14 @@ include(LibFindMacros)
 libfind_pkg_detect(Fmt fmt FIND_PATH fmt/core.h fmt/format.h FIND_LIBRARY fmt)
 set(Fmt_VERSION ${Fmt_PKGCONF_VERSION})
 libfind_process(Fmt)
+
+if (NOT Fmt_FOUND)
+    message("Building fmt from sources...")
+    include(FetchContent)
+    FetchContent_Declare(fmt-sources
+      GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+      GIT_TAG        b6f4ceaed0a0a24ccf575fab6c56dd50ccf6f1a9
+      SOURCE_DIR fmt-src
+    )
+    FetchContent_MakeAvailable(fmt-sources)
+endif()
