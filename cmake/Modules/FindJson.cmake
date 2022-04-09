19385f30dd89b63d35e8f257b4e4fbe41961c3d4
Vincent Le Ligeour <yoda-jm@users.sourceforge.net>
2022-04-10 01:02:06 +0200

    fix: allow fetching nlohmann_json when not found on system

--- cmake/Modules/FindJson.cmake
+++ cmake/Modules/FindJson.cmake
@@ -1,4 +1,4 @@
-# - Try to find nlohmann_json
+# - Try to find nlohmann_json, if not found, download it from github
 # Once done, this will define
 #
 #  Json_FOUND - system has nlohmann_json
@@ -10,6 +10,23 @@
 
 include(LibFindMacros)
 
+set(Json_FIND_REQUIRED FALSE)
+set(Json_FIND_QUIETLY TRUE)
 libfind_pkg_detect(Json nlohmann_json FIND_PATH nlohmann/json.hpp)
-set(Json_VERSION ${Json_PKGCONF_VERSION})
 libfind_process(Json)
+
+if (Json_FOUND)
+    set(Json_VERSION ${Json_PKGCONF_VERSION})
+else ()
+    message("-- Fetching nlohmann_json from github...")
+    include(FetchContent)
+    set(Json_VERSION "3.10.5")
+    FetchContent_Declare(nlohmann_json
+      GIT_REPOSITORY https://github.com/nlohmann/json.git
+      GIT_TAG        v${Json_VERSION}
+      SOURCE_DIR     nlohmann_json
+    )
+    FetchContent_MakeAvailable(nlohmann_json)
+    set(Json_INCLUDE_DIRS ${nlohmann_json_SOURCE_DIR}/include/)
+    set(Json_FOUND TRUE)
+endif()
