28cdd89269f048bafe8396107c92101cbb964563
Vincent Le Ligeour <yoda-jm@users.sourceforge.net>
2022-04-12 16:36:18 +0200

    refactor: make usage of self-build aubio clearer
    In order to do this, the following flags are added to CMake:
    - ALLOW_SELF_BUILT_AUBIO (default to false): tells CMake that he is
      allowed to fetch aubio and build it from sources if no valid aubio
      intallation is found on the system (not found or not recent enough),
    - FORCE_SELF_BUILT_AUBIO (default to false): tells CMake to always fetch
      aubio and build it from sources (even if a valid version is found on
      the system).

--- cmake/Modules/FindAubio.cmake
+++ cmake/Modules/FindAubio.cmake
@@ -1,25 +1,39 @@
 include(LibFindMacros)
 
-set(Aubio_FIND_REQUIRED FALSE)
-set(Aubio_FIND_QUIETLY TRUE)
-libfind_pkg_detect(Aubio aubio FIND_PATH aubio.h PATH_SUFFIXES aubio FIND_LIBRARY aubio)
-libfind_process(Aubio)
+function(_buildAubioFromSource)
+	message(STATUS "Fetching aubio from github...")
+	include(FetchContent)
+	set(Aubio_VERSION "0.4.9-performous" PARENT_SCOPE)
+	FetchContent_Declare(aubio_from_source
+		GIT_REPOSITORY https://github.com/performous/aubio.git
+		GIT_SHALLOW    TRUE
+		GIT_TAG        14fec3da6749fbcc47b56648d7a38296eccd9499
+		SOURCE_DIR aubio-src
+	)
+	FetchContent_MakeAvailable(aubio_from_source)
+	set(Aubio_INCLUDE_DIRS ${aubio_from_source_SOURCE_DIR}/include/ PARENT_SCOPE)
+	#set(Aubio_LIBRARIES "-laubio" PARENT_SCOPE)
+	set(Aubio_FOUND TRUE PARENT_SCOPE)
+endfunction()
 
-if (Aubio_FOUND)
+if(FORCE_SELF_BUILT_AUBIO)
+	message(STATUS "aubio forced to build from source")
+	_buildAubioFromSource()
+	message(STATUS "Found Aubio ${Aubio_VERSION}")
+elseif(NOT ALLOW_SELF_BUILT_AUBIO)
+	libfind_pkg_detect(Aubio aubio FIND_PATH aubio.h PATH_SUFFIXES aubio FIND_LIBRARY aubio)
+	libfind_process(Aubio)
 	set(Aubio_VERSION ${Aubio_PKGCONF_VERSION})
-else ()
-    message("-- Fetching aubio from github...")
-    include(FetchContent)
-    set(Aubio_VERSION "0.4.9-performous")
-    FetchContent_Declare(aubio_from_source
-      GIT_REPOSITORY https://github.com/performous/aubio.git
-      GIT_SHALLOW    TRUE
-      GIT_TAG        14fec3da6749fbcc47b56648d7a38296eccd9499
-      SOURCE_DIR aubio-src
-    )
-    FetchContent_MakeAvailable(aubio_from_source)
-    set(Aubio_INCLUDE_DIRS ${aubio_from_source_SOURCE_DIR}/include/)
-    set(Aubio_LIBRARIES "-laubio")
-    set(Aubio_FOUND TRUE)
+else()
+	set(Aubio_FIND_REQUIRED FALSE)
+	set(Aubio_FIND_QUIETLY TRUE)
+	libfind_pkg_detect(Aubio aubio FIND_PATH aubio.h PATH_SUFFIXES aubio FIND_LIBRARY aubio)
+	libfind_process(Aubio)
+	if(NOT Aubio_FOUND)
+		message(STATUS "aubio build from source because not found on system")
+		_buildAubioFromSource()
+	else()
+		set(Aubio_VERSION ${Aubio_PKGCONF_VERSION})
+	endif()
+	message(STATUS "Found Aubio ${Aubio_VERSION}")
 endif()
-message(STATUS "Found Aubio ${Aubio_VERSION}")
