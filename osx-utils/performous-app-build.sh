eceb3669072dc4dbcbfcd214564df53e834a0a85
Vincent Le Ligeour <yoda-jm@users.sourceforge.net>
2022-04-13 14:07:18 +0200

    fix: use new dependency download flag in OSX build

--- osx-utils/performous-app-build.sh
+++ osx-utils/performous-app-build.sh
@@ -212,8 +212,8 @@ function main {
 	  -DCMAKE_OSX_ARCHITECTURES="x86_64" \
 	  -B "${PERFORMOUS_SOURCE}/build" \
 	  -S "${PERFORMOUS_SOURCE}" \
-	  -DALLOW_SELF_BUILT_AUBIO="TRUE" \
-	  -DALLOW_SELF_BUILT_JSON="TRUE"
+	  -DSELF_BUILT_AUBIO="AUTO" \
+	  -DSELF_BUILT_JSON="AUTO"
 	
 	make -C "${PERFORMOUS_SOURCE}/build" -j${MAKE_JOBS} install # You can change the -j value in order to spawn more build threads.
 
