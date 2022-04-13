4c4c688ce508bd5bb040fe2e5987567031e05882
Vincent Le Ligeour <yoda-jm@users.sourceforge.net>
2022-04-13 13:36:43 +0200

    new: allow to specify an alternate git root for dependencies
    This become handy for developing in mobility (for example in
    the train with faulty internet access) because the dependencies
    are cloned inside the build directory and cleaning all the build
    directory to test some build options might also remove those
    preciously fetched sources.

--- cmake/Modules/FindJson.cmake
+++ cmake/Modules/FindJson.cmake
@@ -16,7 +16,7 @@ function(_buildJsonFromSource)
 	set(Json_VERSION "3.10.5")
 	set(Json_VERSION "3.10.5" PARENT_SCOPE)
 	FetchContent_Declare(nlohmann_json
-	  GIT_REPOSITORY https://github.com/performous/json.git
+	  GIT_REPOSITORY ${SELF_BUILT_GIT_BASE}/json.git
 	  GIT_SHALLOW    TRUE
 	  GIT_TAG        v${Json_VERSION}
 	  SOURCE_DIR     nlohmann_json
