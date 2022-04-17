8fc171cf126c6dda44201e7455b0a5bf67222c9d
Vincent Le Ligeour <yoda-jm@users.sourceforge.net>
2022-04-17 15:49:09 +0200

    refactor: fetching and making available dependency packages

--- cmake/Modules/FindAubio.cmake
+++ cmake/Modules/FindAubio.cmake
@@ -1,24 +1,15 @@
 include(LibFindMacros)
+include(LibFetchMacros)
 
-function(_buildAubioFromSource)
-	message(STATUS "Fetching aubio from github...")
-	include(FetchContent)
-	set(Aubio_VERSION "0.4.9-performous" PARENT_SCOPE)
-	FetchContent_Declare(aubio_from_source
-		GIT_REPOSITORY ${SELF_BUILT_GIT_BASE}/aubio.git
-		GIT_SHALLOW    TRUE
-		GIT_TAG        14fec3da6749fbcc47b56648d7a38296eccd9499
-		SOURCE_DIR aubio-src
-	)
-	FetchContent_MakeAvailable(aubio_from_source)
-	set(Aubio_INCLUDE_DIRS ${aubio_from_source_SOURCE_DIR}/include/ PARENT_SCOPE)
-	#set(Aubio_LIBRARIES "-laubio" PARENT_SCOPE)
-	set(Aubio_FOUND TRUE PARENT_SCOPE)
-endfunction()
+set(Aubio_GIT_VERSION "14fec3da6749fbcc47b56648d7a38296eccd9499")
 
 if(SELF_BUILT_AUBIO STREQUAL "ALWAYS")
 	message(STATUS "aubio forced to build from source")
-	_buildAubioFromSource()
+	libfetch_git_pkg(Aubio
+		NAME aubio
+		REPOSITORY ${SELF_BUILT_GIT_BASE}/aubio.git
+		REFERENCE  ${Aubio_GIT_VERSION}
+	)
 	message(STATUS "Found Aubio ${Aubio_VERSION}")
 elseif(SELF_BUILT_AUBIO STREQUAL "NEVER")
 	libfind_pkg_detect(Aubio aubio FIND_PATH aubio.h PATH_SUFFIXES aubio FIND_LIBRARY aubio)
@@ -31,7 +22,11 @@ elseif(SELF_BUILT_AUBIO STREQUAL "AUTO")
 	libfind_process(Aubio)
 	if(NOT Aubio_FOUND)
 		message(STATUS "aubio build from source because not found on system")
-		_buildAubioFromSource()
+		libfetch_git_pkg(Aubio
+			NAME aubio
+			REPOSITORY ${SELF_BUILT_GIT_BASE}/aubio.git
+			REFERENCE  ${Aubio_GIT_VERSION}
+		)
 	else()
 		set(Aubio_VERSION ${Aubio_PKGCONF_VERSION})
 	endif()
