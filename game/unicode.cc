fee64946faa77bb44d3d21f3ff928422194925bf
Gregorio Litenstein <g.litenstein@gmail.com>
2022-04-28 14:43:14 -0400

    Unicode: fix indents

--- game/unicode.cc
+++ game/unicode.cc
@@ -19,15 +19,15 @@ std::string UnicodeUtil::getCharset (std::string const& str) {
 	bool is_reliable;
 	
 	Encoding encoding = CompactEncDet::DetectEncoding(
-        text, strlen(text),
-        nullptr, nullptr, nullptr,
-        UNKNOWN_ENCODING,
-        UNKNOWN_LANGUAGE,
-        CompactEncDet::WEB_CORPUS,
-        true,
-        &bytes_consumed,
-        &is_reliable);
-        
+		text, strlen(text),
+		nullptr, nullptr, nullptr,
+		UNKNOWN_ENCODING,
+		UNKNOWN_LANGUAGE,
+		CompactEncDet::WEB_CORPUS,
+		true,
+		&bytes_consumed,
+		&is_reliable);
+		
 	if (!is_reliable) {
 			std::clog << "unicode/warning: detected encoding (" <<
 			MimeEncodingName(encoding) << ") for text: " <<
