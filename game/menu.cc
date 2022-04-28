c24adf5186573756744f7af7086fba27f7864f28
Gregorio Litenstein <g.litenstein@gmail.com>
2022-04-28 18:25:57 -0400

    Menus: Use smart pointers.
    Current code re-created the menus by value, from scratch, on every frame. I’m sure we can do better.

--- game/menu.cc
+++ game/menu.cc
@@ -5,8 +5,9 @@
 
 
 MenuOption::MenuOption(std::string const& nm, std::string const& comm, MenuImage img):
-  type(), value(), newValue(), callback(), image(img), name(nm), comment(comm), namePtr(), commentPtr()
-{}
+  type(), m_linePos(-1.0f), value(), newValue(), options(std::make_shared<MenuOptions>()), callback(), image(img), name(nm), comment(comm), namePtr(), commentPtr()
+{
+}
 
 std::string MenuOption::getName() const {
 	if (namePtr) return *namePtr;
@@ -14,7 +15,6 @@ std::string MenuOption::getName() const {
 	if (value) return value->getValue();
 	return "";
 }
-
 std::string MenuOption::getVirtName() const {
 	return virtualName.empty() ? std::string() : virtualName;
 }
@@ -22,7 +22,7 @@ std::string MenuOption::getVirtName() const {
 const std::string& MenuOption::getComment() const { return commentPtr ? *commentPtr : comment; }
 
 bool MenuOption::isActive() const {
-	if (type == OPEN_SUBMENU && options.empty()) return false;
+	if (type == OPEN_SUBMENU && options->empty()) return false;
 	if (type == CHANGE_VALUE) {
 		if (!value) return false;
 		if (value->get_type() == "option_list" && value->ol().size() <= 1) return false;
@@ -31,10 +31,12 @@ bool MenuOption::isActive() const {
 }
 
 
-Menu::Menu(): dimensions(), m_open(true) { clear(); }
+Menu::Menu(): dimensions(), m_open(true), root_options(std::make_shared<MenuOptions>()) { 
+	clear();
+}
 
-void Menu::add(MenuOption opt) {
-	root_options.push_back(opt);
+void Menu::add(std::unique_ptr<MenuOption> opt) {
+	root_options->push_back(std::move(opt));
 	clear(true); // Adding resets menu stack
 }
 
@@ -50,8 +52,8 @@ void Menu::select(size_t sel) {
 void Menu::action(int dir) {
 	switch (current().type) {
 		case MenuOption::OPEN_SUBMENU: {
-			if (current().options.empty()) break;
-			menu_stack.push_back(&current().options);
+			if (current().options->empty()) break;
+			menu_stack.push_back(current().options);
 			selection_stack.push_back(0);
 			break;
 		}
@@ -91,10 +93,10 @@ void Menu::action(int dir) {
 }
 
 void Menu::clear(bool save_root) {
-	if (!save_root) root_options.clear();
+	if (!save_root) root_options->clear();
 	menu_stack.clear();
 	selection_stack.clear();
-	menu_stack.push_back(&root_options);
+	menu_stack.push_back(root_options);
 	selection_stack.push_back(0);
 }
 
