f6bf61c20f90ce3f702c1b69a770ebda96c11657
Gregorio Litenstein <g.litenstein@gmail.com>
2022-05-13 14:01:31 -0400

    util: fix warnings.

--- game/util.cc
+++ game/util.cc
@@ -1,8 +1,8 @@
 #include "util.hh"
 
 // Only conversion types used in Performous are provided
+template <> unsigned sconv(std::string const& s) { return static_cast<std::uint32_t>(std::stoul(s)); }
 template <> int sconv(std::string const& s) { return std::stoi(s); }
-template <> unsigned int sconv(std::string const& s) { return std::stoul(s); }
 template <> float sconv(std::string const& s) { return std::stof(s); }
 template <> double sconv(std::string const& s) { return std::stod(s); }
 template <> std::string sconv(std::string const& s) { return s; }
