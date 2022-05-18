ddc0f7e32feac98b33985849641b2c8651de210c
Tobias Wollgam <twollgam@gmail.com>
2022-05-18 20:12:44 +0200

    Optimize replacing spaces by tabs

--- misc/git-hook/optional/precommit/modify/spaces2tabs.bash
+++ misc/git-hook/optional/precommit/modify/spaces2tabs.bash
@@ -7,7 +7,7 @@ process()
 	file="$1"
 
 	mv "$file" "$file.orig"
-	cat "$file.orig" | sed 's/^\([\t]*\)    /\1\t/g'  | sed 's/^\([\t]*\) \{1,3\}\t/\1\t/g' > "$file"
+	cat "$file.orig" | sed 's/^\([\t]*\) \{1,3\}\t/\1\t/g' | sed 's/^\([\t]*\)        /\1\t\t/g' | sed 's/^\([\t]*\)    /\1\t/g' > "$file"
 	rm "$file.orig"
 	git add "$file"
 }
