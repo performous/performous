4eb0b39994b0c753f8112e1dc90155f7e4aca129
Sébastien Gonzalve <sebastien.gonzalve@aliceadsl.fr>
2022-06-25 10:21:05 +0200

    Add comment on singleton

--- game/singleton.hh
+++ game/singleton.hh
@@ -1,6 +1,14 @@
 #pragma once
 
 /// template for creating singletons
+/// FIXME This class is completey broken:
+/// a singlton should afford that one and only one instance of a class can
+/// exists... and this do not guaranty nothing....
+/// Doing so, there should be a factory to create the object so that the
+/// "Instance" is manaed internally.  Here, uniqueness is not handled, nor
+/// lifetime... so this is more or less a handler for a global pointer... that
+/// can be overwritten without any check....  Taht said, singleton is an anti
+/// patern, so better drop it in future reworks.
 template <class T> class Singleton {
   protected:
 	/// pointer to object
