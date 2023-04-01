5d5564d978323a626c25bdb4cc22f7b0d68015f6
Gregorio Litenstein <g.litenstein@gmail.com>
2023-04-01 14:14:22 -0300

    Dockerfiles and linux scripts: Fix indents

--- docker/build_performous.sh
+++ docker/build_performous.sh
@@ -9,46 +9,46 @@ GIT_REPOSITORY='https://github.com/performous/performous.git'
 
 ## Function to print the help message
 usage() {
-  set +x
-  echo ""
-  echo "Usage: ${0}"
-  echo ""
-  echo "Optional Arguments:"
-  echo "  -b <Git Branch>: Build the specified git branch, tag, or sha"
-  echo "  -D </path/to/build/directory>: Disable cloning the repo from git and build in the specified directory"
-  echo "  -E <'Extra Cmake Args'>: A quoted list of extra arguments to pass directly to cmake"
-  echo "  -g : Generate Packages"
-  echo "  -p <Pull Request #>: Build the specified Github Pull Request number"
-  echo "  -r <Repository URL>: Git repository to pull from"
-  echo "  -R : Perform a 'Release' Cmake Build (Default is 'RelWithDebInfo')"
-  echo "  -h : Show this help message"
-  exit 1
+	set +x
+	echo ""
+	echo "Usage: ${0}"
+	echo ""
+	echo "Optional Arguments:"
+	echo "	-b <Git Branch>: Build the specified git branch, tag, or sha"
+	echo "	-D </path/to/build/directory>: Disable cloning the repo from git and build in the specified directory"
+	echo "	-E <'Extra Cmake Args'>: A quoted list of extra arguments to pass directly to cmake"
+	echo "	-g : Generate Packages"
+	echo "	-p <Pull Request #>: Build the specified Github Pull Request number"
+	echo "	-r <Repository URL>: Git repository to pull from"
+	echo "	-R : Perform a 'Release' Cmake Build (Default is 'RelWithDebInfo')"
+	echo "	-h : Show this help message"
+	exit 1
 }
 
 ## Set up getopts
 while getopts "b:D:E:gp:r:R:h" OPTION; do
-  case ${OPTION} in
-    "b")
-      GIT_BRANCH=${OPTARG};;
-    "D")
-      BUILD_DIRECTORY=${OPTARG};;
-    "E")
-      EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} ${OPTARG}";;
-    "g")
-      GENERATE_PACKAGES=true;;
-    "p")
-      PULL_REQUEST=${OPTARG};;
-    "r")
-      GIT_REPOSITORY=${OPTARG};;
-    "R")
-      PERFORMOUS_BUILD_TYPE=${OPTARG};;
-    "h")
-      HELP=true;;
-  esac
+	case ${OPTION} in
+	"b")
+		GIT_BRANCH=${OPTARG};;
+	"D")
+		BUILD_DIRECTORY=${OPTARG};;
+	"E")
+		EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} ${OPTARG}";;
+	"g")
+		GENERATE_PACKAGES=true;;
+	"p")
+		PULL_REQUEST=${OPTARG};;
+	"r")
+		GIT_REPOSITORY=${OPTARG};;
+	"R")
+		PERFORMOUS_BUILD_TYPE=${OPTARG};;
+	"h")
+		HELP=true;;
+	esac
 done
 
 if [[ "${HELP}" == "true" ]]; then
-  usage
+	usage
 fi
 
 # Pull in any platform-specific flags that may have been set under each docker container.
@@ -58,24 +58,24 @@ fi
 
 ## All the git stuff
 if [[ "${BUILD_DIRECTORY}" == "" ]]; then
-  git clone ${GIT_REPOSITORY}
-  cd performous
-  if [[ "${PULL_REQUEST}" != "" ]]; then
-    git fetch origin pull/${PULL_REQUEST}/head:pr
-    git checkout pr
-  elif [[ "${GIT_BRANCH}" != "" ]]; then
-    git checkout ${GIT_BRANCH}
-  fi
-  git submodule update --init --recursive
+	git clone ${GIT_REPOSITORY}
+	cd performous
+	if [[ "${PULL_REQUEST}" != "" ]]; then
+		git fetch origin pull/${PULL_REQUEST}/head:pr
+		git checkout pr
+	elif [[ "${GIT_BRANCH}" != "" ]]; then
+		git checkout ${GIT_BRANCH}
+	fi
+	git submodule update --init --recursive
 else
-  mkdir -p "${BUILD_DIRECTORY}"
-  cd "${BUILD_DIRECTORY}"
+	mkdir -p "${BUILD_DIRECTORY}"
+	cd "${BUILD_DIRECTORY}"
 fi
 
 
 ## If PACKAGE_TYPE is unset, use tgz.
 if [[ "${PACKAGE_TYPE}" == "" ]]; then
-    PACKAGE_TYPE="TGZ"
+		PACKAGE_TYPE="TGZ"
 fi
 
 
@@ -84,15 +84,15 @@ mkdir build
 cd build
 cmake ${EXTRA_CMAKE_ARGS} \
 	${PLATFORM_CMAKE_FLAGS} \
-    -DENABLE_WEBSERVER=ON \
-    -DCMAKE_VERBOSE_MAKEFILE=ON \
-    -DENABLE_WEBCAM=ON \
-    -DCMAKE_BUILD_TYPE=${PERFORMOUS_BUILD_TYPE} \
-    -DBUILD_TESTS=ON \
-    ..
+		-DENABLE_WEBSERVER=ON \
+		-DCMAKE_VERBOSE_MAKEFILE=ON \
+		-DENABLE_WEBCAM=ON \
+		-DCMAKE_BUILD_TYPE=${PERFORMOUS_BUILD_TYPE} \
+		-DBUILD_TESTS=ON \
+		..
 CPU_CORES=$(nproc --all)
 make -j${CPU_CORES}
 if [[ "${GENERATE_PACKAGES}" == "true" ]]; then
-  cpack -G ${PACKAGE_TYPE}
+	cpack -G ${PACKAGE_TYPE}
 fi
 cd ..
