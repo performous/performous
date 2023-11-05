ee059fbf6ae9e454e99c6d26c1eb7d652aafb9a3
Tobias Wollgam <twollgam@gmail.com>
2023-11-05 14:34:35 +0100

    fix build error mingw

--- game/utils/math.cc
+++ game/utils/math.cc
@@ -3,7 +3,7 @@
 #include <cmath>
 
 float pi() {
-#ifdef WIN32
+#if defined( WIN32) || defined( __MINGW32__ )
 	return static_cast<float>(std::atan(1.0) * 4.0);
 #else
 	return static_cast<float>(M_PI);
