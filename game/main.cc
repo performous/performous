affa75914a23ad7a757fe24976f457fde69900b0
Tobias Wollgam <twollgam@gmail.com>
2023-12-10 15:11:55 +0100

    add command line option --clearcache

--- game/main.cc
+++ game/main.cc
@@ -1,4 +1,5 @@
 #include "backgrounds.hh"
+#include "cache.hh"
 #include "chrono.hh"
 #include "config.hh"
 #include "controllers.hh"
@@ -285,7 +286,8 @@ int main(int argc, char** argv) try {
 	  ("jstest", "utility to get joystick button mappings");
 	po::options_description opt3("Hidden options");
 	opt3.add_options()
-	  ("songdir", po::value<std::vector<std::string> >(&songdirs)->composing(), "");
+	  ("songdir", po::value<std::vector<std::string> >(&songdirs)->composing(), "")
+	  ("clearcache", "clear the cache at start");
 	// Process flagless options as songdirs
 	po::positional_options_description p;
 	p.add("songdir", -1);
@@ -336,6 +338,9 @@ int main(int argc, char** argv) try {
 	confOverride(songdirs, "paths/songs");
 	confOverride(devices, "audio/devices");
 	getPaths(); // Initialize paths before other threads start
+	if (vm.count("clearcache")) {
+		cache::clear();
+	}
 	if (vm.count("jstest")) { // Joystick test program
 		std::clog << "core/notice: Starting jstest input test utility." << std::endl;
 		std::cout << std::endl << "Joystick utility - Touch your joystick to see buttons here" << std::endl
