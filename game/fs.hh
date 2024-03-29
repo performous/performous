80cbba687fc740a06abc3e7b734df8ba4b3766d3
Tobias Wollgam <twollgam@gmail.com>
2024-03-29 17:40:32 +0100

    Remove boost filesystem
    It is only used for macOS less 10.15 which is not supported anymore

--- game/fs.hh
+++ game/fs.hh
@@ -6,23 +6,6 @@
 #include <list>
 #include <vector>
 
-#ifdef USE_BOOST_FS
-#include <boost/filesystem.hpp>
-namespace fs = boost::filesystem;
-
-	#if BOOST_VERSION < 107400
-	// create_directory was added in boost 1.74
-	// see https://www.boost.org/users/history/version_1_74_0.html
-
-	// in boost, copy_directory means create_directory
-	// https://www.boost.org/doc/libs/1_66_0/libs/filesystem/doc/reference.html#copy_directory
-	static inline void create_directory(const fs::path &to, const fs::path &from) {
-	    copy_directory(from, to);
-	}
-
-	#endif
-
-#else
 #include <filesystem>
 namespace fs {
 
@@ -49,8 +32,6 @@ static inline fs::path absolute(const fs::path& p, const fs::path& base) {
             return fs::absolute(base) / p;
     }
 }
-
-#endif
 
 typedef std::vector<std::uint8_t> BinaryBuffer;
 
