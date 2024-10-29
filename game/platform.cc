c0127ea60f613518f3645e4c44244637872d1f69
Gregorio Litenstein <g.litenstein@gmail.com>
2024-10-29 20:20:05 -0300

    Replace windows.h with IntSafe.h

--- game/platform.cc
+++ game/platform.cc
@@ -104,7 +104,7 @@ int Platform::defaultBackEnd() {
 #if (BOOST_OS_WINDOWS)
 extern "C" {
 // For DWORD (see end of file)
-#include "windows.h"
+#include <IntSafe.h>
 // Force high-performance graphics on dual-GPU systems
 	// http://developer.download.nvidia.com/devzone/devcenter/gamegraphics/files/OptimusRenderingPolicies.pdf
 	__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
