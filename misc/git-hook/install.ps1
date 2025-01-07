36380cb17dbade64501c11a5082ead2825d7f732
Tobias Wollgam <twollgam@gmail.com>
2025-01-07 20:50:38 +0100

    Remove optional hook support from install.ps1 and remove modifying scripts

--- misc/git-hook/install.ps1
+++ misc/git-hook/install.ps1
@@ -1,41 +1,36 @@
-param([switch]$all, [switch]$clear)
+param([switch]$clear)
 
 $gitDir = git rev-parse --git-common-dir
 
 if (!(test-path $gitDir)) {
-    $gitDir = git rev-parse --git-dir
+	$gitDir = git rev-parse --git-dir
 }
 
 $gitDir = (Resolve-Path ($gitDir)).Path
 
 if (!$gitDir) {
-    throw ".git - Directory not found"
+	throw ".git - Directory not found"
 }
 
 $gitHooksDir = join-path $gitDir "hooks"
 
 if (!(test-path $gitHooksDir)) {
-    throw "$gitHooksDir not present"
+	throw "$gitHooksDir not present"
 }
 
 if($clear) {
-    write-host "cleaning old hooks dir: $githooksdir"
-    Remove-Item (join-path $githooksdir "*") -force -recurse -exclude "*.sample"
+	write-host "cleaning old hooks dir: $githooksdir"
+	Remove-Item (join-path $githooksdir "*") -force -recurse -exclude "*.sample"
 
-    exit
+	exit
 }
 
 write-host "providing files:"
 
-if ($all) {
-    Copy-Item $PSScriptRoot/optional/* $githooksdir -force -recurse -PassThru | format-table Fullname | out-host
-}
-
 Copy-Item $PSScriptRoot/necessary/* $githooksdir -force -recurse -PassThru | format-table Fullname | out-host
 
 if($IsLinux) {
-    chmod 755 (join-path $githooksdir "post-merge")
-    chmod 755 (join-path $githooksdir "pre-commit")
-    chmod 755 (join-path $githooksdir "precommit/check/*")
-    chmod 755 (join-path $githooksdir "precommit/modify/*")
+	chmod 755 (join-path $githooksdir "post-merge")
+	chmod 755 (join-path $githooksdir "pre-commit")
+	chmod 755 (join-path $githooksdir "precommit/check/*")
 }
