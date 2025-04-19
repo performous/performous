87cf74a80ed9a063727520aee2af190ffde87a38
Gary Kertopermono <garycxjk@gmail.com>
2025-04-19 23:21:00 +0200

    Added grouping by language, refactored imports

---
+++ data/themes/default-booth/www/js/Components/Layout/Content/ContentBar.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { inject } from 'vue';
+
+const content = inject('content');
+
+const buttons = {
+    "folders": "Playlists",
+    "languages": "Languages"
+};
+</script>
+<template>
+    <div class="content-switcher">
+        <button v-for="(label, cat) in buttons" type="button" @click="content = cat" :disabled="content === cat">{{ label }}</button>
+    </div>
+</template>
