604d8dc35b2a039cf3977a14ddc9c5394b529d81
Gary Kertopermono <garycxjk@gmail.com>
2025-04-19 23:21:00 +0200

    Added grouping by language, refactored imports

---
+++ data/themes/default-booth/www/js/constants.js
@@ -0,0 +1,14 @@
+const constants = {
+    // Remaps languages to other languages, allowing you to group similar languages together
+    remapLanguages: {
+        Drents: 'Dutch',
+        'Español': 'Spanish',
+        Scat: 'Other',
+    },
+
+    // Allows you to remap the language to a language file name (without file extension)
+    remapFlags: {},
+    
+    // Allows you to remap the folders to a different file name for the cover image (without file extension)
+    remapFolders: {},
+};
