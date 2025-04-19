a06e5b1748cec39ac414d26d21200d1457cf6dd8
Gary Kertopermono <garycxjk@gmail.com>
2025-04-19 05:06:20 +0200

    Created new web frontend

---
+++ data/themes/default-booth/www/js/Components/Layout/Content/Folders/Category.vue
@@ -0,0 +1,23 @@
+<script setup>
+import SongList from '../../../Common/SongList.vue';
+const { songs } = defineProps({
+    songs: {
+        type: Array,
+        required: true,
+    },
+});
+
+const category = defineModel({
+    type: String,
+    required: true,
+});
+</script>
+<template>
+    <div>
+        <div class="header">
+            {{ category }}
+            <div class="back" @click="category = null">&lt; Back</div>
+        </div>
+        <SongList :songs />
+    </div>
+</template>
