bf9002a36656475fdc8bb9620421c892a37bb5a3
Gary Kertopermono <garycxjk@gmail.com>
2025-04-20 18:03:41 +0200

    Attempt to mitigate issue with regular expressions

--- data/themes/default-booth/www/js/helpers.js
+++ data/themes/default-booth/www/js/helpers.js
@@ -1,7 +1,7 @@
 const helpers = (() => {
     const exports = {
         getLanguageName(language) {
-            let lang = language.trim().replace(/\s*\(romanized\)$/i, '');
+            let lang = language.trim().replace(/\s?\(romanized\)$/i, '');
             if (constants.remapLanguages[lang]) {
                 lang = constants.remapLanguages[lang];
             }
