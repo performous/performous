04291de5c1dd4b7c4e6c86d9fae677b4612cb54b
Gary Kertopermono <garycxjk@gmail.com>
2025-04-20 02:56:33 +0200

    Added infinite scroll to gradually load in all song elements on search screen

--- data/themes/default-booth/www/js/Components/Layout/Content/Search.vue
+++ data/themes/default-booth/www/js/Components/Layout/Content/Search.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, onBeforeUnmount } from 'vue';
+import { computed, onBeforeUnmount, ref, watch } from 'vue';
 import { useStore } from 'vuex';
 import SongList from '../../Common/SongList.vue';
 
@@ -17,16 +17,27 @@ const search = computed({
     },
 });
 
-const songs = computed(() => store.state.database);
+const songs = computed(() => [...store.state.database]);
+
+const songLength = computed(() => songs.value.length);
+const prevLength = ref(songLength.value);
+const renderLimit = ref(100);
 
 onBeforeUnmount(() => {
     store.dispatch('refreshDatabase');
+});
+
+watch(songLength, (newLength) => {
+    if (prevLength.value !== newLength) {
+        renderLimit.value = 0;
+    }
+    prevLength.value = newLength;
 });
 </script>
 <template>
 <div class="search">
     <div class="header">{{ $store.state.language.search }}</div>
     <div class="input"><input type="text" v-model="search" /></div>
-    <SongList :songs />
+    <SongList :songs v-model="renderLimit" />
 </div>
 </template>
