365bc700276b3deeb8c7753cae28a6668916e36f
Gregorio Litenstein <g.litenstein@gmail.com>
2025-06-01 23:00:55 -0400

    Account for incompatibilities in fmt and spdlog
    Not a good idea to have such generic function lying around in global namespace. Maybe we should make a `utils:: namespace?
    
    Signed-off-by: Gregorio Litenstein <g.litenstein@gmail.com>

--- cmake/Modules/FindSpdlog.cmake
+++ cmake/Modules/FindSpdlog.cmake
@@ -3,7 +3,16 @@ cmake_minimum_required(VERSION 3.24)
 include(LibFindMacros)
 include(LibFetchMacros)
 
-set(Spdlog_GIT_VERSION "v1.15.0")
+set(Spdlog_GIT_VERSION "v1.15.3")
+
+message(STATUS "FMT VERSION: ${fmt_VERSION}")
+
+if(fmt_VERSION VERSION_GREATER_EQUAL 11.1.0)
+	set(spdlog_needed "1.15.1")
+else()
+	set(spdlog_needed "1.12.0")
+	set(Spdlog_GIT_VERSION "v1.15.2")
+endif()
 
 if(SELF_BUILT_SPDLOG STREQUAL "ALWAYS")
 	message(STATUS "spdlog forced to build from source")
@@ -13,9 +22,9 @@ if(SELF_BUILT_SPDLOG STREQUAL "ALWAYS")
 		OVERRIDE_FIND_PACKAGE
 	)
 elseif(SELF_BUILT_SPDLOG STREQUAL "NEVER")
-	find_package(spdlog 1.12.0 REQUIRED PATHS /usr/lib PATH_SUFFIXES ${CMAKE_CXX_LIBRARY_ARCHITECTURE}/cmake/spdlog)
+	find_package(spdlog ${spdlog_needed} REQUIRED PATHS /usr/lib PATH_SUFFIXES ${CMAKE_CXX_LIBRARY_ARCHITECTURE}/cmake/spdlog)
 elseif(SELF_BUILT_SPDLOG STREQUAL "AUTO")
-	find_package(spdlog 1.12.0 PATHS /usr/lib PATH_SUFFIXES ${CMAKE_CXX_LIBRARY_ARCHITECTURE}/cmake/spdlog)
+	find_package(spdlog ${spdlog_needed} PATHS /usr/lib PATH_SUFFIXES ${CMAKE_CXX_LIBRARY_ARCHITECTURE}/cmake/spdlog)
 	if(NOT spdlog_FOUND)
 		message(STATUS "spdlog build from source because not found on system")
 		libfetch_git_pkg(Spdlog
