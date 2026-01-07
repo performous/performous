42defcc7381585db753a9025e980876c782cd573
kt-- <krt@krt.com.au>
2026-01-08 06:45:22 +1100

    Remove bit of comment-out accidentally left in

--- game/graphic/text_renderer.hh
+++ game/graphic/text_renderer.hh
@@ -13,7 +13,6 @@ public:
 
 	OpenGLText render(const std::string &, const TextStyle&, float m);
 	Size measure(const std::string &, const TextStyle&, float m);
-	size_t measureColumnsOLD(const std::string& text, const TextStyle& style, float m, std::array<float,MAX_COLUMNS>& columns);
 	size_t measureColumns(const std::string& text, const TextStyle& style, float m, std::array<float,MAX_COLUMNS>& columns);
 
     size_t tokenSplitter(const std::string &text, char token, std::array<TextExtent,MAX_COLUMNS> &extents); ///< Finds extents of token-delimited substrings
