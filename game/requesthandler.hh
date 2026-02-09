75633f505d786283937cae75ced844bf463a6653
Gary Kertopermono <garycxjk@gmail.com>
2026-02-09 21:20:18 +0100

    Enhance SongsToJsonObject method to support pagination with start and limit parameters

--- game/requesthandler.hh
+++ game/requesthandler.hh
@@ -27,7 +27,7 @@ class RequestHandler
 
 	void HandleFile(web::http::http_request request, std::string filePath = "");
 	web::json::value SongToJsonObject(std::shared_ptr<Song> song);
-	web::json::value SongsToJsonObject();
+	web::json::value SongsToJsonObject(size_t start = 0, size_t limit = 0);
 	std::map<std::string, std::string> GenerateLocaleDict();
 	std::vector<std::string> GetTranslationKeys();
 	std::shared_ptr<Song> GetSongFromJSON(web::json::value);
